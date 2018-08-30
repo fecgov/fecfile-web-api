bf585cef078896872ba830d94bafcce9b6e9327c
Roberto Sanchez <rsanchez.ctr@fec.gov>
2018-08-30 14:03:33 -0400

    django commit to development

---
+++ django-backend/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read(),
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU General Public License (GPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+]
+
+setup(
+    author="Alex Clark",
+    author_email="aclark@aclark.net",
+    maintainer='Leonardo J. Caballero G.',
+    maintainer_email='leonardocaballero@gmail.com',
+    name='helloworld',
+    version='0.1',
+    description='A Django hello world example ',
+    long_description=LONG_DESCRIPTION,
+    url='https://github.com/django-ve/helloworld',
+    license='GPL',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    install_requires=[
+        'Django==1.9.4',
+    ],
+    packages=find_packages(exclude=["project","project.*"]),
+    include_package_data=True,
+    zip_safe = False
+)
