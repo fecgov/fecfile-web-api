a63b8d6c739dbae6b88ded3aecd75cdaed029702
Craig Stroman <cstroman@gmail.com>
2018-09-04 14:42:16 -0400

    sprint 9

--- front-end/src/app/app-routing/app-routing.module.spec.ts
+++ front-end/src/app/app-routing/app-routing.module.spec.ts
@@ -1,13 +1,54 @@
-import { AppRoutingModule } from './app-routing.module';
+import { Location } from '@angular/common';
+import { TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router, Routes } from '@angular/router';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { routes, AppRoutingModule } from './app-routing.module';
+import { AppComponent } from '../app.component';
+import { LoginComponent } from '../login/login.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+import { ProfileComponent } from '../profile/profile.component';
 
 describe('AppRoutingModule', () => {
   let appRoutingModule: AppRoutingModule;
+  let location: Location;
+  let router: Router;
+  let fixture;
 
   beforeEach(() => {
     appRoutingModule = new AppRoutingModule();
+
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule.withRoutes(routes)
+      ],
+      declarations: [
+        AppComponent,
+        LoginComponent,
+        DashboardComponent,
+        ProfileComponent
+      ],
+      providers: [
+        FormBuilder
+      ]
+    });
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
   });
 
   it('should create an instance', () => {
     expect(appRoutingModule).toBeTruthy();
   });
+
+  it('navigate to "" redirects you to login', fakeAsync(() => {
+    router.navigate(['']);
+    tick();
+    expect(location.path()).toBe('');
+  }));
 });
