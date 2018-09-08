57b8d3994689a9a66ce4a5eb61ab23233dec00ce
Ranjith Dasaradhi <ranjith.dasaradhi@ctr.salientcrgt.com>
2018-09-08 19:29:33 +0000

    CSRF Exempt added

--- django-backend/fecfiler/urls.py
+++ django-backend/fecfiler/urls.py
@@ -4,6 +4,7 @@ from rest_framework_nested import routers
 from .authentication.views import AccountViewSet, LoginView, LogoutView
 from .posts.views import AccountPostsViewSet, PostViewSet
 from .views import IndexView
+from django.views.decorators.csrf import csrf_exempt
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
@@ -21,8 +22,8 @@ urlpatterns = patterns(
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/', include(accounts_router.urls)),
-    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/login/$', csrf_exempt(LoginView.as_view()), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/docs/', include('rest_framework_swagger.urls')),
-    url('^.*$', IndexView.as_view(), name='index'),
+    #url('^.*$', IndexView.as_view(), name='index'),
 )
