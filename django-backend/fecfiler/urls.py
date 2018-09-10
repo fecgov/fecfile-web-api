951bdcf40da6fca3af5b9e0624b8f4f32cf4b225
rdasaradhi <41478590+rdasaradhi@users.noreply.github.com>
2018-09-10 12:42:07 -0400

    Update urls.py

--- django-backend/fecfiler/urls.py
+++ django-backend/fecfiler/urls.py
@@ -22,7 +22,7 @@ urlpatterns = patterns(
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api/v1/', include(router.urls)),
     url(r'^api/v1/', include(accounts_router.urls)),
-    url(r'^api/v1/auth/login/$', csrf_exempt(LoginView.as_view()), name='login'),
+    url(r'^api/v1/auth/login$', csrf_exempt(LoginView.as_view()), name='login'),
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
     url(r'^api/docs/', include('rest_framework_swagger.urls')),
     url('^.*$', IndexView.as_view(), name='index'),
