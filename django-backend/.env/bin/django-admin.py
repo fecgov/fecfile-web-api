8894ca363821858983ed4c8be1b87e7562963235
Roberto Sanchez <rsanchez@rhythmictech.com>
2018-09-15 01:13:36 -0400

    removed python virtualenv

--- django-backend/.env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/rsanchez/Documents/projects/FEC/nxg_fec_frontendDocker/django-backend/.env/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
