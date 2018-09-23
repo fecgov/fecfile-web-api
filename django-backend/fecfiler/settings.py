30e4f7ff0cd6325e46e1e8bd8436a6971b592ee8
rsanchez-s <roberto.sanchez@ctr.salientcrgt.com>
2018-09-23 01:53:20 -0400

    removed node_modules and other directories git cache

--- django-backend/fecfiler/settings.py
+++ django-backend/fecfiler/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'db',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
