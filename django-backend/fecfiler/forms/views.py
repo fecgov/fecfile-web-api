1ca0589edf4a08b5868bb80c68cd87996cb66623
jpraveenkmrSalient <praveen.jinka@ctr.salientcrgt.com>
2018-10-19 15:16:33 -0400

    pdf validation

--- django-backend/fecfiler/forms/views.py
+++ django-backend/fecfiler/forms/views.py
@@ -12,6 +12,7 @@ import os
 from django.views.decorators.csrf import csrf_exempt
 import logging
 import datetime
+import magic
 from django.http import JsonResponse
 
 # API view functionality for GET DELETE and PUT
@@ -242,6 +243,7 @@ def validate_f99(request):
             'treasurerprefix': request.data.get('treasurerprefix'),
             'treasurersuffix': request.data.get('treasurersuffix'),
             'email_on_file' : request.data.get('email_on_file'),
+            'file': request.data.get('file'),
         }
     else:
         return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -255,48 +257,62 @@ def validate_f99(request):
     errormess = []
 
     if comm.committeename!=request.data.get('committeename'):
-        errormess.append('Committee Name does not match with Form 1 data.')
+        errormess.append('Committee Name does not match the Form 1 data.')
         
     if comm.street1!=request.data.get('street1'):
-        errormess.append('Street1 does not match with Form 1 data.')
+        errormess.append('Street1 does not match the Form 1 data.')
             
-    if comm.street2!=request.data.get('street2'):
-        errormess.append('Street2 does not match with Form 1 data.')
+    if 'street2' in request.data and comm.street2!=request.data.get('street2'):
+        errormess.append('Street2 does not match the Form 1 data.')
         
     if comm.city!=request.data.get('city'):
-        errormess.append('City does not match with Form 1 data.')
+        errormess.append('City does not match the Form 1 data.')
             
     if comm.state!=request.data.get('state'):
-        errormess.append('State does not match with Form 1 data.')
-        
+        errormess.append('State does not match the Form 1 data.')
+    
+    if comm.zipcode!=int(request.data.get('zipcode')):
+        errormess.append('Zipcode does not match the Form 1 data.')
+
     if comm.treasurerlastname!=request.data.get('treasurerlastname'):
-        errormess.append('Treasurer Last Name does not match with Form 1 data.')
+        errormess.append('Treasurer Last Name does not match the Form 1 data.')
             
     if comm.treasurerfirstname!=request.data.get('treasurerfirstname'):
-        errormess.append('Treasurer First Name does not match with Form 1 data.')
+        errormess.append('Treasurer First Name does not match the Form 1 data.')
         
-    if comm.treasurermiddlename!=request.data.get('treasurermiddlename'):
-        errormess.append('Treasurer Middle Name does not match with Form 1 data.')
+    if 'treasurermiddlename' in request.data and comm.treasurermiddlename!=request.data.get('treasurermiddlename'):
+        errormess.append('Treasurer Middle Name does not match the Form 1 data.')
         
-    if comm.treasurerprefix!=request.data.get('treasurerprefix'):
-        errormess.append('Treasurer Prefix does not match with Form 1 data.')
+    if 'treasurerprefix' in request.data and comm.treasurerprefix!=request.data.get('treasurerprefix'):
+        errormess.append('Treasurer Prefix does not match the Form 1 data.')
         
-    if comm.treasurersuffix!=request.data.get('treasurersuffix'):
-        errormess.append('Treassurer Suffix does not match with Form 1 data.')
+    if 'treasurersuffix' in request.data and comm.treasurersuffix!=request.data.get('treasurersuffix'):
+        errormess.append('Treasurer Suffix does not match the Form 1 data.')
         
     if len(request.data.get('text'))>20000:
-        errormess.append('Text greater than 20000')
+        errormess.append('Text greater than 20000.')
+
+    if len(request.data.get('text'))==0:
+        errormess.append('Text field is empty.')
         
     conditions = [request.data.get('reason')=='MST', request.data.get('reason')=='MSM', request.data.get('reason')=='MSI', request.data.get('reason')=='MSW']
     if not any(conditions):
-        errormess.append('Reason does not match the exisiting data')
-        
-    #if not request.FILES['file'].name.endswith('.pdf'):
-    #    errormess.append('Supports only pdf file extensions')
-        
-    #if request.FILES['file']._size > 33554432:
-    #    errormess.append('File size is more than 32 MB')
-        
+        errormess.append('Reason does not match the pre-defined codes.')
+    
+    #pdf validation for type, extension and size
+    if 'file' in request.data:
+        valid_mime_types = ['application/pdf']
+        file = request.data.get('file')
+        file_mime_type = magic.from_buffer(file.read(1024), mime=True)
+        if file_mime_type not in valid_mime_types:
+            errormess.append('This is not a pdf file type. Kindly open your document using a pdf reader before uploading it.')
+        valid_file_extensions = ['.pdf']
+        ext = os.path.splitext(file.name)[1]
+        if ext.lower() not in valid_file_extensions:
+            errormess.append('Unacceptable file extension. Only files with .pdf extensions are accepted.')
+        if file._size > 33554432:
+            errormess.append('The File size is more than 32 MB. Kindly reduce the size of the file before you upload it.')    
+            
     if len(errormess)==0:
         errormess.append('Validation successful!')
         return JsonResponse(errormess, status=200, safe=False)
