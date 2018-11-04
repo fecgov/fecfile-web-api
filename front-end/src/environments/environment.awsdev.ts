0fd497dd07d208a955fd26ea94d7f57a3c143172
Cris Daniluk <cdaniluk@rhythmictech.clom>
2018-11-03 20:25:09 -0400

    added new awsdev environment as a temporary workaround until api url can be externalized

---
+++ front-end/src/environments/environment.awsdev.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  name: 'awsdev',
+  apiUrl: 'https://fecfile-api.efdev.fec.gov/api/v1',
+  validateSuccess: 'All required fields have been validated.'
+};
