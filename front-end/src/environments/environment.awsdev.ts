ee4ee86b1a41ac4e4b4def6ad8fb268983b802a8
Roberto Sanchez <rsanchez@rhythmictech.com>
2018-11-23 23:07:12 -0500

    added app title to environment

--- front-end/src/environments/environment.awsdev.ts
+++ front-end/src/environments/environment.awsdev.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   name: 'awsdev',
+  'appTitle': 'fecfile',
   apiUrl: 'https://dev-fecfile-api.efdev.fec.gov/api/v1',
   validateSuccess: 'All required fields have been validated.'
 };
