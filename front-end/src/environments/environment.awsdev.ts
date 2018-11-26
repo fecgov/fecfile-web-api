4154f9385f9d69ee3ef90275b86d6cd316b15c69
Roberto Sanchez <rsanchez@rhythmictech.com>
2018-11-26 15:05:16 -0500

    removed duplicate entries

--- front-end/src/environments/environment.awsdev.ts
+++ front-end/src/environments/environment.awsdev.ts
@@ -1,7 +1,6 @@
 export const environment = {
   production: true,
   name: 'awsdev',
-  'appTitle': 'fecfile',
   apiUrl: 'https://dev-fecfile-api.efdev.fec.gov/api/v1',
   appTitle: 'FECFile',
   validateSuccess: 'All required fields have been validated.'
