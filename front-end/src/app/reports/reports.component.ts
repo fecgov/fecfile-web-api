6fa9c7d88d62441cc07fffece3876485f8653f6b
Craig Stroman <cstroman@gmail.com>
2018-11-27 22:31:36 -0500

    updates

--- front-end/src/app/reports/reports.component.ts
+++ front-end/src/app/reports/reports.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { IReport } from './report';
+import { ReportService } from './reports.service';
+
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['./reports.component.scss']
+  styleUrls: ['./reports.component.scss'],
+  providers:[ReportService]
 })
+
 export class ReportsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  reports: IReport[];
+  
+  constructor(private _reportService:ReportService){ 
   }
-
+  
+  /*trackById(index:number, reports:IReport[]): string{
+    return reports[[]].id;
+  }*/
+ 
+  
+  ngOnInit() {
+    console.log("accessing service call...");
+    this._reportService.getReports()
+      .subscribe(res => this.reports = <IReport[]> res);
+    console.log(this.reports)
+  }
 }
