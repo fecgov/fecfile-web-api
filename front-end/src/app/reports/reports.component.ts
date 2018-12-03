5067933e6d8bd2cc8c1664a8ab71ea6fb9d202e0
Ranjith Dasaradhi <ranjith.dasaradhi@ctr.salientcrgt.com>
2018-12-03 19:26:14 +0000

    fixing issues

--- front-end/src/app/reports/reports.component.ts
+++ front-end/src/app/reports/reports.component.ts
@@ -1,57 +1,56 @@
-<section>
-	<div class="reports-container">
-	    <div class="grid-row">
-	      <div class="grid-row__col grid-row__col--12">
-					<h1 class="text-left">Reports</h1>
-					<table class="table-striped" style="width:100%">
-					<thead>
-						<tr text-align="left">
-							<th text-align="left">Form</th>
-							<th text-align="left">Status</th>
-							<th text-align="right">FEC ID</th>
-							<th text-align="left">Coverage Start Date</th>
-							<th text-align="left">Coverage End Date</th>
-							<th text-align="left">Report</th>
-							<th text-align="left">Filed Date</th>
-							<th text-align="left">Actions</th>
-						</tr>
-					</thead>	
-					<tbody>
-						<tr *ngFor="let report of reports || async">
-							<td text-align="left">{{report.form_type}}</td>
-							<td text-align="left">{{report.is_submitted ? 'Submitted' :'Save'}}</td>
-							<td text-align="right" >{{report.is_submitted ? report.id : ''}}</td>
-							<td text-align="left">{{report.coverage_start_date | date: 'MM/dd/yyyy'}}</td>
-							<td text-align="left">{{report.coverage_end_date | date: 'MM/dd/yyyy'}}</td>
-							<td text-align="left">{{report.ReportType}}</td>
-							<td text-align="left">{{report.created_at | date: 'MM/dd/yyyy'}}</td>
-							<td></td>
-						</tr>
-					</tbody>
-					</table>
-	      </div>
-	    </div>		
-	</div>
- <div>
-	<footer>
-        <div class="footer">
-          <div class="legal-disclaimer">
-            <button
-              type="button"
-              class="btn btn-link"
-              (click)="open()"
-            >
-              Legal Disclaimer
-            </button>
-          </div>
-          <div class="legal-text" [hidden]="!showLegalDisclaimer">
-            <p>
-              Please be advised that knowingly and willfully making any materially false, fictitious, or fraudulent statement or representation to a federal
-              government agency, including the Federal Election Commission, is punishable under the provisions of 18 U.S.C. § 1001. The Commission may report
-              apparent violations to the appropriate law enforcement authorities. 52 U.S.C. § 30107(a)(9).
-            </p>          
-          </div>          
-        </div>
-      </footer>
- </div>	
-</section>
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { IReport } from './report';
+import { ReportService } from './reports.service';
+
+import { SessionService } from '../shared/services/SessionService/session.service';
+import { ApiService } from '../shared/services/APIService/api.service';
+import { MessageService } from '../shared/services/MessageService/message.service';
+import { HeaderComponent } from '../shared/partials/header/header.component';
+import { SidebarComponent } from '../shared/partials/sidebar/sidebar.component';
+import { FormsComponent } from '../forms/forms.component';
+
+@Component({
+  selector: 'app-reports',
+  templateUrl: './reports.component.html',
+  styleUrls: ['./reports.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  providers:[ReportService,MessageService]
+})
+
+export class ReportsComponent implements OnInit {
+  reports: IReport[];
+  public showSideBar: boolean = true;
+  public showLegalDisclaimer: boolean = false;
+
+  constructor(
+    private _reportService:ReportService,
+    private _sessionService: SessionService,
+    private _apiService: ApiService,
+    private _modalService: NgbModal
+    /*rivate _messageService: MessageService*/
+    ){}
+  
+  /*trackById(index:number, reports:IReport[]): string{
+    return reports[[]].id;
+  }*/
+ 
+  public toggleSideNav(): void {
+    if (this.showSideBar) {
+      this.showSideBar = false;
+    } else if (!this.showSideBar) {
+      this.showSideBar = true;
+    }
+}
+  ngOnInit() {
+    console.log("accessing service call...");
+    this._reportService.getReports()
+      .subscribe(res => this.reports = <IReport[]> res);
+    console.log(this.reports)
+  }
+
+  public open(): void{
+    this.showLegalDisclaimer = !this.showLegalDisclaimer;
+  }
+
+}
