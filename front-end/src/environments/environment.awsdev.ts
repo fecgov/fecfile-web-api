40d9f7bd93b864c139b8eaaee4a287e2188f9ebf
Ranjith Dasaradhi <ranjith.dasaradhi@ctr.salientcrgt.com>
2018-12-07 05:10:25 +0000

    Mahendra's changes env, rep, sub, log,app,ind, dash

--- front-end/src/environments/environment.awsdev.ts
+++ front-end/src/environments/environment.awsdev.ts
@@ -3,5 +3,5 @@ export const environment = {
   name: 'awsdev',
   apiUrl: 'https://dev-fecfile-api.efdev.fec.gov/api/v1',
   validateSuccess: 'All required fields have passed validation.',
-  appTitle: 'FECFile'
+  appTitle: 'FECFile Online'
 };
