f16cd6998c53598b8913d8fa4e8bc6a617b44578
Craig Stroman <cstroman@gmail.com>
2018-12-11 16:09:50 -0500

    updates

--- front-end/src/app/shared/partials/sidebar/sidebar.component.ts
+++ front-end/src/app/shared/partials/sidebar/sidebar.component.ts
@@ -11,7 +11,8 @@ export class SidebarComponent implements OnInit {
   @Output() status: EventEmitter<any> = new EventEmitter<any>();
 
   public iconClass: string = 'bars-icon';
-  public sidebarVisibleClass: string = 'sidebar-visible';
+  public sidebarVisibleClass: string = 'sidebar-hidden';
+  public formType: string = null;
 
   constructor(
     private _router: Router,
@@ -21,7 +22,7 @@ export class SidebarComponent implements OnInit {
   ngOnInit(): void {   
     let route: string = this._router.url;
 
-    if(route) {
+    /*if(route) {
       if(route.indexOf('/dashboard') === 0) {
         if(this.iconClass !== 'close-icon') {
           this.iconClass = 'close-icon';
@@ -31,21 +32,43 @@ export class SidebarComponent implements OnInit {
           });               
         }
       }
-    }       
+    }   */    
     
     this._router
       .events
       .subscribe(val => {
         if(val) {
           if(val instanceof NavigationEnd) {
+            console.log('val: ', val);
             if(val.url.indexOf('/forms/form/') === 0) {
+              console.log('form');
               this._closeNavBar();           
             } else if (val.url.indexOf('/dashboard') === 0) {
+              console.log('dashboard: ');
               this._openNavBar();        
             }
           }
         }
       });  
+
+      /*this._activatedRoute
+        .params
+        .subscribe(params => {
+          console.log('params: ', params);
+        });*/
+
+      /*this._activatedRoute
+        .params
+        .subscribe(params => {
+          console.log('params: ', params);
+          this.formType = params['form_id'];
+
+          console.log('formType:', this.formType);
+        });*/
+  }
+
+  ngDoCheck(): void {
+    console.log('sidebar component: ');
   }
 
   /**
@@ -59,6 +82,14 @@ export class SidebarComponent implements OnInit {
       this._openNavBar();      
     } 	
   }
+
+  public toggleToolTip(tooltip): void {
+    if (tooltip.isOpen()) {
+      tooltip.close();
+    } else {
+      tooltip.open();
+    }      
+  }    
 
   /**
    * Closes the navbar.
@@ -79,10 +110,14 @@ export class SidebarComponent implements OnInit {
    * Opens the navbar.
    */
   private _openNavBar(): void {
+    console.log('_openNavBar:');
     this.iconClass = 'close-icon';
 
     setTimeout(() => {
+      console.log('update visibile class: ');
       this.sidebarVisibleClass = 'sidebar-visible';
+
+      console.log('this.sidebarVisibleClass:', this.sidebarVisibleClass);
     }, 10);    
 
     this.status.emit({
