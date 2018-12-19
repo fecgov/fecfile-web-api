b631ec4d297cf52c30158781ec4279e6cfe06165
Ranjith Dasaradhi <ranjith.dasaradhi@ctr.salientcrgt.com>
2018-12-19 19:17:41 +0000

    fixed empty static prefix issue regards to file upload

--- django-backend/fecfiler/urls.py
+++ django-backend/fecfiler/urls.py
@@ -39,5 +39,5 @@ urlpatterns = [
     #url('^.*$', IndexView.as_view(), name='index'),
 ]
 
-if settings.DEBUG:
-  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+#  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
