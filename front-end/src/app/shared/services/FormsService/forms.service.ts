5ce9b96c03903d0112e15707ebe5d6d7e1ad1034
MahendraMarathe <44204994+MahendraMarathe@users.noreply.github.com>
2018-12-31 13:38:53 -0500

    Update forms.service.ts
    print preview related changes

--- front-end/src/app/shared/services/FormsService/forms.service.ts
+++ front-end/src/app/shared/services/FormsService/forms.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, identity } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 import { form99 } from '../../interfaces/FormsService/FormsService';
@@ -341,6 +341,224 @@ export class FormsService {
       )
       .pipe(map(res => {
           if (res) {
+            return true;
+          }
+          return false;
+      }));      
+  }
+
+  public PreviewForm_ReasonScreen(formObj: any, file: any, form_type: string): Observable<any> {
+    let form_99_details_res: any ={};
+    let form_99_details_res_tmp: form99;
+
+    let token: string = JSON.parse(this._cookieService.get('user'));
+    let data: any = {};
+    let formData: FormData = new FormData();
+    let httpOptions =  new HttpHeaders();
+    let url: string = '/f99/save_print_f99';
+    let id:string;
+    let org_filename="";
+    let org_fileurl="";
+    let fileuploaded:boolean=false;
+    let printpriview_filename = "";
+    let printpriview_fileurl = "";
+
+    httpOptions = httpOptions.append('enctype', 'multipart/form-data');
+    httpOptions = httpOptions.append('Authorization', 'JWT ' + token); 
+
+    if(form_type === '99') {
+
+      let form99_details: form99 = JSON.parse(localStorage.getItem(`form_${form_type}_details`));
+
+      if(file && file.name) {
+        fileuploaded=true;
+        localStorage.setItem('form_99_details.file', file);
+        formData.append('file', file, file.name);         
+        formData.append('committeeid', form99_details.committeeid);
+        formData.append('committeename', form99_details.committeename);
+        formData.append('street1', form99_details.street1);
+        formData.append('street2', form99_details.street2);
+        formData.append('city', form99_details.city);
+        formData.append('state', form99_details.state);
+        formData.append('zipcode', form99_details.zipcode);
+        formData.append('treasurerprefix', form99_details.treasurerprefix);
+        formData.append('treasurerfirstname', form99_details.treasurerfirstname);
+        formData.append('treasurermiddlename', form99_details.treasurermiddlename);
+        formData.append('treasurerlastname', form99_details.treasurerlastname);
+        formData.append('treasurersuffix', form99_details.treasurersuffix);
+        formData.append('reason', form99_details.reason);
+        formData.append('text', form99_details.text);
+        formData.append('signee', form99_details.signee);
+        formData.append('email_on_file', form99_details.email_on_file);
+        formData.append('email_on_file_1', form99_details.email_on_file_1);
+        formData.append('additional_email_1', form99_details.additional_email_1);
+        formData.append('additional_email_2', form99_details.additional_email_2);        
+        formData.append('created_at', form99_details.created_at);
+        formData.append('is_submitted', "False");
+        formData.append('filename', form99_details.filename);
+        formData.append('form_type', "F99");
+        if (form99_details.id ==='' || form99_details.id ==="" || form99_details.id===null){
+          /*data['id']="0";*/
+          formData.append('id', "0");
+        }
+        else
+        {
+          formData.append('id', form99_details.id.toString());
+        }
+      }
+      else
+      {
+
+        fileuploaded=false;
+        /*form99_details.is_submitted=false; 
+        formData.append('file', form99_details.file);  */
+
+        if (form99_details.filename!= null || form99_details.filename!= '' || form99_details.filename != "" || form99_details.filename != undefined){
+          formData.append('filename', form99_details.filename);    
+        }
+
+        formData.append('committeeid', form99_details.committeeid);
+        formData.append('committeename', form99_details.committeename);
+        formData.append('street1', form99_details.street1);
+        formData.append('street2', form99_details.street2);
+        formData.append('city', form99_details.city);
+        formData.append('state', form99_details.state);
+        formData.append('zipcode', form99_details.zipcode);
+        formData.append('treasurerprefix', form99_details.treasurerprefix);
+        formData.append('treasurerfirstname', form99_details.treasurerfirstname);
+        formData.append('treasurermiddlename', form99_details.treasurermiddlename);
+        formData.append('treasurerlastname', form99_details.treasurerlastname);
+        formData.append('treasurersuffix', form99_details.treasurersuffix);
+        formData.append('reason', form99_details.reason);
+        formData.append('text', form99_details.text);
+        formData.append('signee', form99_details.signee);
+        formData.append('email_on_file', form99_details.email_on_file);
+        formData.append('email_on_file_1', form99_details.email_on_file_1);
+        formData.append('additional_email_1', form99_details.additional_email_1);
+        formData.append('additional_email_2', form99_details.additional_email_2);        
+        formData.append('created_at', form99_details.created_at);
+        formData.append('is_submitted', "False");
+        /*formData.append('filename', form99_details.filename);*/
+        formData.append('form_type', "F99");
+        if (form99_details.id ==='' || form99_details.id ==="" || form99_details.id===null){
+        
+          /*data['id']="0";*/
+          formData.append('id', "0");
+        }
+        else
+        {
+          formData.append('id', form99_details.id.toString());
+        }
+
+      }  
+     
+      data=formData;
+
+  
+    }
+
+    console.log ('PreviewForm_ReasonScreen Data: ',data);
+
+    for (var value of data.values()) {
+      console.log(value); 
+   }
+    console.log ('loop through form Data: ');
+    console.log(data);
+
+   new Response(data).text().then(console.log)
+    
+    return this._http
+      .post(
+        `${environment.apiUrl}${url}`,
+        data,
+        {
+          headers: httpOptions
+        }
+      )
+      .pipe(map(res => {
+          if (res) {
+            localStorage.setItem('form_99_details_res', JSON.stringify(res));
+            let form99_details_res: form99 = JSON.parse(localStorage.getItem(`form_99_details_res`));
+            id=form99_details_res.id.toString();
+
+            org_filename=form99_details_res.filename;
+            org_fileurl=form99_details_res.file;
+            printpriview_filename = form99_details_res.printpriview_filename;
+            printpriview_fileurl = form99_details_res.printpriview_fileurl;
+            
+            localStorage.setItem('form_99_details.id', id);
+            localStorage.setItem('form_99_details.org_filename', org_filename);
+            localStorage.setItem('form_99_details.org_fileurl', org_fileurl);
+
+            console.log ('org_filename',org_filename);
+            console.log ('org_fileurl',org_fileurl);
+            console.log ('org_filename',printpriview_filename);
+            console.log ('org_fileurl',printpriview_fileurl);
+
+            if (fileuploaded)
+            {
+              org_filename=form99_details_res.filename;
+              org_fileurl=form99_details_res.file;
+              /*form_99_details_res_tmp.id=form_99_details_res.id; //just to get form 99 id
+              localStorage.setItem('id', form_99_details_res_tmp.id);*/
+              localStorage.setItem('orm_99_details.org_filename', org_filename);
+              localStorage.setItem('orm_99_details.org_fileurl', org_fileurl);
+              console.log ('org_filename on Reason screen',org_filename);
+              console.log ('org_fileurl on Reason screen',org_fileurl);
+            }
+
+            return res;
+          }
+          return false;
+      }));
+  }
+  public PreviewForm_Preview_sign_Screen(formObj: any, form_type): Observable<any> {
+    let token: string = JSON.parse(this._cookieService.get('user'));
+    let data: any = {};
+    let httpOptions =  new HttpHeaders();
+    let url: string = '';
+    let id:string;
+    let printpriview_filename = "";
+    let printpriview_fileurl = "";
+    httpOptions = httpOptions.append('Content-Type', 'application/json');
+    httpOptions = httpOptions.append('Authorization', 'JWT ' + token);
+
+    if(form_type === '99') {
+      let form99_details: form99 = JSON.parse(localStorage.getItem('form_99_details'));
+
+      url = '/f99/update_print_f99';
+      data = form99_details;
+
+       data['form_type'] = 'F99';
+
+       console.log('PreviewForm_Preview_Screen form99_details',form99_details);
+       console.log('PreviewForm_Preview_Screen Data',data);
+    }
+    return this._http
+      .post(
+        `${environment.apiUrl}${url}`,
+        data,
+        {
+          headers: httpOptions
+        }
+      )
+      .pipe(map(res => {
+          if (res) {
+            localStorage.setItem('form_99_details_res', JSON.stringify(res));
+            let form99_details_res: form99 = JSON.parse(localStorage.getItem(`form_99_details_res`));
+            id=form99_details_res.id.toString();
+
+            printpriview_filename = form99_details_res.printpriview_filename;
+            printpriview_fileurl = form99_details_res.printpriview_fileurl;
+            
+            localStorage.setItem('form_99_details.id', id);
+            localStorage.setItem('form_99_details.printpriview_filename', printpriview_filename);
+            localStorage.setItem('form_99_details.printpriview_fileurl', printpriview_fileurl);
+
+            console.log ('PreviewForm_Preview_sign_Screen id',identity);
+            console.log ('PreviewForm_Preview_sign_Screen printpriview_filename',printpriview_filename);
+            console.log ('PreviewForm_Preview_sign_Screen printpriview_fileurl',printpriview_fileurl);
+
             return true;
           }
           return false;
