559f66e959460a100599af6585089d62106cfcf8
develop <praveen.jinka@ctr.salientcrgt.com>
2019-01-15 20:04:25 -0800

    FNE_528 base code

--- django-backend/fecfiler/form3x/urls.py
+++ django-backend/fecfiler/form3x/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^f3x/get_filed_report_types$', views.get_filed_report_types, name='get_filed_report_types')
+    url(r'^f3x/get_filed_report_types$', views.get_filed_report_types, name='get_filed_report_types'),
+    url(r'^f3x/get_transaction_categories$', views.get_transaction_categories, name='get_transaction_categories')
 ]
