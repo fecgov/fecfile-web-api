d83c89e1986780ebca177f5abd5c1fc0991382f0
rsanchez-s <41480001+rsanchez-s@users.noreply.github.com>
2019-02-15 12:55:35 -0500

    Qa build (#239)
    * added qa image build.
    
    * fixed syntax error.
    
    * refined jenkins deploy
    
    * Updated django to 1.11.19
    
    * Updated django to 1.11.20

---
+++ front-end/src/environments/environment.awsqa.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  name: 'awsuat',
+  apiUrl: 'https://qa-fecfile-api.efdev.fec.gov/api/v1',
+  validateSuccess: 'All required fields have passed validation',
+  appTitle: 'FECFile Online',
+};
