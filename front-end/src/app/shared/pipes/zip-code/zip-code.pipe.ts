5f17307043edb378f08c73913e28ca8b1f8599e7
Shawn <shawn.mahal@salientcrgt.com>
2019-03-04 18:02:08 -0500

    remove duplicate zip in template

--- front-end/src/app/shared/pipes/zip-code/zip-code.pipe.ts
+++ front-end/src/app/shared/pipes/zip-code/zip-code.pipe.ts
@@ -11,15 +11,15 @@ export class ZipCodePipe implements PipeTransform {
    * @param      {string}  zipCode  The zip code to be formatted
    */
   transform(zipCode: string): string {
-      if (!zipCode) {
-        return '';
-      } else if (zipCode.length > 0 && zipCode.length < 6) {
-        return zipCode;
-      } else if (zipCode.length === 9) {
-        return zipCode.substring(0, 5) + '-' + zipCode.substring(5);
-      } else {
-        return zipCode;
-      }
+    if (!zipCode) {
+      return '';
+    } else if (zipCode.length > 0 && zipCode.length < 6) {
+      return zipCode;
+    } else if (zipCode.length === 9) {
+      return zipCode.substring(0, 5) + '-' + zipCode.substring(5);
+    } else {
+      return zipCode;
+    }
   }
 
 }
