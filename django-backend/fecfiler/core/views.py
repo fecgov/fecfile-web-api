a8acedcd5f02295b3528d766ff4fd26dc3d85e68
jpraveenkmrSalient <41488234+jpraveenkmrSalient@users.noreply.github.com>
2019-03-28 14:52:58 -0400

    Revert "Patch notnull constraints (#348)" (#349)
    This reverts commit 17faf02705fcef99cd684e7627ba649adb83ffc8.

--- django-backend/fecfiler/core/views.py
+++ django-backend/fecfiler/core/views.py
@@ -254,37 +254,6 @@ def check_email(email):
     except:
         raise
 
-def check_mandatory_fields_report(data):
-    try:
-        list_mandatory_fields_report = ['form_type', 'amend_ind', 'cmte_id']
-        error =[]
-        for field in list_mandatory_fields_report:
-            if not(field in data and check_null_value(data.get(field))):
-                error.append(field)
-        if len(error) > 0:
-            string = ""
-            for x in error:
-                string = string + x + ", "
-            string = string[0:-2]
-            raise Exception('The following mandatory fields are required in order to save data to Reports table: {}'.format(string))
-    except:
-        raise
-
-def check_mandatory_fields_form3x(data):
-    try:
-        list_mandatory_fields_form3x = ['form_type', 'amend_ind']
-        error =[]
-        for field in list_mandatory_fields_form3x:
-            if not(field in data and check_null_value(data.get(field))):
-                error.append(field)
-        if len(error) > 0:
-            string = ""
-            for x in error:
-                string = string + x + ", "
-            string = string[0:-2]
-            raise Exception('The following mandatory fields are required in order to save data to Form3x table: {}'.format(string))
-    except:
-        raise
 
 """
 **************************************************** FUNCTIONS - REPORT IDS **********************************************************
@@ -461,7 +430,6 @@ def delete_sql_form3x(report_id, cmte_id):
 """
 def post_reports(data):
     try:
-        check_mandatory_fields_report(data)
         cmte_id = data.get('cmte_id')
         form_type = data.get('form_type')
         cvg_start_dt = data.get('cvg_start_dt')
@@ -484,7 +452,6 @@ def post_reports(data):
             try:
                 #Insert data into Form 3X table
                 if data.get('form_type') == "F3X":
-                    check_mandatory_fields_form3x(data)
                     post_sql_form3x(report_id, data.get('cmte_id'), data.get('form_type'), data.get('amend_ind'), data.get('report_type'), data.get('election_code'), data.get('date_of_election'), data.get('state_of_election'), data.get('cvg_start_dt'), data.get('cvg_end_dt'), data.get('coh_bop'))                                            
                 output = get_reports(data)
             except Exception as e:
@@ -749,22 +716,6 @@ def check_entity_id(entity_id):
         raise Exception('The Entity ID is not in the specified format. Input received: ' + entity_id)
         
 
-def check_mandatory_fields_entity(data):
-    try:
-        list_mandatory_fields_entity = ['entity_type', 'cmte_id']
-        error =[]
-        for field in list_mandatory_fields_entity:
-            if not(field in data and check_null_value(data.get(field))):
-                error.append(field)
-        if len(error) > 0:
-            string = ""
-            for x in error:
-                string = string + x + ", "
-            string = string[0:-2]
-            raise Exception('The following mandatory fields are required in order to save data to Entity table: {}'.format(string))
-    except:
-        raise
-
 def post_sql_entity(entity_id, entity_type, cmte_id, entity_name, first_name, last_name, middle_name, preffix, suffix, street_1, street_2, city, state, zip_code, occupation, employer, ref_cand_cmte_id):
 
     try:
@@ -864,7 +815,6 @@ def remove_sql_entity(entity_id, cmte_id):
 def post_entities(data):
 
     try:
-        check_mandatory_fields_entity(data)
         entity_type = data.get('entity_type')
         check_entity_type(entity_type)
         entity_id = get_next_entity_id(entity_type)
