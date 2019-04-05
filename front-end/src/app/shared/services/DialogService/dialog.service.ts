1fce6cb3a82ebbdb9bf750c9ba68263a8fa98180
MahendraMarathe <44204994+MahendraMarathe@users.noreply.github.com>
2019-04-05 14:04:41 -0400

    FNE 830 hot fix (#369)
    FNE 830 hot fix

--- front-end/src/app/shared/services/DialogService/dialog.service.ts
+++ front-end/src/app/shared/services/DialogService/dialog.service.ts
@@ -14,49 +14,56 @@ export class DialogService {
     private _modalService: NgbModal
   ) { }
 
-  public confirm(message?: string,
-                  modalContent?: any,
-                  modalTitle?: string,
-                  isShowCancel?: boolean,
-                  headerClass?: ModalHeaderClassEnum): Promise<any> {
-    const modalOptions: any = {
-      'backdrop': true,
-      'keyboard': false,
-    };
+public confirm(message?: string,
+    modalContent?: any,
+    modalTitle?: string,
+    isShowCancel?: boolean,
+    headerClass?: ModalHeaderClassEnum,
+    dataMap?: Map<string, any>): Promise<any> {
+const modalOptions: any = {
+'backdrop': true,
+'keyboard': false,
+};
 
-    const modalRef = this._modalService
-      .open(modalContent, modalOptions);
+const modalRef = this._modalService
+.open(modalContent, modalOptions);
 
-    if (message) {
-      modalRef.componentInstance.message = message;
-    }
-    if (modalTitle) {
-      modalRef.componentInstance.modalTitle = modalTitle;
-    }
-    if (isShowCancel !== undefined && isShowCancel !== null) {
-      modalRef.componentInstance.isShowCancel = isShowCancel;
-    }
-    if (headerClass) {
-      modalRef.componentInstance.headerClass = headerClass;
-    }
-    return modalRef
-      .result
-      .then(res => {
-        /**
-         * Returned if a button on the modal is clicked.
-         */
-        return res;
-      }, (res) => {
-        /**
-         * Returned if the modal backdrop or escape is clicked.
-         * Although in this case I have the keyboard disabled.
-         */
-        if (res === ModalDismissReasons.BACKDROP_CLICK || res === ModalDismissReasons.ESC) {
-          return 'cancel';
-        }
-      });
-  }
-  
+if (message) {
+modalRef.componentInstance.message = message;
+}
+if (modalTitle) {
+modalRef.componentInstance.modalTitle = modalTitle;
+}
+if (isShowCancel !== undefined && isShowCancel !== null) {
+modalRef.componentInstance.isShowCancel = isShowCancel;
+}
+if (headerClass) {
+modalRef.componentInstance.headerClass = headerClass;
+}
+if (dataMap) {
+dataMap.forEach((value, key) => {
+const propName = key;
+const propValue = value;
+modalRef.componentInstance[propName] = propValue;
+});
+}
+return modalRef
+.result
+.then(res => {
+/**
+* Returned if a button on the modal is clicked.
+*/
+return res;
+}, (res) => {
+/**
+* Returned if the modal backdrop or escape is clicked.
+* Although in this case I have the keyboard disabled.
+*/
+if (res === ModalDismissReasons.BACKDROP_CLICK || res === ModalDismissReasons.ESC) {
+return 'cancel';
+}
+});
+}
 
   public reportExist(message?: string,
     modalContent?: any,
