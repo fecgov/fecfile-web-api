3ae7f8d48b80a035d798ba18ed051cb73cf7b3b7
MahendraMarathe <44204994+MahendraMarathe@users.noreply.github.com>
2019-05-24 11:07:02 -0400

    FNE 889 changes
    FNE 889 changes

--- django-backend/fecfiler/urls.py
+++ django-backend/fecfiler/urls.py
@@ -1,29 +1,48 @@
-from django.conf.urls import url
-from . import views
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from rest_framework_nested import routers
+from .authentication.views import AccountViewSet, LoginView, LogoutView
+#from .posts.views import AccountPostsViewSet, PostViewSet
+from .views import IndexView
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework_swagger.views import get_swagger_view
+#import fecfiler.form3x.urls as f3xurls
+schema_view = get_swagger_view(title='FEC-Filer API')
 
-# url's for comm_info and get_default_reasons for filing
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
+#router.register(r'posts', PostViewSet)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+#accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = [
-    url(r'^f99/fetch_f99_info$', views.fetch_f99_info, name='fetch_f99_info' ),
-    url(r'^f99/create_f99_info$', views.create_f99_info, name='create_f99_info'),
-    url(r'^f99/update_f99_info$', views.update_f99_info, name='update_f99_info'),
-    url(r'^f99/get_default_reasons$', views.get_f99_reasons, name='get_f99_reasons'),
-    url(r'^f99/submit_comm_info$', views.submit_comm_info, name='submit_comm_info'),
-    url(r'^f99/validate_f99$', views.validate_f99, name='validate_f99'),
-    url(r'^f99/get_signee$', views.get_signee, name='get_signee' ),
-    url(r'^f99/get_rad_analyst_info$', views.get_rad_analyst_info, name='get_rad_analyst_info' ),    
-    url(r'^f99/get_form99list$', views.get_form99list, name='get_form99list' ),
-    #url(r'^forms/get_cmm_lookup$', views.get_comm_lookup, name='get_comm_lookup'),
-    #url(r'^core/get_filed_form_types$', views.get_filed_form_types, name='get_filed_form_types'),
-    #url(r'^f99/print_f99_info$', views.print_f99_info, name='print_f99_info' ),
-    #url(r'^f99/f99_file_upload$', views.f99_file_upload, name='f99_file_upload'),
-    url(r'^f99/delete_forms$', views.delete_forms, name='delete_forms'),
-    url(r'^f99/print_pdf_info$', views.print_pdf_info, name='print_pdf_info'),
-    url(r'^core/get_committee_details$', views.get_committee_details, name='get_committee_details' ),
-    url(r'^core/update_committee_details/(?P<cid>[0-9,a-z,A-Z]+)$', views.update_committee, name='update_committee' ),    
-    url(r'^core/create_committee$', views.create_committee, name='create_committee'),
-    url(r'^f99/save_print_f99$', views.save_print_f99, name='save_print_f99'),
-    url(r'^f99/update_print_f99$', views.update_print_f99, name='update_print_f99'),
-    url(r'^f99/print_pdf$', views.print_pdf, name='print_pdf'),
-    url(r'^f99/get_f99_report_info$', views.get_f99_report_info, name='get_f99_report_info'),
+    #url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin$', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
+    url(r'^api/v1/', include('fecfiler.forms.urls')),
+    url(r'^api/v1/', include('fecfiler.core.urls')),
+    
+    #url(r'^api/v1/', include('fecfiler.form3x.urls')),
+    url(r'^api/v1/', include('fecfiler.sched_A.urls')),
+    url(r'^api/v1/', include('fecfiler.sched_B.urls')),
+    #url(r'^api/v1/auth/login$', csrf_exempt(LoginView.as_view()), name='login'),
+    #url(r'^api/v1/auth/login$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+    #url(r'^api/docs/', include('rest_framework_swagger.urls')),
+    url(r'^api/docs$', schema_view),
+    url(r'^api/v1/token/obtain$', obtain_jwt_token),
+    url(r'^api/v1/token/refresh$', refresh_jwt_token),
+    url(r'^files/', include('db_file_storage.urls')),
+    #url('^.*$', IndexView.as_view(), name='index'),
 ]
+
+#if settings.DEBUG:
+#  urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
