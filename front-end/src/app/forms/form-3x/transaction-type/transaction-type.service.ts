e826ca7cbc094273fb95e4a7745ccbdeaa50a610
Shawn <shawn.mahal@salientcrgt.com>
2019-08-01 06:41:15 -0400

    add cmte type to get transaction categories

--- front-end/src/app/forms/form-3x/transaction-type/transaction-type.service.ts
+++ front-end/src/app/forms/form-3x/transaction-type/transaction-type.service.ts
@@ -4,46 +4,48 @@ import { Observable, identity } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { CookieService } from 'ngx-cookie-service';
 import { environment } from '../../../../environments/environment';
-import { form3x_data, Icommittee_form3x_reporttype, form3XReport} from '../../../shared/interfaces/FormsService/FormsService';
+import {
+  form3x_data,
+  Icommittee_form3x_reporttype,
+  form3XReport
+} from '../../../shared/interfaces/FormsService/FormsService';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransactionTypeService {
-
-  constructor(
-    private _http: HttpClient,
-    private _cookieService: CookieService
-  ) { }
+  constructor(private _http: HttpClient, private _cookieService: CookieService) {}
 
   /**
    * Gets the transaction categories.
    *
    * @param      {string}  formType  The form type
    */
-  public getTransactionCategories( formType: string): Observable<any> {
+  public getTransactionCategories(formType: string): Observable<any> {
     let token: string = JSON.parse(this._cookieService.get('user'));
-    let httpOptions =  new HttpHeaders();
-    let url: string = '';
+    let httpOptions = new HttpHeaders();
+    let url = '';
     let params = new HttpParams();
 
-
-    //url = '/f3x/get_transaction_categories?formType=F3X';
-    url = '/core/get_transaction_categories?form_type=F3X';
+    url = '/core/get_transaction_categories';
 
     httpOptions = httpOptions.append('Content-Type', 'application/json');
     httpOptions = httpOptions.append('Authorization', 'JWT ' + token);
 
     params = params.append('form_type', 'F3X');
 
-    return this._http
-       .get(
-          `${environment.apiUrl}${url}`,
-          {
-           /* headers: httpOptions,
-            params*/
-            headers: httpOptions/*  */
-          }
-       );
+    let cmteTypeCategory = '';
+    if (localStorage.getItem('committee_details') !== null) {
+      const committeeDetails: any = JSON.parse(localStorage.getItem('committee_details'));
+      if (committeeDetails.cmte_type_category !== null) {
+        cmteTypeCategory = committeeDetails.cmte_type_category;
+      }
+    }
+    params = params.append('cmte_type_category', cmteTypeCategory);
+
+    return this._http.get(`${environment.apiUrl}${url}`, {
+      params,
+      headers: httpOptions
+    });
   }
 }
