68ad97b5f667cb96425ff7d2894699d579b560b2
MahendraMarathe <44204994+MahendraMarathe@users.noreply.github.com>
2019-08-01 08:00:28 -0400

    Fne 1144 sprint18 mahendra (#780)
    * FNE  1101 changes
    
    * FNE 1352 changes
    
    * FNE 1262 changes
    
    * FNE 1352 changes
    
    * FNE 1352 changes
    
    * FNE 1262 changes
    
    * FNE 1262 changes

--- front-end/src/app/forms/form-3x/individual-receipt/individual-receipt.component.ts
+++ front-end/src/app/forms/form-3x/individual-receipt/individual-receipt.component.ts
@@ -582,23 +582,8 @@ export class IndividualReceiptComponent implements OnInit {
   }
 
   public printPreview(): void {
-    this._reportTypeService.signandSaveSubmitReport('3X', 'Saved');
-    this._reportTypeService.printPreviewPdf('3X', 'PrintPreviewPDF').subscribe(
-      res => {
-        if (res) {
-          console.log('Accessing FinancialSummaryComponent printPriview res ...', res);
-          if (res.hasOwnProperty('results')) {
-            if (res['results.pdf_url'] !== null) {
-              console.log("res['results.pdf_url'] = ", res['results.pdf_url']);
-              window.open(res.results.pdf_url, '_blank');
-            }
-          }
-        }
-      },
-      error => {
-        console.log('error: ', error);
-      }
-    ); /*  */
+    
+    this._reportTypeService.printPreview(this._formType);
   }
 
   /**
