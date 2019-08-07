3ad991142513942699d24b690ef09f9fccdcab00
qsunctr <qsun.ctr@fec.gov>
2019-08-07 13:51:47 -0400

    adding loading transaction_types from db to sched_b

--- django-backend/fecfiler/sched_B/views.py
+++ django-backend/fecfiler/sched_B/views.py
@@ -26,7 +26,9 @@ from fecfiler.core.views import (
     remove_entities,
     undo_delete_entities,
 )
-
+from fecfiler.sched_A.views import (
+    get_line_number_trans_type,
+)
 # Create your views here.
 logger = logging.getLogger(__name__)
 
@@ -45,9 +47,8 @@ list_mandatory_fields_schedB = [
     "report_id",
     "expenditure_date",
     "expenditure_amount",
-    "cmte_id",
-    "line_number",
-    "transaction_type",
+    "transaction_type_identifier",
+    "entity_type"
 ]
 # list_mandatory_fields_aggregate = ['transaction_type']
 # list_child_schedA = ['16']
@@ -681,6 +682,10 @@ def schedB_sql_dict(data):
             "back_ref_transaction_id": data.get("back_ref_transaction_id"),
 
         }
+        if 'entity_id' in data and check_null_value(data.get('entity_id')):
+            datum['entity_id'] = data.get('entity_id')
+        datum['line_number'], datum['transaction_type'] = get_line_number_trans_type(
+            data.get('transaction_type_identifier'))
         return datum
     except:
         raise
