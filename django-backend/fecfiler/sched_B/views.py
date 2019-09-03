60d21a2e08b57a5b48dd562f53188cdeb93a10e1
qsunctr <qsun.ctr@fec.gov>
2019-09-03 12:17:29 -0400

    update on sched_b aggregate_amt and semi_annual_refund_bundled_amount

--- django-backend/fecfiler/sched_B/views.py
+++ django-backend/fecfiler/sched_B/views.py
@@ -589,7 +589,8 @@ def post_schedB(datum):
         else:
             entity_data = post_entities(datum)
         logger.debug("...entity saved")
-        if "beneficiary_cand_entity_id" in datum:
+        logger.debug("***datum:{}".format(datum))
+        if datum.get("beneficiary_cand_entity_id"):
             logger.debug("saving cand data...")
             # get_data = {
             #     "cmte_id": datum.get("cmte_id"),
@@ -718,15 +719,15 @@ def put_schedB(datum):
             entity_data = put_entities(datum)
         else:
             entity_data = post_entities(datum)
-        if "beneficiary_cand_entity_id" in datum:
+        if datum.get("beneficiary_cand_entity_id"):
             # get_data = {
             #     "cmte_id": datum.get("cmte_id"),
             #     "entity_id": datum.get("entity_id"),
             # }
             # prev_entity_list = get_entities(get_data)
             cand_data = put_cand_entity(datum)
-        else:
-            cand_data = post_cand_entity(datum)
+        # else:
+        #     cand_data = post_cand_entity(datum)
         entity_id = entity_data.get("entity_id")
         datum["entity_id"] = entity_id
         datum["beneficiary_cand_entity_id"] = cand_data.get("entity_id")
@@ -851,9 +852,9 @@ def schedB_sql_dict(data):
             "back_ref_sched_name": data.get("back_ref_sched_name"),
             "expenditure_date": date_format(data.get("expenditure_date")),
             "expenditure_amount": check_decimal(data.get("expenditure_amount", None)),
-            "semi_annual_refund_bundled_amount": check_decimal(
-                data.get("semi_annual_refund_bundled_amount", 0)
-            ),
+            # "semi_annual_refund_bundled_amount": check_decimal(
+            #     data.get("semi_annual_refund_bundled_amount", 0)
+            # ),
             "expenditure_purpose": data.get("expenditure_purpose"),
             "category_code": data.get("category_code"),
             "memo_code": data.get("memo_code"),
@@ -879,7 +880,7 @@ def schedB_sql_dict(data):
             "beneficiary_cand_middle_name": data.get("beneficiary_cand_middle_name"),
             "beneficiary_cand_prefix": data.get("beneficiary_cand_prefix"),
             "beneficiary_cand_suffix": data.get("beneficiary_cand_suffix"),
-            "aggregate_amt": check_decimal(data.get("aggregate_amt", None)),
+            # "aggregate_amt": check_decimal(data.get("aggregate_amt", None)),
             "entity_type": data.get("entity_type"),
             "entity_name": data.get("entity_name"),
             "first_name": data.get("first_name"),
@@ -896,7 +897,30 @@ def schedB_sql_dict(data):
             "employer": data.get("employer"),
             "ref_cand_cmte_id": data.get("ref_cand_cmte_id"),
             "back_ref_transaction_id": data.get("back_ref_transaction_id"),
+            # cand_entity_data
+            "cand_office": data.get("cand_office"),
+            "cand_office_state": data.get("cand_office_state"),
+            "cand_office_district": data.get("cand_office_district"),
+            # "beneficiary_cmte_name": data.get("beneficiary_cmte_name"),
+            "cand_last_name": data.get("cand_last_name"),
+            "cand_first_name": data.get("cand_first_name"),
+            "cand_middle_name": data.get("cand_middle_name"),
+            "cand_prefix": data.get("cand_prefix"),
+            "cand_suffix": data.get("cand_suffix"),
+            "cand_street_1": data.get("cand_street_1"),
+            "cand_street_2": data.get("cand_street_2"),
+            "cand_city": data.get("cand_city"),
+            "cand_state": data.get("cand_state"),
+            "cand_zip_code": data.get("cand_zip_code"),
         }
+        if "aggregate_amt" in data and check_decimal(data.get("aggregate_amt")):
+            datum["aggregate_amt"] = data.get("aggregate_amt")
+        if "semi_annual_refund_bundled_amount" in data and check_decimal(
+            data.get("semi_annual_refund_bundled_amount")
+        ):
+            datum["semi_annual_refund_bundled_amount"] = data.get(
+                "semi_annual_refund_bundled_amount"
+            )
         if "entity_id" in data and check_null_value(data.get("entity_id")):
             datum["entity_id"] = data.get("entity_id")
         datum["line_number"], datum["transaction_type"] = get_line_number_trans_type(
