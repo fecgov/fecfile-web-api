c2cb0c3a54f0bbdf1a253521dc9c0b9568f14e30
Gowtham Venkatachalam <gowtham.v.91@gmail.com>
2019-09-12 18:07:01 -0400

    Adding typeahead for search by type

--- front-end/src/app/forms/form-3x/transaction-sidebar/transaction-sidebar.component.ts
+++ front-end/src/app/forms/form-3x/transaction-sidebar/transaction-sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
@@ -9,6 +9,12 @@ import { DialogService } from '../../../shared/services/DialogService/dialog.ser
 import { ConfirmModalComponent } from '../../../shared/partials/confirm-modal/confirm-modal.component';
 import { TransactionTypeService } from '../../../forms/form-3x/transaction-type/transaction-type.service';
 import { CashOnHandModel } from '../../transactions/model/cashOnHand.model';
+import {Observable, Subject} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map, merge} from 'rxjs/operators';
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import { TypeaheadService } from 'src/app/shared/partials/typeahead/typeahead.service';
+
+const transactionCategoryOptions = [];
 
 @Component({
   selector: 'transaction-sidebar',
@@ -23,7 +29,11 @@ export class TransactionSidebarComponent implements OnInit {
   @Input() step: string = '';
 
   public itemSelected: string = '';
+  public typeaheadValue: string = '';
   public receiptsTotal: number = 0.0;
+  public frmOption: FormGroup;
+  public transactionType: string = null;
+  public transactionTypeText: string = null;
   public cashOnHandTotal: number = 0.0;
   public disbursementsTotal: number = 0.0;
   public loansanddebtsTotal: number = 0.0;
@@ -31,6 +41,11 @@ export class TransactionSidebarComponent implements OnInit {
 
   private _formType: string = '';
   public transactionCategory: string = '';
+  _mainTransactionCategory: any;
+  secondaryOptions: any;
+  transactionCategorySelected: boolean;
+  transactionTypeFailed: boolean;
+  tranasctionCategoryVal: string;
 
   public cashOnHand: CashOnHandModel;
 
@@ -42,11 +57,16 @@ export class TransactionSidebarComponent implements OnInit {
     private _messageService: MessageService,
     private _router: Router,
     private _formsService: FormsService,
-    private _dialogService: DialogService
+    private _dialogService: DialogService,
+    private _typeaheadService: TypeaheadService
   ) {
     this._config.placement = 'right';
     this._config.triggers = 'click';
   }
+
+  @ViewChild('instance') instance: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
 
   ngOnInit(): void {
     this._formType = this._activatedRoute.snapshot.paramMap.get('form_id');
@@ -54,6 +74,21 @@ export class TransactionSidebarComponent implements OnInit {
       if (res) {
         this.transactionCategories = res.data.transactionCategories;
         this.cashOnHand = res.data.cashOnHand;
+      }
+      for (let transactionCategorieIndex = 0; transactionCategorieIndex < this.transactionCategories.length; transactionCategorieIndex++) {
+        for (let transactionCategoryOptionIndex = 0;
+          transactionCategoryOptionIndex <
+          this.transactionCategories[transactionCategorieIndex].options.length; transactionCategoryOptionIndex++) {
+            for (let transactionCategoryOptionOptionsIndex = 0;
+              transactionCategoryOptionOptionsIndex <
+              this.transactionCategories[transactionCategorieIndex].options[transactionCategoryOptionIndex].options.length;
+              transactionCategoryOptionOptionsIndex++) {
+              transactionCategoryOptions.push(
+                this.transactionCategories[transactionCategorieIndex]
+                  .options[transactionCategoryOptionIndex].options[transactionCategoryOptionOptionsIndex]
+              );
+            }
+        }
       }
     });
 
@@ -106,9 +141,22 @@ export class TransactionSidebarComponent implements OnInit {
             }
           }
         }
+
+        if (this.transactionCategory && localStorage.getItem(`form_${this._formType}_temp_transaction_type`) === null) {
+          this._setSecondaryTransactionCategories();
+        } else if (
+          this.transactionCategory &&
+          localStorage.getItem(`form_${this._formType}_temp_transaction_type`) !== null
+        ) {
+          const transactionObj: any = JSON.parse(localStorage.getItem(`form_${this._formType}_temp_transaction_type`));
+
+          if (transactionObj.mainTransactionTypeText !== this.transactionCategory) {
+            this._setSecondaryTransactionCategories();
+          }
+        }
       }
     });
-    
+
     if (this.receiptsTotal === 0 && localStorage.getItem(`form_${this._formType}_totals`) !== null) {
       const totals: any = JSON.parse(localStorage.getItem(`form_${this._formType}_totals`));
 
@@ -129,6 +177,46 @@ export class TransactionSidebarComponent implements OnInit {
   ngOnDestroy(): void {
     this._messageService.clearMessage();
   }
+
+    /**
+   * Sets the secondary transaction categories.
+   */
+  private _setSecondaryTransactionCategories(): void {
+    this._mainTransactionCategory = this.transactionCategories.filter(el => el.value === this.transactionCategory);
+    const mainTransactionTypeText: string = this._mainTransactionCategory[0].text;
+    const mainTransactionTypeValue: string = this._mainTransactionCategory[0].value;
+    const transactionObj: any = {
+      mainTransactionTypeText,
+      mainTransactionTypeValue,
+      transactionType: '',
+      childTransactionType: ''
+    };
+
+    localStorage.setItem(`form_${this._formType}_temp_transaction_type`, JSON.stringify(transactionObj));
+
+    this.secondaryOptions = this._mainTransactionCategory[0].options;
+    this.transactionCategorySelected = true;
+
+    this.transactionTypeFailed = false;
+
+    if (this.transactionCategory) {
+      this.tranasctionCategoryVal = this.transactionCategory;
+
+      this.transactionCategory = '';
+    }
+  }
+
+  searchByType = (text$: Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      merge(this.focus$),
+      merge(this.click$.pipe(filter(() => !this.instance.isPopupOpen()))),
+      map(term => (term === '' ? transactionCategoryOptions
+        : transactionCategoryOptions.filter(v => v.text.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+    )
+
+  formatter = (x: { text: string }) => x.text;
 
   /**
    * Sets the selected item.
@@ -159,6 +247,50 @@ export class TransactionSidebarComponent implements OnInit {
     }
   }
 
+  public selectedTypeAheadValue(e): void {
+
+    if (e) {
+      for (let transactionCategorieIndex = 0;
+        transactionCategorieIndex < this.transactionCategories.length; transactionCategorieIndex++) {
+        for (let transactionCategoryOptionIndex = 0;
+          transactionCategoryOptionIndex <
+          this.transactionCategories[transactionCategorieIndex].options.length; transactionCategoryOptionIndex++) {
+          for (let transactionCategoryOptionOptionsIndex = 0;
+            transactionCategoryOptionOptionsIndex <
+            this.transactionCategories[transactionCategorieIndex].options[transactionCategoryOptionIndex].options.length;
+            transactionCategoryOptionOptionsIndex++) {
+            if (e.text === this.transactionCategories[transactionCategorieIndex]
+              .options[transactionCategoryOptionIndex].options[transactionCategoryOptionOptionsIndex].text) {
+              this.itemSelected = this.transactionCategories[transactionCategorieIndex].value;
+              this.status.emit({
+                form: this._formType,
+                transactionCategory: this.transactionCategories[transactionCategorieIndex].value
+              });
+
+              this._messageService.sendMessage({
+                form: this._formType,
+                transactionCategory: this.transactionCategories[transactionCategorieIndex].value
+              });
+              if (
+                localStorage.getItem('Transaction_Table_Screen') === 'Yes' ||
+                localStorage.getItem('Summary_Screen') === 'Yes' ||
+                localStorage.getItem('Receipts_Entry_Screen') === 'Yes'
+              ) {
+                this._router.navigate([`/forms/form/${this._formType}`], {
+                  queryParams: { 
+                    step: 'step_2',
+                    transactionCategory: this.transactionCategories[transactionCategorieIndex].value
+                  }
+                });
+              }
+              break;
+            }
+          }
+        }
+      }
+    }
+  }
+
   public viewSummary(): void {
 
     localStorage.setItem('Summary_Screen', 'Yes');
@@ -168,6 +300,17 @@ export class TransactionSidebarComponent implements OnInit {
 
   public signandSubmit(): void {
     this._router.navigate([`/signandSubmit/${this._formType}`]);
+  }
+
+  public updateTypeSelected(e): void {
+    const val: string = e.target.value;
+
+    this.transactionType = val;
+    this.transactionTypeText = e.target.value.text;
+
+    this.frmOption.controls['secondaryOption'].setValue(val);
+
+    this.transactionTypeFailed = false;
   }
 
   /**
