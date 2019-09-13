e3d10d67f6d67d126e855ed9dc97847de4e16c5a
Roberto Sanchez <rsanchez@rhythmictech.com>
2019-09-13 14:52:52 -0400

    Added usersnap to frontend.

--- front-end/src/main.ts
+++ front-end/src/main.ts
@@ -10,3 +10,5 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
+
+document.write('<script type="text/javascript"> (function() { var s = document.createElement("script"); s.type = "text/javascript"; s.async = true; s.src = \'//api.usersnap.com/load/55f4a866-178e-4cf4-b4b4-58457fdca2bd.js\'; var x = document.getElementsByTagName(\'script\')[0]; x.parentNode.insertBefore(s, x); })(); </script>')
