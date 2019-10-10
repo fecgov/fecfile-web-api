ead44eef9cdd76dc0dc72036b1698707548e1043
william <williamhehk@gmail.com>
2019-10-10 17:44:17 -0400

    add amend post headers

--- front-end/src/app/reports/service/report.service.ts
+++ front-end/src/app/reports/service/report.service.ts
@@ -528,6 +528,7 @@ private getDateMMDDYYYYformat(dateValue: Date): string {
     let httpOptions = new HttpHeaders();
     const url = '/core/create_amended_reports';
     
+    httpOptions = httpOptions.append('Content-Type', 'application/json');
     httpOptions = httpOptions.append('Authorization', 'JWT ' + token);
 
     const formData: FormData = new FormData();
