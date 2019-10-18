20dc85962a596178824d6aff69a25797119f4c13
qsunctr <qsun.ctr@fec.gov>
2019-10-18 16:25:47 -0400

    adding sched_c api for getting all outstanding loans

--- django-backend/fecfiler/sched_C/urls.py
+++ django-backend/fecfiler/sched_C/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^sc/schedC$', views.schedC, name='schedC'),
-    url(r'^sc/schedC1$', views.schedC1, name='schedC1'),
-    url(r'^sc/schedC2$', views.schedC2, name='schedC2'),
+    url(r"^sc/schedC$", views.schedC, name="schedC"),
+    url(
+        r"^sc/get_outstanding_loans$",
+        views.get_outstanding_loans,
+        name="get_outstanding_loans",
+    ),
+    url(r"^sc/schedC1$", views.schedC1, name="schedC1"),
+    url(r"^sc/schedC2$", views.schedC2, name="schedC2"),
 ]
