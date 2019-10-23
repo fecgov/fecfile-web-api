21e6de7055d5db6108dae6f4e08eea49554a6123
Shawn <shawn.mahal@salientcrgt.com>
2019-10-23 11:35:16 -0400

    h1/h2 component placeholder support

--- front-end/src/app/forms/form-3x/individual-receipt/abstract-schedule.ts
+++ front-end/src/app/forms/form-3x/individual-receipt/abstract-schedule.ts
@@ -755,13 +755,15 @@ export abstract class AbstractSchedule implements OnInit, OnDestroy, OnChanges {
     this._receiptService.getFedNonFedPercentage(totalAmount, activityEvent).subscribe(res => {
 
       // TODO remove this if H1/H2 not found goes back to retuning a non-200 http code.
-      if (res.toLowerCase().includes('no h1 data found') ||
-        res.toLowerCase().includes('no h2 data found') ||
-        res.toLowerCase().includes('no valid h1 data found') ||
-        res.toLowerCase().includes('no valid h2 data found')
-      ) {
-        this._handleNoH1H2(res);
-        return;
+      if (typeof res === 'string') {
+        if (res.toLowerCase().includes('no h1 data found') ||
+          res.toLowerCase().includes('no h2 data found') ||
+          res.toLowerCase().includes('no valid h1 data found') ||
+          res.toLowerCase().includes('no valid h2 data found')
+        ) {
+          this._handleNoH1H2(res);
+          return;
+        }
       }
 
       if (res) {
@@ -816,6 +818,7 @@ export abstract class AbstractSchedule implements OnInit, OnDestroy, OnChanges {
     // Hard Coding DF/DC  => H2 for now.  Once dynamic forms provides schedule in activityEventTypes
     const activityEventType = this.frmIndividualReceipt.get('activity_event_type').value;
     const scheduleName = (activityEventType === 'DF' || activityEventType === 'DC') ? 'H2' : 'H1';
+    const scheduleType = (activityEventType === 'DF' || activityEventType === 'DC') ? 'sched_h2' : 'sched_h1';
 
     // TODO decide on a specific code or message for not found.
     // Handling various versions from API until then.
@@ -829,13 +832,23 @@ export abstract class AbstractSchedule implements OnInit, OnDestroy, OnChanges {
       this._dialogService.confirm(message, ConfirmModalComponent, 'Warning!', false).then(res => {
         if (res === 'okay') {
 
-          // TODO this proves a new sched can be viewed.  Need to go to h1 or h2
-          // based on the event type selected.
-          this.clearFormValues();
-          this.scheduleType = 'sched_h6';
-          this.transactionType = 'ALLOC_FEA_DISB_DEBT';
-          this.transactionTypeText = 'Allocated FEA Debt Payment (H6 Test until H1/H2 is ready!)';
-          window.scrollTo(0, 0);
+          // // TODO this proves a new sched can be viewed.  Need to go to h1 or h2
+          // // based on the event type selected.
+          // this.clearFormValues();
+          // this.scheduleType = 'sched_h6';
+          // this.transactionType = 'ALLOC_FEA_DISB_DEBT';
+          // this.transactionTypeText = 'Allocated FEA Debt Payment (H6 Test until H1/H2 is ready!)';
+          // window.scrollTo(0, 0);
+
+          const emitObj: any = {
+            form: this.frmIndividualReceipt,
+            direction: 'next',
+            step: 'step_3',
+            previousStep: 'step_2',
+            scheduleType: scheduleType,
+            action: ScheduleActions.add
+          };
+          this.status.emit(emitObj);
         }
       });
     }
@@ -1580,12 +1593,7 @@ export abstract class AbstractSchedule implements OnInit, OnDestroy, OnChanges {
               scheduleType: this.subTransactionInfo.subScheduleType,
               action: ScheduleActions.addSubTransaction
             };
-            // let prePopulateFieldArray = null;
-            // if (this.scheduleType = 'sched_d') {
-            //   prePopulateFieldArray = this._checkForSchedDPrePopulate();
-            // } else {
-            //   prePopulateFieldArray = this._checkForEarmarkPurposePrePopulate(res);
-            // }
+
             const prePopulateFieldArray = this._checkForEarmarkPurposePrePopulate(res);
             if (prePopulateFieldArray) {
               addSubTransEmitObj.prePopulateFieldArray = prePopulateFieldArray;
