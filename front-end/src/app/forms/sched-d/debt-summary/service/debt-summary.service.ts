539e173d4ae49fb5ada6a07eddc052c511940099
Shawn <shawn.mahal@salientcrgt.com>
2019-12-09 06:35:11 -0500

    debt summary

---
+++ front-end/src/app/forms/sched-d/debt-summary/service/debt-summary.service.ts
@@ -0,0 +1,126 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
+import { OrderByPipe } from 'src/app/shared/pipes/order-by/order-by.pipe';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { ReportTypeService } from 'src/app/forms/form-3x/report-type/report-type.service';
+import { DatePipe } from '@angular/common';
+import { DebtSummaryModel } from '../model/debt-summary.model';
+
+/**
+ * The service for Debt Summary
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class DebtSummaryService {
+  constructor(
+    private _http: HttpClient,
+    private _cookieService: CookieService,
+    private _datePipe: DatePipe,
+    private _orderByPipe: OrderByPipe,
+    private _reportTypeService: ReportTypeService
+  ) {}
+
+  /**
+   * Get the Debts comprising the Debt Summary.
+   */
+  public getDebts(): Observable<any> {
+    const token: string = JSON.parse(this._cookieService.get('user'));
+    let httpOptions = new HttpHeaders();
+    const url = '/sd/schedD';
+    const reportId: string = this._reportTypeService.getReportIdFromStorage('3X').toString();
+
+    httpOptions = httpOptions.append('Content-Type', 'application/json');
+    httpOptions = httpOptions.append('Authorization', 'JWT ' + token);
+
+    let params = new HttpParams();
+    params = params.append('report_id', reportId);
+
+    return this._http
+      .get(`${environment.apiUrl}${url}`, {
+        headers: httpOptions,
+        params
+      })
+      .pipe(
+        map(res => {
+          if (res) {
+            return res;
+          }
+          return false;
+        })
+      );
+  }
+
+  public deleteDebt(debt: DebtSummaryModel): Observable<any> {
+    const token: string = JSON.parse(this._cookieService.get('user'));
+    let httpOptions = new HttpHeaders();
+    const url = '/sd/schedD';
+    const reportId: string = this._reportTypeService.getReportIdFromStorage('3X').toString();
+
+    httpOptions = httpOptions.append('Content-Type', 'application/json');
+    httpOptions = httpOptions.append('Authorization', 'JWT ' + token);
+
+    let params = new HttpParams();
+    params = params.append('transaction_id', debt.transactionId);
+    params = params.append('report_id', reportId);
+
+    return this._http
+      .delete(`${environment.apiUrl}${url}`, {
+        params,
+        headers: httpOptions
+      })
+      .pipe(
+        map(res => {
+          if (res) {
+            console.log('get_outstanding_loans API res: ', res);
+
+            return res;
+          }
+          return false;
+        })
+      );
+  }
+
+  /**
+   * Sort Debt summary items.
+   *
+   * @param array
+   * @param sortColumnName
+   * @param descending
+   */
+  public sortDebtSummary(array: any, sortColumnName: string, descending: boolean) {
+    const direction = descending ? -1 : 1;
+    this._orderByPipe.transform(array, { property: sortColumnName, direction: direction });
+    return array;
+  }
+
+  /**
+   * Map server fields from the response to the model.
+   *
+   * TODO The API should be changed to pass the property names expected by the front end.
+   */
+  public mapFromServerFields(serverData: any) {
+    if (!serverData || !Array.isArray(serverData)) {
+      return;
+    }
+    const modelArray = [];
+    for (const row of serverData) {
+      const model = new DebtSummaryModel({});
+      model.transactionTypeIdentifier = row.transaction_type_identifier;
+      model.transactionId = row.transaction_id;
+      model.backRefTransactionId = row.back_ref_transaction_id;
+      model.child = row.child;
+      model.entityType = row.entity_type;
+      model.name = row.name;
+      model.beginningBalance = row.beginning_balance ? row.beginning_balance : 0;
+      model.incurredAmt = row.incurred_amount ? row.incurred_amount : 0;
+      model.paymentAmt = row.payment_amount ? row.payment_amount : 0;
+      model.closingBalance = row.balance_at_close ? row.balance_at_close : 0;
+      modelArray.push(model);
+    }
+    return modelArray;
+  }
+}
