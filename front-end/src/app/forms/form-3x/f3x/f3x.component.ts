16632a123b436e1a0faa23101abb48408110efc6
Zobair <zsaleem@halvik.com>
2020-01-08 11:05:17 -0500

    schedE initial commit

--- front-end/src/app/forms/form-3x/f3x/f3x.component.ts
+++ front-end/src/app/forms/form-3x/f3x/f3x.component.ts
@@ -498,6 +498,13 @@ export class F3xComponent implements OnInit {
       e.scheduleType = 'sched_c_loan_payment';
     }
 
+    //TODO-Remove this elseif once transactions are moved to Disubursements
+    if (
+      e.schedulueType = 'Schedule E'
+    ) {
+      e.scheduleType = 'sched_e';
+    }
+
     //default to sched_a ?
     this.scheduleType = e.scheduleType ? e.scheduleType : 'sched_a';
 
