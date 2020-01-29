c192a00f1cd541634325a5e04ad4a0571825067f
qsunctr <qsun.ctr@fec.gov>
2020-01-29 15:36:48 -0500

    update sched_l aggregate on LA contributions.

--- django-backend/fecfiler/core/aggregation_helper.py
+++ django-backend/fecfiler/core/aggregation_helper.py
@@ -20,6 +20,235 @@ def date_format(cvg_date):
         raise
 
 
+def find_form_type(report_id, cmte_id):
+    """
+    load form type based on report_id and cmte_id
+    """
+    try:
+        # handling cases where report_id is reported as 0
+        if report_id in ["0", "0", 0]:
+            return "F3X"
+        # end of error handling
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """SELECT form_type FROM public.reports WHERE report_id = %s AND cmte_id = %s AND delete_ind is distinct from 'Y'""",
+                [report_id, cmte_id],
+            )
+            form_types = cursor.fetchone()
+        if cursor.rowcount == 0:
+            raise Exception(
+                "The Report ID: {} is either already deleted or does not exist in reports table".format(
+                    report_id
+                )
+            )
+        else:
+            return form_types[0]
+    except Exception as e:
+        raise Exception("The form_type function is throwing an error:" + str(e))
+
+
+def list_all_transactions_entity_sl(
+    aggregate_start_date, aggregate_end_date, entity_id, cmte_id, levin_account_id
+):
+    """
+    load all transactions for an entity within a time window
+    return value: a list of transction_records [
+       (contribution_amount, transaction_id, report_id, line_number, contribution_date),
+       ....
+    ]
+    return items are sorted by contribution_date in ASC order
+    """
+    logger.debug(
+        "fetching transactions for entity {} and levin acct {}".format(
+            entity_id, levin_account_id
+        )
+    )
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """
+            SELECT t1.contribution_amount, 
+                t1.transaction_id, 
+                t1.report_id, 
+                t1.line_number, 
+                t1.contribution_date, 
+                (SELECT t2.delete_ind FROM public.reports t2 WHERE t2.report_id = t1.report_id), 
+                t1.memo_code, 
+                t1.back_ref_transaction_id,
+                t1.transaction_type_identifier
+            FROM public.sched_a t1 
+            WHERE entity_id = %s 
+            AND cmte_id = %s 
+            AND contribution_date >= %s 
+            AND contribution_date <= %s
+            AND levin_account_id = %s 
+            AND delete_ind is distinct FROM 'Y' 
+            ORDER BY contribution_date ASC, create_date ASC
+            """,
+                [
+                    entity_id,
+                    cmte_id,
+                    aggregate_start_date,
+                    aggregate_end_date,
+                    levin_account_id,
+                ],
+            )
+            transactions_list = cursor.fetchall()
+        logger.debug("transaction list loaded.")
+        return transactions_list
+    except Exception as e:
+        raise Exception(
+            "The list_all_transactions_entity function is throwing an error: " + str(e)
+        )
+
+
+def put_sql_agg_amount_schedA(cmte_id, transaction_id, aggregate_amount):
+    """
+    update aggregate amount
+    """
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                """UPDATE public.sched_a SET aggregate_amt = %s WHERE transaction_id = %s AND cmte_id = %s AND delete_ind is distinct from 'Y'""",
+                [aggregate_amount, transaction_id, cmte_id],
+            )
+            if cursor.rowcount == 0:
+                raise Exception(
+                    "put_sql_agg_amount_schedA function: The Transaction ID: {} does not exist in schedA table".format(
+                        transaction_id
+                    )
+                )
+        logger.debug(
+            "***put_sql_agg_amount_schedA done with:{}".format(aggregate_amount)
+        )
+    except Exception:
+        raise
+
+
+def update_aggregate_sl(datum):
+    """
+    helper function for updating private contribution line number based on aggrgate amount
+    the aggregate amount is a contribution_date-based incremental update, the line number
+    is updated accordingly:
+    edit 1: check if the report corresponding to the transaction is deleted or not (delete_ind flag in reports table) 
+            and only when it is NOT add contribution_amount to aggregate amount
+    edit 2: updation of aggregate amount will roll to all transacctions irrespective of report id and report being filed
+    edit 3: if back_ref_transaction_id is none, then check if memo_code is NOT 'X' and if it is not, add contribution_amount to aggregate amount; 
+            if back_ref_transaction_id is NOT none, then add irrespective of memo_code, add contribution_amount to aggregate amount
+    e.g.
+    date, contribution_amount, aggregate_amount, line_number
+    1/1/2018, 50, 50
+    2/1/2018, 60, 110
+    3/1/2018, 100, 210 (aggregate_amount > 200, transaction becomes itemized)
+    """
+    logger.debug("update sla aggregate with data {}".format(datum))
+    try:
+        # logger("update sla aggregate with data {}".format(datum))
+        cmte_id = datum.get("cmte_id")
+        report_id = datum.get("report_id")
+        entity_id = datum.get("entity_id")
+        levin_account_id = datum.get("levin_account_id")
+        contribution_date = datum.get("contribution_date")
+        child_flag_SB = False
+        child_flag_SA = False
+        itemization_value = 200
+        # itemized_transaction_list = []
+        # unitemized_transaction_list = []
+        form_type = find_form_type(report_id, cmte_id)
+        if isinstance(contribution_date, str):
+            contribution_date = date_format(contribution_date)
+        aggregate_start_date, aggregate_end_date = find_aggregate_date(
+            form_type, contribution_date
+        )
+        # checking for child tranaction identifer for updating auto generated SB transactions
+        # if transaction_type_identifier in AUTO_GENERATE_SCHEDB_PARENT_CHILD_TRANSTYPE_DICT:
+        #     child_flag_SB = True
+        #     child_transaction_type_identifier = AUTO_GENERATE_SCHEDB_PARENT_CHILD_TRANSTYPE_DICT.get(transaction_type_identifier)
+        # checking for child tranaction identifer for updating auto generated SA transactions
+        # if transaction_type_identifier in AUTO_GENERATE_SCHEDA_PARENT_CHILD_TRANSTYPE_DICT:
+        #     child_flag_SA = True
+        #     child_transaction_type_identifier = AUTO_GENERATE_SCHEDA_PARENT_CHILD_TRANSTYPE_DICT.get(transaction_type_identifier)
+        # make sure transaction list comes back sorted by contribution_date ASC
+        transactions_list = list_all_transactions_entity_sl(
+            aggregate_start_date,
+            aggregate_end_date,
+            entity_id,
+            cmte_id,
+            levin_account_id,
+        )
+        print("transaction list:{}".format(transactions_list))
+        # put field here
+        #    for quick reference
+        #      SELECT t1.contribution_amount, 0
+        # t1.transaction_id, 1
+        # t1.report_id, 2
+        # t1.line_number, 3
+        # t1.contribution_date, 4
+        # (SELECT t2.delete_ind FROM public.reports t2 WHERE t2.report_id = t1.report_id), 5
+        # t1.memo_code, 6
+        # t1.back_ref_transaction_id,  7
+        # t1.transaction_type_identifier  8
+        aggregate_amount = 0
+        # PAC_aggregate_amount = 0
+        # HQ_aggregate_amount = 0
+        # CO_aggregate_amount = 0
+        # NPRE_aggregate_amount = 0
+        # RE_aggregate_amount = 0
+        # REMAIN_aggregate_amount = 0
+        # committee_type = cmte_type(cmte_id)
+        for transaction in transactions_list:
+            # checking in reports table if the delete_ind flag is false for the corresponding report
+            if transaction[5] != "Y":
+                # checking if the back_ref_transaction_id is null or not.
+                # If back_ref_transaction_id is none, checking if the transaction is a memo or not, using memo_code not equal to X.
+                if transaction[7] != None or (
+                    transaction[7] == None and transaction[6] != "X"
+                ):
+                    aggregate_amount += transaction[0]
+
+                    # if (committee_type == 'PAC') and transaction[8] in PAC_AGGREGATE_TYPES_1:
+                    #     PAC_aggregate_amount += transaction[0]
+                    #     aggregate_amount = PAC_aggregate_amount
+                    # elif (committee_type == 'PTY') and transaction[8] in PTY_AGGREGATE_TYPES_HQ:
+                    #     HQ_aggregate_amount += transaction[0]
+                    #     aggregate_amount = HQ_aggregate_amount
+                    # elif (committee_type == 'PTY') and transaction[8] in PTY_AGGREGATE_TYPES_CO:
+                    #     CO_aggregate_amount += transaction[0]
+                    #     aggregate_amount = CO_aggregate_amount
+                    # elif (committee_type == 'PTY') and transaction[8] in PTY_AGGREGATE_TYPES_NPRE:
+                    #     NPRE_aggregate_amount += transaction[0]
+                    #     aggregate_amount = NPRE_aggregate_amount
+                    # elif (committee_type == 'PTY') and transaction[8] in PTY_AGGREGATE_TYPES_RE:
+                    #     RE_aggregate_amount += transaction[0]
+                    #     aggregate_amount = RE_aggregate_amount
+                    # else:
+                    #     REMAIN_aggregate_amount += transaction[0]
+                    #     aggregate_amount = REMAIN_aggregate_amount
+                # Removed report_id constraint as we have to modify aggregate amount irrespective of report_id
+                # if str(report_id) == str(transaction[2]):
+                if contribution_date <= transaction[4]:
+                    transaction_id = transaction[1]
+                    # line_number,itemized_ind = get_linenumber_itemization(transaction[8], aggregate_amount, itemization_value, transaction[3])
+                    # put_sql_agg_amount_sl(cmte_id, transaction_id, aggregate_amount)
+                    put_sql_agg_amount_schedA(cmte_id, transaction_id, aggregate_amount)
+
+                # Updating aggregate amount to child auto generate sched A transactions
+                # if child_flag_SA:
+                #     child_SA_transaction_list = get_list_child_schedA(report_id, cmte_id, transaction[1])
+                #     for child_SA_transaction in child_SA_transaction_list:
+                #         put_sql_agg_amount_schedA(cmte_id, child_SA_transaction.get('transaction_id'), aggregate_amount)
+                # #Updating aggregate amount to child auto generate sched B transactions
+                # if child_flag_SB:
+                #     child_SB_transaction_list = get_list_child_transactionId_schedB(cmte_id, transaction[1])
+                #     for child_SB_transaction in child_SB_transaction_list:
+                #         put_sql_agg_amount_schedB(cmte_id, child_SB_transaction[0], aggregate_amount)
+
+    except Exception as e:
+        raise Exception(
+            "The update_aggregate_sl function is throwing an error: " + str(e)
+        )
+
+
 def find_aggregate_date(form_type, contribution_date):
     """
     calculate aggregate start, end dates
