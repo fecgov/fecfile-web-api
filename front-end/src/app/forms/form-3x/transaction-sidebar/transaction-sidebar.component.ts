fbe45e2747e3359105b9df4e087249cf7e734a94
william <williamhehk@gmail.com>
2020-02-26 16:16:16 -0500

    Filer navigates to specific tab in all transaction table

--- front-end/src/app/forms/form-3x/transaction-sidebar/transaction-sidebar.component.ts
+++ front-end/src/app/forms/form-3x/transaction-sidebar/transaction-sidebar.component.ts
@@ -331,12 +331,14 @@ export class TransactionSidebarComponent implements OnInit {
   }
 
   public goToAllTransactions(){
+    const transactionCategory: 'receipts' = this._activatedRoute.snapshot.queryParams.transactionCategory;
+
     this._transactionMessageService.sendLoadDefaultTabMessage(
       {
         step: 'transactions',
         reportId: this.reportId,
         edit: this.editMode,
-        transactionCategory: 'receipts'
+        transactionCategory: transactionCategory
       }
     )
   }
