09df55d1ae7cea5dae3936d84ca9ac5237f48c75
qsunctr <qsun.ctr@fec.gov>
2020-03-17 12:14:45 -0400

    update all_transaction_table to enable force itemize

--- django-backend/fecfiler/core/views.py
+++ django-backend/fecfiler/core/views.py
@@ -3167,7 +3167,7 @@ def get_trans_query(category_type, cmte_id, param_string):
             """SELECT report_id, report_type, reportStatus, transaction_type, transaction_type_desc, transaction_id, api_call, name, street_1, street_2, city, state, zip_code, transaction_date, 
                                 COALESCE(transaction_amount, 0.0) AS transaction_amount, back_ref_transaction_id,
                                 COALESCE(aggregate_amt, 0.0) AS aggregate_amt, purpose_description, occupation, employer, memo_code, memo_text, itemized, beneficiary_cmte_id, election_code, 
-                                election_year, election_other_description,transaction_type_identifier, entity_id, entity_type, deleteddate, isEditable, aggregation_ind, hasChild, isredesignatable, "isRedesignation" from all_disbursements_transactions_view
+                                election_year, election_other_description,transaction_type_identifier, entity_id, entity_type, deleteddate, isEditable, forceitemizable, aggregation_ind, hasChild, isredesignatable, "isRedesignation" from all_disbursements_transactions_view
                             where cmte_id='"""
             + cmte_id
             + """' """
@@ -3205,7 +3205,7 @@ def get_trans_query(category_type, cmte_id, param_string):
             """SELECT report_id, report_type, reportStatus, transaction_type, transaction_type_desc, transaction_id, api_call, name, street_1, street_2, city, state, zip_code, transaction_date, 
                                 COALESCE(transaction_amount, 0.0) AS transaction_amount, back_ref_transaction_id,
                                 COALESCE(aggregate_amt, 0.0) AS aggregate_amt, purpose_description, occupation, employer, memo_code, memo_text, itemized, election_code, election_other_description, 
-                                transaction_type_identifier, entity_id, entity_type, deleteddate, isEditable, aggregation_ind, hasChild, isreattributable, "isReattribution" from all_receipts_transactions_view
+                                transaction_type_identifier, entity_id, entity_type, deleteddate, isEditable, forceitemizable, aggregation_ind, hasChild, isreattributable, "isReattribution" from all_receipts_transactions_view
                             where cmte_id='"""
             + cmte_id
             + """' """
