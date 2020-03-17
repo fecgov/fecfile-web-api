de947e384b791cf2ae01976992660e294eb90881
Roberto Sanchez <rsanchez@rhythmictech.com>
2020-03-16 21:49:49 -0400

    removed force_(un)itemized_sb from url.py

--- django-backend/fecfiler/sched_B/urls.py
+++ django-backend/fecfiler/sched_B/urls.py
@@ -4,17 +4,25 @@ from . import views
 
 urlpatterns = [
 
-    url(r'^sb/schedB$', views.schedB, name='schedB'),
     url(
-        r"^sb/force_itemize_sb$", views.force_itemize_sb, name="force_itemize_sb"
+        r'^sb/schedB$', 
+        views.schedB,
+        name='schedB'
     ),
+#    url(
+#        r"^sb/force_itemize_sb$",
+#        views.force_itemize_sb,
+#        name="force_itemize_sb"
+#    ),
+#    url(
+#        r"^sb/force_unitemize_sb$",
+#        views.force_unitemize_sb,
+#        name="force_unitemize_sb",
+#    ),
     url(
-        r"^sb/force_unitemize_sb$",
-        views.force_unitemize_sb,
-        name="force_unitemize_sb",
-    ),
-    url(
-        r"^sb/force_aggregate_sb$", views.force_aggregate_sb, name="force_aggregate_sb"
+        r"^sb/force_aggregate_sb$", 
+        views.force_aggregate_sb, 
+        name="force_aggregate_sb"
     ),
     url(
         r"^sb/force_unaggregate_sb$",
