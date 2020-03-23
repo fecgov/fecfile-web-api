53910371112a0ec47080508a5cee0c04210bc429
qsunctr <qsun.ctr@fec.gov>
2020-03-22 22:15:06 -0400

    update sched_b force itemize

--- django-backend/fecfiler/sched_B/urls.py
+++ django-backend/fecfiler/sched_B/urls.py
@@ -9,16 +9,16 @@ urlpatterns = [
         views.schedB,
         name='schedB'
     ),
-#    url(
-#        r"^sb/force_itemize_sb$",
-#        views.force_itemize_sb,
-#        name="force_itemize_sb"
-#    ),
-#    url(
-#        r"^sb/force_unitemize_sb$",
-#        views.force_unitemize_sb,
-#        name="force_unitemize_sb",
-#    ),
+    url(
+       r"^sb/force_itemize_sb$",
+       views.force_itemize_sb,
+       name="force_itemize_sb"
+    ),
+    url(
+       r"^sb/force_unitemize_sb$",
+       views.force_unitemize_sb,
+       name="force_unitemize_sb",
+    ),
     url(
         r"^sb/force_aggregate_sb$",
         views.force_aggregate_sb,
