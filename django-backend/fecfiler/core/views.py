456a18bfd599a5d24d78704a6b0744101317b5f4
qsunctr <qsun.ctr@fec.gov>
2020-03-24 16:00:35 -0400

    new migration for h4 and h6 aggregation

--- django-backend/fecfiler/core/views.py
+++ django-backend/fecfiler/core/views.py
@@ -3212,7 +3212,7 @@ def get_trans_query(category_type, cmte_id, param_string):
             """SELECT report_id, schedule, report_type, reportStatus, activity_event_identifier, transaction_type, transaction_type_desc, transaction_id, back_ref_transaction_id, api_call, 
                           name, street_1, street_2, city, state, zip_code, transaction_date, COALESCE(transaction_amount, 0.0) AS transaction_amount, 
                                 COALESCE(aggregate_amt, 0.0) AS aggregate_amt, purpose_description, occupation, employer, memo_code, memo_text, itemized, 
-                                election_code, election_other_description, transaction_type_identifier, entity_id, entity_type, deleteddate, isEditable, hasChild, istrashable
+                                election_code, election_other_description, transaction_type_identifier, entity_id, entity_type, deleteddate, isEditable, aggregation_ind, hasChild, istrashable
                             from all_other_transactions_view
                             where cmte_id='"""
             + cmte_id
