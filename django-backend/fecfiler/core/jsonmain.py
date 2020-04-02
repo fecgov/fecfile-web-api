0d844b22923688457fc70c7ea84e6e0f24f8baa4
jpraveenkmrSalient <41488234+jpraveenkmrSalient@users.noreply.github.com>
2020-04-02 10:00:59 -0400

    FNE-2144 fix (#2228)

--- django-backend/fecfiler/core/jsonmain.py
+++ django-backend/fecfiler/core/jsonmain.py
@@ -103,7 +103,7 @@ DICT_PURPOSE_DESCRIPTION_VALUES = {'Convention Account' : ['IND_NP_CONVEN_ACC','
                                     'See Memos Below' : ['LEVIN_PARTN_REC', 'PARTN_REC'],
                                     # Removing 'EAR_MEMO' from below as it being populated from front-end
                                     'Total Earmarked through Conduit' : ['EAR_REC_CONVEN_ACC_MEMO','EAR_REC_HQ_ACC_MEMO','EAR_REC_RECNT_ACC_MEMO','PAC_EAR_MEMO'],
-                                    'Earmarked from' : ['CON_EAR_DEP_MEMO']
+                                    'Earmarked from' : ['CON_EAR_DEP_MEMO', 'CON_EAR_UNDEP_MEMO']
                                     }
 
 logger = logging.getLogger(__name__)
