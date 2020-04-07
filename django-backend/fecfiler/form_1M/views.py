73226fb8f2e455b6906dc1f46d949c1a3d684e55
jpraveenkmrSalient <praveen.jinka@ctr.salientcrgt.com>
2020-04-07 15:02:57 -0400

    f1m

--- django-backend/fecfiler/form_1M/views.py
+++ django-backend/fecfiler/form_1M/views.py
@@ -17,7 +17,7 @@ from fecfiler.core.report_helper import new_report_date
 # Create your views here.
 logger = logging.getLogger(__name__)
 
-LIST_F1M_COLUMNS = ['report_id', 'est_status', 'cmte_id', 'aff_cmte_id', 'can1_id', 'can1_con', 'can2_id', 'can2_con', 'can3_id', 
+LIST_F1M_COLUMNS = ['report_id', 'est_status', 'cmte_id', 'aff_cmte_id', 'aff_date', 'can1_id', 'can1_con', 'can2_id', 'can2_con', 'can3_id', 
 					'can3_con', 'can4_id', 'can4_con', 'can5_id', 'can5_con', 'date_51', 'orig_date', 'metreq_date', 'sign_id', 'sign_date']
 
 DICT_F1M_COLUMNS_MAP_INPUT = {
@@ -116,7 +116,7 @@ def put_sql(request_dict, table, error_function="blank"):
 				cmte_id = %s AND report_id = %s AND delete_ind IS DISTINCT FROM 'Y'"""
 			value_list.extend([cmte_id, report_id])
 			cursor.execute(sql,value_list)
-			logger.debug(table + " POST")
+			logger.debug(table + " PUT")
 			logger.debug(cursor.query)
 			if cursor.rowcount == 0:
 				raise Exception('Failed to update data into {} table.'.format(table))
@@ -204,6 +204,45 @@ def reports_post(request):
 		raise Exception(
 			'The reports_post function is throwing an error: ' + str(e))
 
+"""
+***************************** CANDIDATE DETAILS API *************************************
+"""
+def get_candidate_details(request_dict):
+	try:
+		output_list = []
+		for i in range(1,6):
+			column_name = 'can'+str(i)+'_id'
+			candidate_id = request_dict.get(column_name)
+			if candidate_id:
+				candidate_dict = get_sql_candidate(candidate_id)
+				candidate_dict['contribution_date'] = request_dict.get(column_name[:-2]+'con')
+				candidate_dict['candidate_number'] = i
+				output_list.append[candidate_dict]
+			del request_dict[column_name]
+			del request_dict[column_name[:-2]+'con']
+		request_dict['candidates'] = output_list
+		return request_dict
+	except Exception as e:
+		raise Exception(
+			'The get_candidate_details function is throwing an error: ' + str(e))
+
+def get_sql_candidate(candidate_id):
+	try:
+		sql = """SELECT cand_id AS "candidate_id", cand_last_name, cand_first_name, cand_middle_name, 
+		cand_prefix, cand_suffix, cand_office, cand_office_state, cand_office_district 
+		FROM public.candidate_master WHERE cand_id=%s"""
+		with connection.cursor() as cursor:
+			cursor.execute("""SELECT json_agg(t) FROM ({}) AS t""",format(sql),[candidate_id])
+			logger.debug("CANDIDATE TABLE")
+			logger.debug(cursor.query)
+			output_dict = cursor.fetchone()
+			if output_dict:
+				return output_dict[0][0]
+			else:
+				raise Exception('The candidateId: {} does not exist in candidate table.'.format(candidate_id))
+	except Exception as e:
+		raise Exception(
+			'The get_sql_candidate function is throwing an error: ' + str(e))
 
 """
 ************************************ Form 1M Functions ******************************************
@@ -258,29 +297,44 @@ def get_sql_f1m(request_dict):
 		noneCheckMissingParameters(['report_id', 'cmte_id'], checking_dict=request_dict,
 								   value_dict=request_dict, function_name=table+'-PUT')
 		for key, value in request_dict.items() and key in ['report_id', 'cmte_id']:
-			param_string += key + "=%s AND "
+			param_string += "m." + key + "=%s AND "
 			value_list = value_list.append(value)
 		with connection.cursor() as cursor:
 			"""
-			id, report_id, est_status, cmte_id, aff_cmte_id, can1_id, can1_con, 
+			id, report_id, est_status, cmte_id, aff_cmte_id, aff_date, can1_id, can1_con, 
 	       can2_id, can2_con, can3_id, can3_con, can4_id, can4_con, can5_id, 
 	       can5_con, date_51, orig_date, metreq_date, sign_id, sign_date, 
 	       delete_ind, create_date, last_update_date
 			"""
 			sql = """SELECT rp.form_type, rp.amend_ind, rp.status, m.report_id AS "reportId", 
-				m.cmte_id, m.est_status AS "establishmentType", 
-				FROM form_1m m 
-				WHERE 
-				UNION reports rp WHERE m.report_id=rp.report_id AND rp.delete_ind IS DISTINCT FROM 'Y' """
-			value_list.extend([cmte_id, report_id])
-			cursor.execute(sql,value_list)
-			logger.debug(table + " POST")
+				m.cmte_id, m.est_status AS "establishmentType", m.aff_cmte_id AS "committee_id",
+				(SELECT cmte.cmte_name FROM committee_master cmte WHERE cmte.cmte_id = m.aff_cmte_id)
+				AS "committee_name", m.aff_date AS "affiliation_date", m.can1_id, m.can1_con, 
+	        	m.can2_id, m.can2_con, m.can3_id, m.can3_con, m.can4_id, m.can4_con, m.can5_id, 
+	        	m.can5_con, m.date_51 AS "fifty_first_contributor_date", m.orig_date AS "registration_date",
+	        	m.metreq_date AS "requirements_met_date", m.sign_id, e.last_name AS "sign_last_name", 
+	        	e.first_name AS "sign_first_name", e.middle_name AS "sign_middle_name", e.preffix AS "sign_prefix",
+	        	e.suffix AS "sign_suffix", m.sign_date AS "submission_date", rp.email_1, rp.email_2,
+	        	rp.additional_email_1, rp.additional_email_2
+				FROM public.form_1m m 
+				WHERE {} m.delete_ind IS DISTINCT FROM 'Y' 
+				UNION public.reports rp WHERE m.report_id=rp.report_id AND m.cmte_id=rp.cmte_id
+				AND rp.delete_ind IS DISTINCT FROM 'Y' 
+				UNION public.entity e WHERE e.entity_id = m.sign_id""".format(param_string)
+			cursor.execute("""SELECT json_agg(t) FROM ({}) AS t""",format(sql),value_list)
+			logger.debug(table + " GET")
 			logger.debug(cursor.query)
-			if cursor.rowcount == 0:
-				raise Exception('Failed to update data into {} table.'.format(table))
+			output_dict = cursor.fetchone()
+			if output_dict:
+				output_dict = output_dict[0][0]
+				output_dict = get_candidate_details(output_dict)
+			else:				
+				raise Exception("""This report Id: {} for committee Id: {} 
+					does not exist in forms_1m table""".format(request_dict['report_id'], 
+						request_dict['cmte_id']))
 	except Exception as e:
 		raise Exception(
-			'The put_sql function for table : {} is throwing an error in the function {}: '.format(error_function, table) + str(e))
+			'The get_sql_f1m function for table : {} is throwing an error in the function {}: '.format(error_function, table) + str(e))
 
 """
 ************************************ API Call ******************************************
