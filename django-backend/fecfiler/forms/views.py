aa75228adb25ccae6caadcd7fc781c0c5477de1a
jpraveenkmrSalient <praveen.jinka@ctr.salientcrgt.com>
2020-05-01 11:58:11 -0400

    adding additional parameters to get_committee_details API phone, website

--- django-backend/fecfiler/forms/views.py
+++ django-backend/fecfiler/forms/views.py
@@ -629,12 +629,16 @@ def get_committee_details(request):
         cmte_id = request.user.username
         with connection.cursor() as cursor:
             # GET all rows from committee table
-            query_string = """SELECT cmte_id AS "committeeid", cmte_name AS "committeename", street_1 AS "street1", street_2 AS "street2", city, state, zip_code AS "zipcode", 
-                cmte_email_1 AS "email_on_file", cmte_email_2 AS "email_on_file_1", phone_number, cmte_type, cmte_dsgn, cmte_filing_freq, cmte_filed_type, 
-                treasurer_last_name AS "treasurerlastname", treasurer_first_name AS "treasurerfirstname", treasurer_middle_name AS "treasurermiddlename", 
-                treasurer_prefix AS "treasurerprefix", treasurer_suffix AS "treasurersuffix", create_date AS "created_at", cmte_type_category,
-                (SELECT f1.fax FROM public.form_1 f1 WHERE f1.comid=cmte_id ORDER BY f1.sub_date DESC, f1.create_date DESC LIMIT 1)
-                FROM public.committee_master WHERE cmte_id = %s ORDER BY create_date"""
+            query_string = """SELECT cm.cmte_id AS "committeeid", cm.cmte_name AS "committeename", cm.street_1 AS "street1", cm.street_2 AS "street2", 
+                cm.city, cm.state, cm.zip_code AS "zipcode", 
+                cm.cmte_email_1 AS "email_on_file", cm.cmte_email_2 AS "email_on_file_1", cm.phone_number, cm.cmte_type, cm.cmte_dsgn, 
+                cm.cmte_filing_freq, cm.cmte_filed_type, 
+                cm.treasurer_last_name AS "treasurerlastname", cm.treasurer_first_name AS "treasurerfirstname", cm.treasurer_middle_name AS "treasurermiddlename", 
+                cm.treasurer_prefix AS "treasurerprefix", cm.treasurer_suffix AS "treasurersuffix", cm.create_date AS "created_at", cm.cmte_type_category, f1.fax, 
+                f1.tphone as "treasurerphone", f1.url as "website"
+                FROM public.committee_master cm
+                LEFT JOIN public.form_1 f1 ON f1.comid=cmte_id
+                WHERE cm.cmte_id = %s ORDER BY cm.create_date, f1.sub_date DESC, f1.create_date DESC LIMIT 1"""
             cursor.execute("""SELECT json_agg(t) FROM (""" + query_string + """) t""", [cmte_id])
             modified_output = cursor.fetchone()[0]
         if modified_output is None:
