6b09e0878635e33976e6bd43fb6ffea83097ec4e
MeghaA05 <agarwal.megha87@gmail.com>
2020-08-14 14:52:16 -0400

    FNE-2687 forgot password

---
+++ django-backend/fecfiler/password_management/otp.py
@@ -0,0 +1,131 @@
+import codecs
+import datetime
+
+from binascii import unhexlify
+from datetime import datetime
+from datetime import timedelta
+
+import pytz
+from django.contrib.auth.hashers import get_hasher, make_password
+from django.db import connection
+from django_otp.oath import TOTP
+from django_otp.util import random_hex
+from unittest import mock
+import time
+
+from fecfiler.settings import OTP_DIGIT, OTP_TIME_EXPIRY, logger, OTP_MAX_RETRY
+
+
+def save_key_datbase(username, key_val, counter):
+    try:
+        print(key_val)
+        print(key_val.decode("utf-8"))
+        decode_key = key_val.decode("utf-8")
+        with connection.cursor() as cursor:
+            _sql = """UPDATE public.authentication_account SET secret_key = %s, code_generated_counter = %s, updated_at = %s WHERE username = %s AND delete_ind !='Y'"""
+            _v = (decode_key, counter, datetime.now(), username)
+            cursor.execute(_sql, _v)
+            if cursor.rowcount != 1:
+                logger.debug("key save failed for username {}", username)
+        return cursor.rowcount
+    except Exception as e:
+        logger.debug("exception occurred key save for username {}", username)
+
+
+def get_current_counter_val(username):
+    default_sequence = 0
+    try:
+        with connection.cursor() as cursor:
+            _sql = """select code_generated_counter from public.authentication_account where username = %s AND delete_ind !='Y'"""
+            cursor.execute(_sql, [username])
+            code_counter = cursor.fetchone()[0]
+            if code_counter is None:
+                default_sequence
+            else:
+                default_sequence = int(code_counter) + 1
+
+        return default_sequence
+    except Exception as e:
+        raise e
+
+
+def get_last_updated_time(username):
+    try:
+        with connection.cursor() as cursor:
+            _sql = """select updated_at from public.authentication_account where username = %s AND delete_ind !='Y'"""
+            cursor.execute(_sql, [username])
+            update_time = cursor.fetchone()[0]
+
+        return update_time
+    except Exception as e:
+        raise e
+
+
+class TOTPVerification:
+
+    def __init__(self, username):
+
+        self.key = make_password(username).encode('utf-8').hex()
+        self.number_of_digits = OTP_DIGIT
+        self.token_validity_period = OTP_TIME_EXPIRY
+
+    @property
+    def bin_key(self):
+        return unhexlify(self.key.encode())
+
+    def totp_obj(self, username):
+        # create a TOTP object
+        key_val = self.bin_key
+        counter = get_current_counter_val(username)
+        last_updated_time = get_last_updated_time(username)
+        totp = TOTP(key=self.bin_key,
+                    step=self.token_validity_period,
+                    digits=self.number_of_digits)
+
+        totp.time = time.time()
+        est = pytz.timezone('US/Eastern')
+        current_time_est = datetime.now(est)
+        # last_updated_time_end = last_updated_time.replace(tzinfo=pytz.utc)
+        # #last_updated_time_end1 = last_updated_time.replace(est)
+        # current_time = datetime.now()
+        #
+        # current_time_utc = datetime.now(pytz.utc)
+        # time1 = last_updated_time.astimezone(est)
+        current_time_est1 = current_time_est.replace(tzinfo=None)
+        upper_limit = last_updated_time + timedelta(minutes=30)
+        upper_limit1 = upper_limit.replace(tzinfo=None)
+        if counter <= OTP_MAX_RETRY:
+            save_key_datbase(username, key_val, counter)
+        elif counter > OTP_MAX_RETRY and upper_limit1 < current_time_est1:
+            counter = 0
+            save_key_datbase(username, key_val, counter)
+        elif counter > OTP_MAX_RETRY:
+            return None
+
+        return totp
+
+    def generate_token(self, username):
+
+        totp = self.totp_obj(username)
+        if totp is None:
+            return -1
+        token = str(totp.token()).zfill(6)
+        print(token)
+
+        return token
+
+    def verify_token(self, key):
+        try:
+            print(key)
+            print(key.encode("utf-8"))
+            encode_key = key.encode("utf-8")
+            totp = TOTP(key=encode_key,
+                        step=self.token_validity_period,
+                        digits=self.number_of_digits)
+            token = str(totp.token()).zfill(6)
+            print(token)
+
+            return token
+        except ValueError:
+            # return False, if token could not be converted to an integer
+            return 0
---
+++ django-backend/fecfiler/password_management/views.py
@@ -0,0 +1,439 @@
+import logging
+import re
+import time
+import boto3
+import jwt
+
+from datetime import datetime
+from datetime import timedelta
+from callfire.client import CallfireClient
+from botocore.exceptions import ClientError
+from django.contrib.auth.hashers import make_password
+from django.db import connection
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from django_otp.oath import TOTP
+from rest_framework import status
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+
+from fecfiler.core.views import check_null_value
+from fecfiler.password_management.otp import TOTPVerification
+from fecfiler.settings import SECRET_KEY, JWT_PASSWORD_EXPIRY, API_LOGIN, API_PASSWORD
+
+logger = logging.getLogger(__name__)
+
+
+def find_account(cmte_id, personal_key, email):
+    try:
+        with connection.cursor() as cursor:
+            # check if user already exist
+            _sql = """Select * from public.authentication_account WHERE cmtee_id = %s AND lower(email) = lower(%s) 
+            AND personal_key = %s AND delete_ind !='Y' """
+            cursor.execute(_sql, [cmte_id, email, personal_key])
+            user_list = cursor.fetchone()
+            if user_list is None:
+                return False
+            elif cursor.rowcount == 1:
+                return True
+            else:
+                return False
+    except Exception as e:
+        logger.debug("exception occurred while checking if user was previously deleted", str(e))
+        return False
+
+
+def check_madatory_field(data, list_mandatory_fields):
+    try:
+        error = []
+        for field in list_mandatory_fields:
+            if not (field in data and check_null_value(data.get(field))):
+                error.append(field)
+        if len(error) > 0:
+            string = ""
+            for x in error:
+                string = string + x + ", "
+            string = string[0:-2]
+            raise Exception(
+                "The following mandatory fields are required in order to save data to Reports table: {}".format(
+                    string
+                )
+            )
+    except Exception as e:
+        raise e
+
+
+def check_account_exist(cmte_id, email):
+    try:
+        with connection.cursor() as cursor:
+            # check if user already exist
+            _sql = """SELECT json_agg(t) FROM (Select * from public.authentication_account WHERE cmtee_id = %s AND lower(email) = lower(%s) 
+            AND status ='Registered' AND delete_ind !='Y') t"""
+            cursor.execute(_sql, [cmte_id, email])
+            user_list = cursor.fetchone()
+            merged_list = []
+            for dictL in user_list:
+                merged_list = dictL[0]
+
+        return merged_list
+    except Exception as e:
+        logger.debug("Exception occurred while checking user already present", str(e))
+        raise e
+
+
+def reset_code_counter(key):
+    try:
+        with connection.cursor() as cursor:
+            _sql = """UPDATE public.authentication_account SET code_generated_counter = 0, updated_at = %s WHERE secret_key = %s"""
+            cursor.execute(_sql, [datetime.now(), key])
+            if cursor.rowcount != 1:
+                logger.debug("Reset key failed for key {}", key)
+        return cursor.rowcount
+    except Exception as e:
+        logger.debug("exception occurred reset key for key {}", key)
+
+
+def reset_account_password(cmte_id, password, email):
+    try:
+        with connection.cursor() as cursor:
+            _sql = """UPDATE public.authentication_account SET password = %s, status = %s WHERE cmtee_id = %s AND 
+            email = %s AND delete_ind !='Y' """
+            cursor.execute(_sql, [make_password(password), "Registered", cmte_id, email])
+            if cursor.rowcount != 1:
+                raise Exception("Password was not reset.")
+            else:
+                return True
+    except Exception as e:
+        logger.debug("Exception occurred while resetting password.", str(e))
+        raise e
+
+
+def token_verification(request):
+    try:
+        token_received = request.headers['token']
+        payload = verify_token(token_received)
+        return payload
+    except Exception as e:
+        logger.debug("exception occurred while generating token for email option.", str(e))
+        raise e
+
+
+def create_jwt_token(email, cmte_id):
+    now = int(time.time())
+    token = jwt.encode({
+        'email': email,
+        'committee_id': cmte_id,
+        'exp': now + JWT_PASSWORD_EXPIRY
+    }, SECRET_KEY, algorithm='HS256').decode('utf-8')
+    return token
+
+
+def verify_token(token_received):
+    options = {'verify_exp': True,  # Skipping expiration date check
+               'verify_aud': False}  # Skipping audience check
+    payload = jwt.decode(token_received, key=SECRET_KEY, algorithms='HS256', options=options)
+    return payload
+
+
+def check_email(email):
+    pattern = re.compile(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)")
+    if re.fullmatch(pattern, email):
+        return email
+    else:
+        raise Exception(
+            "Email entered is in an invalid format. Input received is: {}. Expected: abc@def.xyz or abc@def.wxy.xyz".format(
+                email
+            )
+        )
+
+
+def send_email(token_val, email):
+    SENDER = "donotreply@fec.gov"
+    RECIPIENT = []
+    data = {}
+    RECIPIENT.append("%s" % email)
+
+    SUBJECT = "Register Account"
+
+    # The email body for recipients with non-HTML email clients.
+    BODY_TEXT = ("Use the code provided below to continue the verification process of your account.\r\n" +
+                 token_val
+                 + "\r\nPlease do not reply to this message"
+                   "\r\nThis email was sent by FEC.gov. If you are receiving this email in error or have any "
+                   "questions, "
+                   "please contact the FEC Electronic Filing Office toll-free at (800) 424-9530 ext. 1307 or locally "
+                   "at (202) 694-1307. "
+                 )
+
+    data['token'] = token_val
+
+    BODY_HTML = render_to_string('forgot_password.html', {'data': data})
+
+    # The character encoding for the email.
+    CHARSET = "UTF-8"
+
+    # Create a new SES resource and specify a region.
+    client = boto3.client('ses', region_name='us-east-1')
+
+    # Send the email.
+    try:
+        # Provide the contents of the email.
+        response = client.send_email(
+            Destination={
+                'ToAddresses':
+                    RECIPIENT,
+
+            },
+            Message={
+                'Body': {
+                    'Html': {
+                        'Charset': CHARSET,
+                        'Data': BODY_HTML,
+                    },
+                    'Text': {
+                        'Charset': CHARSET,
+                        'Data': BODY_TEXT,
+                    },
+                },
+                'Subject': {
+                    'Charset': CHARSET,
+                    'Data': SUBJECT,
+                },
+            },
+            Source=SENDER,
+
+        )
+    # Display an error if something goes wrong.
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+
+
+def send_text(token_val, phone_no):
+    try:
+
+        token_val_formatted = "{:s}".format('\u0332'.join(token_val))
+        client = CallfireClient(API_LOGIN, API_PASSWORD)
+        response = client.texts.sendTexts(
+
+            body=[
+                {
+                    'phoneNumber': phone_no,
+                    'message': 'From the Federal Election Commission: The one-time code you requested is ' + token_val + '.Please use this code to login. '
+                }
+            ]
+        ).result()
+        print(response)
+    except Exception as e:
+        print(e.response['Error']['Message'])
+
+
+def send_call(token_val, phone_no):
+    try:
+        token_formatted_val = " ,".join(token_val)
+        client = CallfireClient(API_LOGIN, API_PASSWORD)
+        response = client.calls.sendCalls(
+
+            defaultVoice='FEMALE1',
+
+            body=[
+                {
+                    'phoneNumber': phone_no,
+                    'liveMessage': 'From the Federal Election Commission: The one-time code you requested is ' + token_formatted_val + ' , , , .Please use this code to login. ',
+                    'machineMessage': 'From the Federal Election Commission: The one-time code you requested is ' + token_formatted_val + ', , , .Please use this code to login. '
+                }
+            ]
+
+        ).result()
+        print(response)
+    except Exception as e:
+        print(e.response['Error']['Message'])
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([])
+def authenticate_password(request):
+    if request.method == "POST":
+
+        try:
+            data = request.data
+            cmte_id = data.get('committee_id', None)
+            email = data.get('email', None)
+            is_allowed = False
+
+            list_mandatory_fields = ["committee_id", "email"]
+            check_madatory_field(data, list_mandatory_fields)
+            check_email(email)
+            user_list = check_account_exist(cmte_id, email)
+
+            if user_list is None:
+                response = {'is_allowed': is_allowed, 'committee_id': cmte_id,
+                            'email': email}
+                return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+            else:
+                is_allowed = True
+                # last_time = user_list["updated_at"]
+                # last_time_obj = datetime.strptime(last_time, '"%m-%d-%Y %T')
+                # upper_limit = last_time_obj + timedelta(minutes=30)
+                # print(upper_limit > datetime.now())
+                token = create_jwt_token(email, cmte_id)
+                response = {'is_allowed': is_allowed, 'committee_id': cmte_id,
+                            'email': email, 'token': token}
+
+            return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+        except Exception as e:
+            logger.debug("exception occurred while getting account information", str(e))
+            json_result = {'message': str(e)}
+            return JsonResponse(json_result, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([])
+def reset_options_password(request):
+    if request.method == "POST":
+
+        if request.data.get("id") == 'EMAIL':
+            try:
+                is_allowed = False
+                payload = token_verification(request)
+                cmte_id = payload.get('committee_id', None)
+                email = payload.get('email', None)
+                data = {"committee_id": cmte_id, "email": email}
+
+                list_mandatory_fields = ["committee_id", "email"]
+                check_madatory_field(data, list_mandatory_fields)
+                user_list = check_account_exist(cmte_id, email)
+
+                if user_list is None:
+                    response = {'is_allowed': is_allowed}
+                    return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+
+                username = user_list["username"]
+                otp_class = TOTPVerification(username)
+                token_val = otp_class.generate_token(username)
+                if token_val != -1:
+                    is_allowed = True
+
+                if is_allowed:
+                    send_email(token_val, email)
+
+                response = {'is_allowed': is_allowed, 'committee_id': cmte_id,
+                            'email': email, 'token_val': token_val}
+                return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+            except Exception as e:
+                logger.debug("exception occurred while generating token for email option.", str(e))
+                json_result = {'is_allowed': False}
+                return JsonResponse(json_result, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+        elif request.data.get("id") == 'TEXT' or request.data.get("id") == 'CALL':
+            try:
+                is_allowed = False
+                payload = token_verification(request)
+                cmte_id = payload.get('committee_id', None)
+                email = payload.get('email', None)
+                data = {"committee_id": cmte_id, "email": email}
+
+                list_mandatory_fields = ["committee_id", "email"]
+                check_madatory_field(data, list_mandatory_fields)
+                user_list = check_account_exist(cmte_id, email)
+
+                if user_list is None:
+                    response = {'is_allowed': is_allowed}
+                    return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+                elif user_list is not None and not check_null_value(user_list["contact"]):
+                    response = {'is_allowed': is_allowed}
+                    return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+
+                username = user_list["username"]
+                otp_class = TOTPVerification(username)
+                token_val = otp_class.generate_token(username)
+                if token_val != -1:
+                    is_allowed = True
+
+                if is_allowed:
+                    if request.data.get("id") == 'TEXT':
+                        send_text(token_val, user_list["contact"])
+
+                    elif request.data.get("id") == 'CALL':
+                        send_call(token_val, user_list["contact"])
+                        pass
+
+                response = {'is_allowed': is_allowed, 'committee_id': cmte_id,
+                            'email': email}
+                return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+            except Exception as e:
+                logger.debug("exception occurred while getting account information", str(e))
+                json_result = {'message': str(e)}
+                return JsonResponse(json_result, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+        else:
+            json_result = {'message': "Please select proper option."}
+            return JsonResponse(json_result, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([])
+def code_verify_password(request):
+    if request.method == "POST":
+
+        try:
+            is_allowed = False
+            code = request.data.get('code', None)
+            payload = token_verification(request)
+            cmte_id = payload.get('committee_id', None)
+            email = payload.get('email', None)
+            data = {"committee_id": cmte_id, "email": email, "code": code}
+
+            list_mandatory_fields = ["committee_id", "email", "code"]
+            check_madatory_field(data, list_mandatory_fields)
+            user_list = check_account_exist(cmte_id, email)
+
+            if user_list is None:
+                is_allowed = False
+                response = {'is_allowed': is_allowed}
+                return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+
+            username = user_list["username"]
+            key = user_list["secret_key"]
+            otp_class = TOTPVerification(username)
+            token_val = otp_class.verify_token(key)
+
+            if code == token_val:
+                is_allowed = True
+                reset_code_counter(key)
+
+            response = {'is_allowed': is_allowed, 'committee_id': cmte_id,
+                        'email': email}
+            return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+        except Exception as e:
+            logger.debug("exception occurred while verifying code", str(e))
+            json_result = {'is_allowed': False}
+            return JsonResponse(json_result, status=status.HTTP_400_BAD_REQUEST, safe=False)
+
+
+@api_view(["POST"])
+@authentication_classes([])
+@permission_classes([])
+def reset_password(request):
+    if request.method == "POST":
+        try:
+            password_reset = False
+            password = request.data.get('password', None)
+            payload = token_verification(request)
+            cmte_id = payload.get('committee_id', None)
+            email = payload.get('email', None)
+            data = {"committee_id": cmte_id, "email": email, "password": password}
+
+            list_mandatory_fields = ["committee_id", "password", "email"]
+            check_madatory_field(data, list_mandatory_fields)
+            account_exist = check_account_exist(cmte_id, email)
+            if account_exist:
+                password_reset = reset_account_password(cmte_id, password, email)
+
+            response = {'password_reset': password_reset}
+            return JsonResponse(response, status=status.HTTP_200_OK, safe=False)
+        except Exception as e:
+            logger.debug("exception occurred while getting account information", str(e))
+            json_result = {'message': "Password Reset was not successful."}
+            return JsonResponse(json_result, status=status.HTTP_400_BAD_REQUEST, safe=False)
