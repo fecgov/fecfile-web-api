93c268abbdaee424be12c1111af151bb79f672f7
MeghaA05 <agarwal.megha87@gmail.com>
2020-08-19 13:27:08 -0400

    2f changes

--- django-backend/fecfiler/settings.py
+++ django-backend/fecfiler/settings.py
@@ -243,8 +243,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
