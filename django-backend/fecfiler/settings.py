183b495303d0123d1e293b5a907065cfa17ba47e
MeghaA05 <agarwal.megha87@gmail.com>
2020-08-21 16:02:18 -0400

    uncommenting till UI integeration

--- django-backend/fecfiler/settings.py
+++ django-backend/fecfiler/settings.py
@@ -243,6 +243,8 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ),
 }
 
