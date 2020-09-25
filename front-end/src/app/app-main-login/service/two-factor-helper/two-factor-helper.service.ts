9cdc8c7fb9687acd9fbfb7a937539c9ab92652bd
Zobair Saleem <zsaleem@halvik.com>
2020-09-25 14:09:29 -0400

    add user fixes.

--- front-end/src/app/app-main-login/service/two-factor-helper/two-factor-helper.service.ts
+++ front-end/src/app/app-main-login/service/two-factor-helper/two-factor-helper.service.ts
@@ -14,7 +14,7 @@ export class TwoFactorHelperService {
   ( private _cookieService: CookieService,
                 private _http: HttpClient ) { }
 
-  requestCode(requestOption: string) {
+  requestCode(requestOption: string, call_from:string) {
     const token: string = JSON.parse(this._cookieService.get('user'));
     let httpOptions = new HttpHeaders();
     const url = '/password/reset/options';
@@ -24,7 +24,7 @@ export class TwoFactorHelperService {
     httpOptions = httpOptions.append('Content-Type', 'application/json');
     httpOptions = httpOptions.append('token' , token);
 
-    const option: any = { id : requestOption.toUpperCase() };
+    const option: any = { id : requestOption.toUpperCase(), call_from: call_from };
 
     return this._http
         .post(`${environment.apiUrl}${url}`, option, {
