e5e175a65e927062a815cea981c58cb5e3d023d7
Shawn <shawn.mahal@salientcrgt.com>
2020-10-26 10:22:24 -0400

    how to

--- front-end/src/app/import-contacts-module/import-contacts/import-how-to/import-how-to.component.ts
+++ front-end/src/app/import-contacts-module/import-contacts/import-how-to/import-how-to.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { ImportContactsStepsEnum } from '../import-contacts-setps.enum';
+import { PaginationInstance } from 'ngx-pagination';
+import { duplicatesResponse } from './contacts-data';
 
 @Component({
   selector: 'app-import-how-to',
@@ -16,6 +18,12 @@ export class ImportHowToComponent implements OnInit {
   public readonly step1Upload = ImportContactsStepsEnum.step1Upload;
   public readonly step3Clean = ImportContactsStepsEnum.step3Clean;
   public readonly step4ImportDone = ImportContactsStepsEnum.step4ImportDone;
+  public contacts: Array<any>;
+  public config: PaginationInstance = {
+    id: 'how_to__table-pagination',
+    itemsPerPage: 4,
+    currentPage: 1
+  };
 
   constructor() {}
 
@@ -26,9 +34,24 @@ export class ImportHowToComponent implements OnInit {
       { text: 'Import', step: this.step4ImportDone }
     ];
     this.currentStep = this.step1Upload;
+    this.contacts = duplicatesResponse.contacts;
+    this.config.totalItems = duplicatesResponse.totalcontactsCount;
+    this.config.itemsPerPage = duplicatesResponse.itemsPerPage;
+    // this.numberOfPages = duplicatesResponse.totalPages;
   }
 
   public returnToImport() {
     this.returnEmitter.emit();
+  }
+
+  public formatName(contact: any): string {
+    let name = '';
+    if (contact.entity_type === 'IND') {
+      // TODO handle suffix and prefix
+      name = `${contact.last_name}, ${contact.first_name}`;
+    } else if (contact.entity_type === 'ORG') {
+      name = contact.entity_name;
+    }
+    return name;
   }
 }
