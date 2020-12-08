40ff99425cfd90ae70205da2fb7056dda51c6516
Shawn <shawn.mahal@salientcrgt.com>
2020-12-08 09:02:30 -0500

    download docs

--- front-end/src/app/import-transactions-module/import-transactions/import-trx-start/import-trx-start.component.ts
+++ front-end/src/app/import-transactions-module/import-transactions/import-trx-start/import-trx-start.component.ts
@@ -12,6 +12,10 @@ export class ImportTrxStartComponent implements OnInit {
   @Output()
   public beginFileSelectEmitter: EventEmitter<any> = new EventEmitter<any>();
 
+  private readonly downloadDocDir = 'transactions/download_docs/';
+  private readonly xlsxType = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+  private readonly csvType = 'text/csv;charset=utf-8';
+
   constructor(
     private _importTransactionsService: ImportTransactionsService,
     private _uploadContactsService: UploadContactsService
@@ -28,6 +32,89 @@ export class ImportTrxStartComponent implements OnInit {
       const type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
       const blob: Blob = new Blob([res], { type: type });
       FileSaver.saveAs(blob, 'Import Contacts Specs and Template.xlsx');
+    });
+  }
+
+  public downloadFormatSpecs() {
+    const files = [
+      'F3L - Schedule A_Format Specs_Import Trasactions_FINAL.xlsx',
+      'F3L - Schedule B_Format Specs_Import Trasactions_FINAL.xlsx',
+      'F3X - Schedule A_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule B_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule E_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule F_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule H3_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule H4_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule H5_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule H6_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule LA_Format Specs_Import Transactions_FINAL.xlsx',
+      'F3X - Schedule LB_Format Specs_Import Transactions_FINAL.xlsx'
+    ];
+    for (const file of files) {
+      const fileMetaData: any = {};
+      fileMetaData.dir = this.downloadDocDir + 'format_specs/';
+      fileMetaData.type = this.xlsxType;
+      fileMetaData.fileName = file;
+      this._downloadFile(fileMetaData);
+    }
+  }
+
+  public downloadF3xTemplate() {
+    const fileMetaData: any = {};
+    fileMetaData.dir = this.downloadDocDir;
+    fileMetaData.type = this.xlsxType;
+    fileMetaData.fileName = 'F3X - Tempate  Schedule Specs_Import Transactions.xlsx';
+    this._downloadFile(fileMetaData);
+  }
+
+  public downloadF3lTemplate() {
+    const fileMetaData: any = {};
+    fileMetaData.dir = this.downloadDocDir;
+    fileMetaData.type = this.xlsxType;
+    fileMetaData.fileName = 'F3L - Template  Schedule Specs_Import Transactions.xlsx';
+    this._downloadFile(fileMetaData);
+  }
+
+  public downloadUniqueTransactionIdentifiers() {
+    const fileMetaData: any = {};
+    fileMetaData.dir = this.downloadDocDir;
+    fileMetaData.type = this.xlsxType;
+    fileMetaData.fileName = 'F3X  F3L Transaction Types - Unique Identifiers.xlsx';
+    this._downloadFile(fileMetaData);
+  }
+
+  public downloadReportCodes() {
+    const fileMetaData: any = {};
+    fileMetaData.dir = this.downloadDocDir;
+    fileMetaData.type = this.xlsxType;
+    fileMetaData.fileName = 'F3X & F3L - Report Codes.xlsx';
+    this._downloadFile(fileMetaData);
+  }
+
+  public downloadCsvTemplate() {
+    const fileMetaData: any = {};
+    fileMetaData.dir = this.downloadDocDir;
+    fileMetaData.type = this.csvType;
+    fileMetaData.fileName = 'Template.csv';
+    this._downloadFile(fileMetaData);
+  }
+
+  public downloadTransactionTypes() {
+    const fileMetaData: any = {};
+    fileMetaData.dir = this.downloadDocDir;
+    fileMetaData.type = this.xlsxType;
+    fileMetaData.fileName = 'Transactions Types + Identifiers.xlsx';
+    this._downloadFile(fileMetaData);
+  }
+
+  private _downloadFile(fileMetaData: any) {
+    const dir = fileMetaData.dir;
+    const fileName = fileMetaData.fileName;
+    const type = fileMetaData.type;
+
+    this._uploadContactsService.getObject(dir + fileName).subscribe(res => {
+      const blob: Blob = new Blob([res.Body], { type: type });
+      FileSaver.saveAs(blob, fileName);
     });
   }
 
