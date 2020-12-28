dd9afe02f7edadf34462999f36d138ceb418b450
spanyala <31629295+spanyala@users.noreply.github.com>
2020-12-28 17:50:49 -0500

    added import_fecfile api (#2844)

--- django-backend/fecfiler/settings.py
+++ django-backend/fecfiler/settings.py
@@ -419,6 +419,11 @@ SUBMIT_REPORT_WAIT_FLAG = "False"
 # Service Endpoint for filing confirmation email
 NXG_FEC_FILING_CONFIRMATION_URL = os.environ.get('FILING_CONFIRMATION_URL',  'http://dev-efile-api.efdev.fec.gov/receiver/v1/acknowledgement_email')
 
+# dcf_converter end point details
+NXG_FEC_DCF_CONVERTER_API_URL = os.environ.get('DCF_CONVERTER_URL', 'https://dev-efile-api.efdev.fec.gov/dcf_converter')
+# NXG_FEC_DCF_CONVERTER_API_URL = os.environ.get('DCF_CONVERTER_URL', 'http://127.0.0.1:5000')
+NXG_FEC_DCF_CONVERTER_API_VERSION = "/v1/import"
+
 try:
   from .local_settings import *
 except:
