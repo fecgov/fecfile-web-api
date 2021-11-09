4b8bd889bb4a248e69a998f0e5540f74ad85d10c
root <root@ip-10-0-0-227.us-west-2.compute.internal>
2021-11-09 22:27:17 +0000

    draft cf-deploy.sh script

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -4,9 +4,17 @@
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 cd ${SCRIPT_DIR}/front-end
+
+#clean enverything up so we get a fresh build
 rm -rf dist node_modules
+
+#create the docker we will use to perform the build
 docker build -t fecfile-web-cf-build -f Dockerfile-cf-build .
 
+#build the app 
 docker run -v ${SCRIPT_DIR}/front-end/:/usr/src/app fecfile-web-cf-build sh -c 'cd /usr/src/app/ && npm install && node --max_old_space_size=4000 ./node_modules/\@angular/cli/bin/ng build'
 
-echo cf push -f frontend-manifest
+#copy all the nginx and cloud foundry files into the Angualr dist dir
+cp ${SCRIPT_DIR}/deploy-config/front-end-nginx-config/* ${SCRIPT_DIR}/front-end/dist/fec-eFilling
+cd ${SCRIPT_DIR}/front-end/dist/fec-eFilling
+cf push 
