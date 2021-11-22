f93ddbb2d2af442e7983a2abefe47917427536e5
Al Crowley <albert.crowley@tcg.com>
2021-11-22 12:25:31 -0500

    Adding eslint * Added lint target in package.json * Reorganized the dockerfiles * created a lint.sh script

--- cf-deploy.sh
+++ cf-deploy.sh
@@ -9,7 +9,7 @@ cd ${SCRIPT_DIR}/front-end
 rm -rf dist node_modules
 
 #create the docker we will use to perform the build
-docker build -t fecfile-web-cf-build -f Dockerfile-cf-build .
+docker build -t fecfile-web-cf-build -f build-scripts/Dockerfile-cf-build .
 
 #build the app 
 docker run -v ${SCRIPT_DIR}/front-end/:/usr/src/app fecfile-web-cf-build sh -c 'cd /usr/src/app/ && npm install && node --max_old_space_size=4000 ./node_modules/\@angular/cli/bin/ng build'
