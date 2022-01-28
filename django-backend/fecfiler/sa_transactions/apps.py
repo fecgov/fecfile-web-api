bd2662582d8bbb143059c56bbc4d8cdd27503cfb
toddlees <todd.lees@tcg.com>
2022-01-28 17:50:43 -0500

    sa_transactions take 1

---
+++ django-backend/fecfiler/sa_transactions/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SchedAConfig(AppConfig):
+    name = 'fecfiler.sa_transactions'
---
+++ django-backend/fecfiler/sa_transactions/tests.py
@@ -0,0 +1,23 @@
+from django.test import TestCase
+from .models import SATransaction
+
+
+class SATransactionTestCase(TestCase):
+    """ Test module for inserting a sched_a item"""
+
+    def setUp(self):
+        self.sa_trans = SATransaction(
+            FORM_TYPE="SA11AI",
+            FILER_COMMITTEE_ID_NUMBER="C00123456",
+            TRANSACTION_ID="A56123456789-1234",
+            ENTITY_TYPE="IND",
+            CONTRIBUTOR_ORGANIZATION_NAME="John Smith & Co.",
+            CONTRIBUTOR_FIRST_NAME="John",
+            CONTRIBUTOR_LAST_NAME="Smith"
+        )
+        
+
+    def test_save(self):
+        self.sa_trans.full_clean()
+        self.sa_trans.save()
+# Create your tests here.
