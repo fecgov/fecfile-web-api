5b09d3badaa2f3dce449e01fa38bffc7f14197ab
Laura Beaufort <lbeaufort@fec.gov>
2022-02-01 10:18:02 -0500

    Add migrations to deploy command, cleanup

--- bin/run.sh
+++ bin/run.sh
@@ -1,2 +1,9 @@
 cd django-backend
+
+# Run migrations
+./manage.py makemigrations
+./manage.py migrate --noinput
+
+# Run application
+# python wait_for_db.py &&
 gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 10 -t 200 --reload
