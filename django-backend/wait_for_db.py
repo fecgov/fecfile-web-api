2da14d7c24be539e4ec897e30b8a6089beac77d3
Matt Travers <matt.travers@tcg.com>
2022-02-02 17:11:26 -0500

    Restored old wait_for_db. Will get updated with api#17

--- django-backend/wait_for_db.py
+++ django-backend/wait_for_db.py
@@ -6,12 +6,25 @@ from retry import retry
 @retry(delay=2, tries=15)
 def postgres_test():
     try:
-        database_uri = os.environ.get('DATABASE_URL')
-        if not database_uri:
-            print("Environment variable DATABASE_URL not found. Please check your settings and try again")
+
+        if os.environ.get('FECFILE_DB_NAME') == None and os.environ.get('FECFILE_DB_PASSWORD') == None:
+            print("Environment settings for database and password not found. Please check your settings and try again")
             exit(1)
-        print("Testing connection...")
-        conn = psycopg2.connect(database_uri)
+
+        print("testing connection with dbname={} user={} host={} password={} connect_timeout=3000".
+            format(
+            os.environ.get('FECFILE_DB_NAME'),
+            os.environ.get('FECFILE_DB_USERNAME'),
+            os.environ.get('FECFILE_DB_HOST'),
+            os.environ.get('FECFILE_DB_PASSWORD')))
+
+        conn = psycopg2.connect(
+            'dbname={} user={} host={} password={} connect_timeout=3000'.
+            format(
+                os.environ.get('FECFILE_DB_NAME'),
+                os.environ.get('FECFILE_DB_USERNAME'),
+                os.environ.get('FECFILE_DB_HOST'),
+                os.environ.get('FECFILE_DB_PASSWORD')))
         conn.close()
         print("DB connection successful")
         return True
