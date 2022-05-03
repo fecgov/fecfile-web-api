263a3fae5b20fc5f71ee678ec7272b2545aae844
Matt Travers <matt.travers@tcg.com>
2022-05-03 16:47:02 -0400

    Fixed lint issue in migration file

--- django-backend/fecfiler/f3x_summaries/migrations/0008_auto_20220503_1411.py
+++ django-backend/fecfiler/f3x_summaries/migrations/0008_auto_20220503_1411.py
@@ -20,5 +20,7 @@ class Migration(migrations.Migration):
             name='qualified_committee',
             field=models.TextField(blank=True, null=True),
         ),
-        migrations.RunSQL('UPDATE f3x_summaries SET change_of_address = NULL, qualified_committee = NULL;')
+        migrations.RunSQL('UPDATE f3x_summaries SET ' \
+                          'change_of_address = NULL, ' \
+                          'qualified_committee = NULL;')
     ]
