f625a53cc81d09d1120a1c45607f567dcf92c76c
Elaine Krauss <elaine@ekrauss.com>
2022-05-19 13:08:45 -0400

    Updates the view to return the default queryset unless the queryset is request in a context that includes an f3x_summary in a request, in which case it returns a queryset of scha transactions that belong to the given f3x summary

--- django-backend/fecfiler/scha_transactions/views.py
+++ django-backend/fecfiler/scha_transactions/views.py
@@ -1,5 +1,6 @@
 from fecfiler.committee_accounts.views import CommitteeOwnedViewSet
 from .models import SchATransaction
+from django.db.models.query import QuerySet
 from .serializers import SchATransactionSerializer
 
 
@@ -12,6 +13,26 @@ class SchATransactionViewSet(CommitteeOwnedViewSet):
     The queryset will be further limmited by the user's committee
     in CommitteeOwnedViewSet's implementation of get_queryset()
     """
+
+    def get_queryset(self):
+        assert self.queryset is not None, (
+            "'%s' should either include a `queryset` attribute, "
+            "or override the `get_queryset()` method." % self.__class__.__name__
+        )
+
+        f3x_summary = None
+        request = self.context.get("request", None)
+        if request:
+            f3x_summary = request.query_params.get("f3x_summary")
+
+        if f3x_summary == None:
+            queryset = self.queryset
+        else:
+            if isinstance(queryset, QuerySet):
+                queryset = queryset.all().filter(
+                    f3x_summary_form=f3x_summary
+                )
+        return queryset
 
     queryset = SchATransaction.objects.all().order_by("-id")
     serializer_class = SchATransactionSerializer
