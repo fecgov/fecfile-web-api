626492fca52b52a2652df46a72ecac584e065e3a
Elaine Krauss <elaine@ekrauss.com>
2022-05-24 09:22:24 -0400

    WIP unit test

--- django-backend/fecfiler/scha_transactions/test_models.py
+++ django-backend/fecfiler/scha_transactions/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from .models import SchATransaction
-
+from ..f3x_summaries.models import F3XSummary
+from ..committee_accounts.models import CommitteeAccount
 
 class SchATransactionTestCase(TestCase):
     fixtures = ["test_committee_accounts", "test_scha_transactions"]
@@ -14,12 +15,22 @@ class SchATransactionTestCase(TestCase):
             contributor_organization_name="John Smith & Co.",
             contributor_first_name="First",
             contributor_last_name="Last",
-            committee_account_id=1000,
+            committee_account_id=self.committee,
         )
 
     def test_get_scha_transaction(self):
         scha_transaction = SchATransaction.objects.get(contributor_last_name="Smith")
         self.assertEquals(scha_transaction.contributor_first_name, "John")
+
+		def test_get_scha_transaction_in_f3x_summary(self):
+				committee = CommitteeAccount()
+				f3x = F3XSummary(
+					committee_account=committee
+				)
+				trans = SchATransaction(
+					committee_account=committee,
+					f3x_summary=f3x
+				)
 
     def test_save_and_delete(self):
         self.sa_trans.save()
