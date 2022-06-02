85d9e314d2948396312ce78ab0620ff1c0fe7eef
David Heitzer <david.heitzer@tcg.com>
2022-06-02 16:13:41 -0400

    Integrate loginData for login.gov approach

--- django-backend/fecfiler/authentication/migrations/0001_initial.py
+++ django-backend/fecfiler/authentication/migrations/0001_initial.py
@@ -8,10 +8,6 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ('auth', '0012_alter_user_first_name_max_length'),
-    ]
-
     operations = [
         migrations.CreateModel(
             name='Account',
--- django-backend/fecfiler/authentication/views.py
+++ django-backend/fecfiler/authentication/views.py
@@ -6,6 +6,8 @@ class LoginDotGovSuccessSpaRedirect(View):
     def get(self, request, *args, **kwargs):
         redirect = HttpResponseRedirect("http://localhost:4200/dashboard")
         redirect.set_cookie("ffapi_jwt", self.request.session.get("oidc_id_token"))
+        redirect.set_cookie("ffapi_committee_id", request.user.cmtee_id)
+        redirect.set_cookie("ffapi_email", request.user.email)
         return redirect
 
 class LoginDotGovSuccessLogoutSpaRedirect(View):
