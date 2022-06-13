feefe8913d8e7e1f58975a95749b49d721a30966
Elaine Krauss <Elaine@Krauss@tcg.com>
2022-06-13 16:13:26 -0400

    Integrates more feedback from CircleCI's linting

--- django-backend/fecfiler/authentication/views.py
+++ django-backend/fecfiler/authentication/views.py
@@ -12,11 +12,11 @@ class AccountViewSet(GenericViewSet, ListModelMixin):
         The Account ViewSet allows the user to retrieve the users in the same committee
 
         The CommitteeOwnedViewset could not be inherited due to the different structure
-        of a user object versus other objects.  
+        of a user object versus other objects.
             (IE - having a "cmtee_id" field instead of "committee_id")
     """
     queryset = Account.objects.all().order_by("id")
     serializer_class = AccountSerializer
 
     def get_queryset(self):
-        return self.queryset.filter(cmtee_id = self.request.user.cmtee_id)
+        return self.queryset.filter(cmtee_id=self.request.user.cmtee_id)
