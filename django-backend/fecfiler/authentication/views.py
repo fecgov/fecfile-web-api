2e7a545b01890188e44876fe97f85ca90d32e531
Elaine Krauss <Elaine@Krauss@tcg.com>
2022-06-14 15:23:31 -0400

    Adds the necessary sorting capability to the Account viewset

--- django-backend/fecfiler/authentication/views.py
+++ django-backend/fecfiler/authentication/views.py
@@ -1,5 +1,8 @@
+from rest_framework import filters
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
+from django.db.models import Value, CharField
+from django.db.models.functions import Concat
 from .models import Account
 from .serializers import AccountSerializer
 import logging
@@ -15,8 +18,11 @@ class AccountViewSet(GenericViewSet, ListModelMixin):
         of a user object versus other objects.
             (IE - having a "cmtee_id" field instead of "committee_id")
     """
-    queryset = Account.objects.all().order_by("id")
+    queryset = Account.objects.annotate(name=Concat('first_name', Value(', '), 'last_name', output_field=CharField())).all()
     serializer_class = AccountSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ["last_name", "first_name", "id", "email", "role", "is_active", "name"]
+    ordering = ["name"]
 
     def get_queryset(self):
         return self.queryset.filter(cmtee_id=self.request.user.cmtee_id)
