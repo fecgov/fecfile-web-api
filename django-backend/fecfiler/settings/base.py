32be5cb077e45be2b3383cd432cf8196e873ab51
toddlees <todd.lees@tcg.com>
2022-06-27 22:03:20 -0400

    working local django-redis-celery setup

--- django-backend/fecfiler/settings/base.py
+++ django-backend/fecfiler/settings/base.py
@@ -192,8 +192,8 @@ LOGGING = {
 
 """Celery configurations
 """
-CELERY_BROKER_URL = "redis://practical_noether:6379"
-CELERY_RESULT_BACKEND = "redis://practical_noether:6379"
+CELERY_BROKER_URL = os.environ.get("REDIS_URL")
+CELERY_RESULT_BACKEND = os.environ.get("REDIS_URL")
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_RESULT_SERIALIZER = "json"
 CELERY_TASK_SERIALIZER = "json"
