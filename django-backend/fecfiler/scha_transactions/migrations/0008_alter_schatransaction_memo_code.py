c26a57874a24a1386a0cbdd807a38ca81bf1b172
toddlees <todd.lees@tcg.com>
2022-06-30 15:18:25 -0400

    noqa can't cover multiple lines

--- django-backend/fecfiler/scha_transactions/migrations/0008_alter_schatransaction_memo_code.py
+++ django-backend/fecfiler/scha_transactions/migrations/0008_alter_schatransaction_memo_code.py
@@ -4,9 +4,7 @@ from django.db import migrations, models
 
 
 def convert_fecfile_booleans(apps, schema_editor):
-    SchATransaction = apps.get_model(
-        "scha_transactions", "SchATransaction"
-    )  # noqa N806
+    SchATransaction = apps.get_model("scha_transactions", "SchATransaction")  # noqa
     for transaction in SchATransaction.objects.all():
         transaction.memo_code = (
             True if transaction.memo_code in ["true", "X"] else False
