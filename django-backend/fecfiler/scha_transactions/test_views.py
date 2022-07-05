5be97d11243279f4c62c270c9399b154ce3d9cac
Elaine Krauss <Elaine@Krauss@tcg.com>
2022-07-05 16:13:38 -0400

    Incorporates Linting Feedback

--- django-backend/fecfiler/scha_transactions/test_views.py
+++ django-backend/fecfiler/scha_transactions/test_views.py
@@ -5,7 +5,8 @@ from ..authentication.models import Account
 
 
 class SchATransactionsViewTest(TestCase):
-    fixtures = ["test_committee_accounts", "test_f3x_summaries", "test_scha_transactions", "test_accounts"]
+    fixtures = ["test_committee_accounts", "test_f3x_summaries",
+                "test_scha_transactions", "test_accounts"]
 
     def setUp(self):
         self.f3x = 1
@@ -21,13 +22,13 @@ class SchATransactionsViewTest(TestCase):
     def test_view_url_exists_at_desired_location(self):
         request = self.factory.get(f'/api/v1/sch-a-transactions/?report_id={self.f3x}')
         request.user = self.user
-        response = SchATransactionViewSet.as_view({"get":"list"})(request)
+        response = SchATransactionViewSet.as_view({"get": "list"})(request)
         self.assertEqual(response.status_code, 200)
 
     def test_is_paginated(self):
         request = self.factory.get(f'/api/v1/sch-a-transactions/?report_id={self.f3x}')
         request.user = self.user
-        response = SchATransactionViewSet.as_view({"get":"list"})(request)
+        response = SchATransactionViewSet.as_view({"get": "list"})(request)
 
         response.render()
         parsed_response = response.data
@@ -42,7 +43,7 @@ class SchATransactionsViewTest(TestCase):
                     f'/api/v1/sch-a-transactions/?report_id={self.f3x_2}']:
             request = self.factory.get(url)
             request.user = self.user
-            response = SchATransactionViewSet.as_view({"get":"list"})(request)
+            response = SchATransactionViewSet.as_view({"get": "list"})(request)
 
             response.render()
             responses.append(response.data)
