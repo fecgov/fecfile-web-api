f312d9eebb6356de7cf1bead21533bdd7636744c
toddlees <todd.lees@tcg.com>
2022-07-08 13:20:43 -0400

    refactor serializer to be more readable and extensable

---
+++ django-backend/fecfiler/web_services/dot_fec_serializer.py
@@ -0,0 +1,88 @@
+from django.db import models
+from fecfile_validate import validate
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def boolean_serializer(model_instance, field_name):
+    return "X" if getattr(model_instance, field_name, None) else ""
+
+
+def date_serializer(model_instance, field_name):
+    date = getattr(model_instance, field_name, None)
+    return date.strftime("%Y%m%d") if date else ""
+
+
+def foreign_key_serializer(model_instance, field_name):
+    """returns value of foreign key rather than the object it points to
+    For example: we have a foreign key for `report_code`.  django stores the key
+    in `report_code_id` and joins the `report_code_label` row into `report_code`.
+    Because we just want to write the key ("Q1" or something) we have to add "_id"
+    to the field name.
+    """
+    return getattr(model_instance, field_name + "_id", "")
+
+
+def default_serializer(model_instance, field_name):
+    """For most field types, just stringifying the value will work.
+    In the case where the field is None, we want empty string rather than
+    "None", thus the falsy condition
+    """
+    value = getattr(model_instance, field_name)
+    return str(value) if value else ""
+
+
+"""A map of model field types to their serializers.
+Pass the model instance and field name into the serializer to
+get a string representation in the FEC standard
+"""
+FIELD_SERIALIZERS = {
+    models.BooleanField: boolean_serializer,
+    models.DateField: date_serializer,
+    models.ForeignKey: foreign_key_serializer,
+    None: default_serializer,
+}
+
+
+def serialize_field(model, model_instance, field_name):
+    """Serialize field to string in FEC standard
+    Args:
+        model (class): Django model class that `model_instance` is an
+        instance of.  We retrieve the field type from this model to determine
+        which serializer to use.
+        model_instance (django.db.models.Model): Instance of `model` that contains
+        field to serialize.  In some cases when serializing a field we need to reference
+        another field in the `model_instance`, so we pass it to the serializer.
+        field_name (str): name of field to serialize
+    """
+    field = model._meta.get_field(field_name)
+    serializer = FIELD_SERIALIZERS.get(type(field), FIELD_SERIALIZERS[None])
+    return serializer(model_instance, field_name)
+
+
+def serialize_model_instance(schema_name, model, model_instance):
+    """Serialize model instance into row of FEC standard
+    Args:
+        schema_name (str): name of schema. the schema informas column layout
+        model (class): Django model class that `model_instance` is an
+        instance of.  We retrieve the field type from this model to determine
+        which serializer to use for each field.
+        model_instance (django.db.models.Model): Instance of `model` that contains
+        field to serialize.
+    """
+    schema = validate.get_schema(schema_name)
+    schema_properties = schema.get("properties", {}).items()
+    column_sequences = {
+        v.get("fec_spec", {}).get("COL_SEQ", None): k for k, v in schema_properties
+    }
+    row_length = max(column_sequences.keys())
+
+    """NOTE: column_index + 1 because FEC schemas define column sequences
+    starting at 1"""
+    row = [
+        serialize_field(model, model_instance, column_sequences[column_index + 1])
+        for column_index in range(0, row_length)
+    ]
+    return ",".join(row)
--- django-backend/fecfiler/web_services/tasks.py
+++ django-backend/fecfiler/web_services/tasks.py
@@ -1,28 +1,10 @@
 from celery import shared_task
 from fecfiler.f3x_summaries.models import F3XSummary
-from fecfile_validate import validate
-from django.db import models
+from .dot_fec_serializer import serialize_model_instance
 
 import logging
 
 logger = logging.getLogger(__name__)
-
-F3X_SCHEMA = validate.get_schema("F3X")
-F3X_PROPERTIES = F3X_SCHEMA.get("properties", {}).items()
-F3X_COLUMNS = {v.get("fec_spec", {}).get("COL_SEQ", None): k for k, v in F3X_PROPERTIES}
-F3X_NUMBER_OF_COLUMNS = max(F3X_COLUMNS.keys())
-SERIALIZERS = {
-    models.BooleanField: lambda b, o, fn: "X" if b else "",
-    models.DateField: lambda d, o, fn: d.strftime("%Y%m%d") if d else "",
-    models.ForeignKey: lambda fo, o, fn: getattr(o, fn + "_id", ""),
-    None: lambda v, o, fn: str(v) if v else "",
-}
-
-
-def serialize_field(object, field_name, model):
-    field = model._meta.get_field(field_name)
-    serializer = SERIALIZERS.get(type(field), SERIALIZERS[None])
-    return serializer(getattr(object, field_name, None), object, field_name)
 
 
 def serialize_f3x_summary(report_id):
@@ -30,11 +12,7 @@ def serialize_f3x_summary(report_id):
     if f3x_summary_result.exists():
         logger.info(f"serializing f3x summary: {report_id}")
         f3x_summary = f3x_summary_result.first()
-        row = [
-            serialize_field(f3x_summary, F3X_COLUMNS[column_index], F3XSummary)
-            for column_index in range(1, F3X_NUMBER_OF_COLUMNS + 1)
-        ]
-        return ",".join(row)
+        return serialize_model_instance("F3X", F3XSummary, f3x_summary)
     else:
         raise Exception(f"report: {report_id} not found")
 
