341cadea817abc861de32ec499e3b3f1992953d8
David Heitzer <david.heitzer@tcg.com>
2022-07-12 10:28:19 -0400

    allow cors controlled origins

--- django-backend/fecfiler/settings/base.py
+++ django-backend/fecfiler/settings/base.py
@@ -102,8 +102,7 @@ TEMPLATES = [
     },
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:4200",
-                        os.environ.get("FRONTEND_URL", "http://localhost:4200")]
+CORS_ALLOWED_ORIGINS = ["fecfile-web-app-dev.app.cloud.gov"]
 CORS_ALLOW_HEADERS = default_headers + ("enctype", "token")
 
 CORS_ALLOW_CREDENTIALS = True
