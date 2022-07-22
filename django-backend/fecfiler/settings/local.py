90d22ef07b743daa087591d2e9550020375830b4
toddlees <todd.lees@tcg.com>
2022-07-22 11:43:31 -0400

    remove unecessary settings in local

--- django-backend/fecfiler/settings/local.py
+++ django-backend/fecfiler/settings/local.py
@@ -2,8 +2,6 @@ from .base import *  # NOSONAR # noqa F401, F403
 
 # These settings are for local development only.
 
-CELERY_WORKER_STORAGE = CELERY_STORAGE_TYPE.LOCAL
-
 try:
     from .local import *  # NOSONAR # noqa F401, F403
 except ImportError:
