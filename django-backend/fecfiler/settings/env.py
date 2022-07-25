cca9b5bd5feaf36c49f09bcff51219f01aaf09dd
toddlees <todd.lees@tcg.com>
2022-07-25 11:37:32 -0400

    start process for api and worker

--- django-backend/fecfiler/settings/env.py
+++ django-backend/fecfiler/settings/env.py
@@ -3,7 +3,7 @@ import os
 
 env = cfenv.AppEnv()
 
-redis = env.get_service(name="fecfile-api-s3")
+redis = env.get_service(name="fecfile-api-redis")
 s3 = env.get_service(name="fecfile-api-s3")
 
 if redis:
@@ -12,5 +12,5 @@ if redis:
 if s3:
     os.environ["AWS_ACCESS_KEY_ID"] = s3.credentials.get("access_key_id")
     os.environ["AWS_SECRET_ACCESS_KEY"] = s3.credentials.get("secret_access_key")
-    os.environ["AWS_STORAGE_BUCKET_NAME"] = s3.credentials.get("storage_bucket_name")
+    os.environ["AWS_STORAGE_BUCKET_NAME"] = s3.credentials.get("bucket")
     os.environ["AWS_REGION"] = s3.credentials.get("region")
