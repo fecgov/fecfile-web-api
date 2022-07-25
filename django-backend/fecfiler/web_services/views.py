735f62e1a9c01ba0b47e64dfb8be9a978bc151be
toddlees <todd.lees@tcg.com>
2022-07-25 18:22:47 -0400

    debug service connections

--- django-backend/fecfiler/web_services/views.py
+++ django-backend/fecfiler/web_services/views.py
@@ -7,6 +7,11 @@ from .serializers import ReportIdSerializer
 from .renderers import DotFECRenderer
 from .web_service_storage import get_file
 from .models import DotFEC
+from fecfiler.settings import CELERY_BROKER_URL
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class WebServicesViewSet(viewsets.ViewSet):
@@ -23,7 +28,10 @@ class WebServicesViewSet(viewsets.ViewSet):
     def create_dot_fec(self, request):
         serializer = ReportIdSerializer(data=request.data, context={"request": request})
         if not serializer.is_valid():
+            logger.error(f"Create .FEC: {serializer.errors}")
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        logger.info(f"Create .FEC starting celery task: {CELERY_BROKER_URL}")
         create_dot_fec.delay(serializer.validated_data["report_id"])
         return Response({"status": ".FEC task created"})
 
@@ -39,10 +47,12 @@ class WebServicesViewSet(viewsets.ViewSet):
             report__id=report_id, report__committee_account__committee_id=committee_id
         ).order_by("-file_name")
         if not dot_fec_record.exists():
+            logger.error(f"No .FEC was found for report id: {report_id}")
             return Response(
                 f"No .FEC was found for report id: {report_id}",
                 status=status.HTTP_400_BAD_REQUEST,
             )
         file_name = dot_fec_record.first().file_name
+        logger.info(f".FEC was found for report id: {report_id}")
         file = get_file(file_name)
         return Response(FileWrapper(file))
