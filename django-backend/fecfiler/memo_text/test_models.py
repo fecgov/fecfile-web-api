f10fd4b6edfcd1f274ba252b55a9ebf9ff563076
David Heitzer <david.heitzer@tcg.com>
2022-07-26 11:13:46 -0400

    test fixes

--- django-backend/fecfiler/memo_text/test_models.py
+++ django-backend/fecfiler/memo_text/test_models.py
@@ -16,8 +16,8 @@ class MemoTextTestCase(TestCase):
         )
 
     def test_get_memo_text(self):
-        memo_text = MemoText.objects.get(id=29)
-        self.assertEquals(memo_text.text4000, "tessst4")
+        memo_text = MemoText.objects.get(id=25)
+        self.assertEquals(memo_text.text4000, "dahtest2")
 
     def test_save_and_delete(self):
         self.valid_memo_text.save()
@@ -26,9 +26,4 @@ class MemoTextTestCase(TestCase):
         )
         self.assertIsInstance(memo_text_from_db, MemoText)
         self.assertEquals(memo_text_from_db.id, 30)
-        memo_text_from_db.delete()
-        self.assertRaises(
-            MemoText.DoesNotExist,
-            MemoText.objects.get,
-            id=30,
-        )
+        self.assertEquals(memo_text_from_db.text4000, "tessst4")
