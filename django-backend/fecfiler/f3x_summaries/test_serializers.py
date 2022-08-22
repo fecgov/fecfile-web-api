40a9705cb88fd7d372fc3cb3451b4e7bd69f6013
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-22 17:19:14 -0400

    WIP adding unit tests

--- django-backend/fecfiler/f3x_summaries/test_serializers.py
+++ django-backend/fecfiler/f3x_summaries/test_serializers.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from .serializers import F3XSummarySerializer
 from fecfiler.authentication.models import Account
 from rest_framework.request import Request, HttpRequest
@@ -14,6 +15,9 @@ class F3XSerializerTestCase(TestCase):
             "treasurer_last_name": "Validlastname",
             "treasurer_first_name": "Validfirstname",
             "date_signed": "2022-01-01",
+            "upload_submission":{
+                "fec_status":"ACCEPTED"
+            }
         }
 
         self.invalid_f3x_summary = {
@@ -40,3 +44,22 @@ class F3XSerializerTestCase(TestCase):
         self.assertFalse(invalid_serializer.is_valid())
         self.assertIsNotNone(invalid_serializer.errors["form_type"])
         self.assertIsNotNone(invalid_serializer.errors["treasurer_first_name"])
+
+    def test_get_submission_status(self):
+        f3x_summary = {
+            "form_type": "F3XN",
+            "filer_committee_id_number": "C00123456",
+            "treasurer_last_name": "Validlastname",
+            "treasurer_first_name": "Validfirstname",
+            "date_signed": "2022-01-01",
+            "upload_submission":{
+                "fec_status":"ACCEPTED"
+            }
+        }
+        valid_serializer = F3XSummarySerializer(
+            data=f3x_summary,
+            context={"request": self.mock_request},
+        )
+
+        valid_serializer.is_valid()
+        self.assertEqual(valid_serializer.data, 'In-Progress')
