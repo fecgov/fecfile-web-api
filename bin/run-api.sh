17fc615c1be119f34621df7d54ce2497f6164e69
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-25 14:55:46 -0400

    Makes running the application conditional upon the success of the migrations

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -1,7 +1,4 @@
 cd django-backend
 
-# Run migrations
-./manage.py migrate --noinput --check --traceback > migrate.out
-
-# Run application
-python wait_for_db.py && gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 -t 200
+# Run migrations and application
+./manage.py migrate --noinput --check --traceback > migrate.out && python wait_for_db.py && gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 -t 200
