28f320bc3cd83c332e78c51ddea7c3c7a9c23f77
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-25 14:15:56 -0400

    Updates the run script to add the check and traceback flags

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -1,7 +1,7 @@
 cd django-backend
 
 # Run migrations
-./manage.py migrate --noinput > migrate.out
+./manage.py migrate --noinput --check --traceback > migrate.out
 
 # Run application
 python wait_for_db.py && gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 -t 200
