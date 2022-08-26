de69d66b9691fea44091d1ee0d2b903e8fb5daba
Elaine Krauss <elaine.krauss@tcg.com>
2022-08-26 10:21:26 -0400

    Removes the intentionally failing migration.  Migration cannot fail silently on deploy any more.  Migration *can* still fail silently on CircleCI

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -1,4 +1,6 @@
 cd django-backend
 
 # Run migrations and application
-./manage.py migrate --noinput --check --traceback > migrate.out && python wait_for_db.py && gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 -t 200
+./manage.py migrate --no-input --traceback --verbosity 3 > migrate.out &&
+	python wait_for_db.py && 
+	gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 -t 200
