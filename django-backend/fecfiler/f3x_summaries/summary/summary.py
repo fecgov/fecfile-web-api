c01fd9b3184a95db4eda0f65fce97efcda59e4c0
toddlees <todd.lees@tcg.com>
2022-08-31 16:35:29 -0400

    tests for calculate summary

--- django-backend/fecfiler/f3x_summaries/summary/summary.py
+++ django-backend/fecfiler/f3x_summaries/summary/summary.py
@@ -1,6 +1,8 @@
+from decimal import Decimal
 from fecfiler.f3x_summaries.models import F3XSummary
 from fecfiler.scha_transactions.models import SchATransaction
 from django.db.models import Q, Sum
+from django.db.models.functions import Coalesce
 
 
 class SummaryService:
@@ -11,6 +13,12 @@ class SummaryService:
         report_transactions = SchATransaction.objects.filter(report=self.report)
         # just line 15
         sa15_query = Q(form_type="SA15", memo_code=False)
-        line_15 = Sum("contribution_amount", filter=sa15_query)
+        line_15 = self._create_contribution_sum(sa15_query)
         summary = report_transactions.aggregate(line_15=line_15)
         return summary
+
+    def _create_contribution_sum(self, query):
+        return Coalesce(
+            Sum("contribution_amount", filter=query),
+            Decimal(0.0),
+        )
--- django-backend/fecfiler/f3x_summaries/summary/test_summary.py
+++ django-backend/fecfiler/f3x_summaries/summary/test_summary.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.test import TestCase
 from fecfiler.f3x_summaries.models import F3XSummary
 from .summary import SummaryService
@@ -17,4 +18,10 @@ class F3XSerializerTestCase(TestCase):
         f3x = F3XSummary.objects.get(id=9999)
         summary_service = SummaryService(f3x)
         summary = summary_service.calculate_summary()
-        self.assertEqual(summary["line_15"], 2125.79)
+        self.assertEqual(summary["line_15"], Decimal("2125.79"))
+
+    def test_report_with_no_transactions(self):
+        f3x = F3XSummary.objects.get(id=10000)
+        summary_service = SummaryService(f3x)
+        summary = summary_service.calculate_summary()
+        self.assertEqual(summary["line_15"], Decimal("0"))
