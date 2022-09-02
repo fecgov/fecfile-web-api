8c72868a7901b9af289f933cf51fa72ae7ecbf72
toddlees <todd.lees@tcg.com>
2022-09-02 09:24:20 -0400

    remove unused imports

--- django-backend/fecfiler/web_services/summary/test_tasks.py
+++ django-backend/fecfiler/web_services/summary/test_tasks.py
@@ -1,7 +1,5 @@
 from decimal import Decimal
 from django.test import TestCase
-from fecfiler.f3x_summaries.models import F3XSummary
-from .summary import SummaryService
 from .tasks import CalculationState, calculate_summary
 
 
