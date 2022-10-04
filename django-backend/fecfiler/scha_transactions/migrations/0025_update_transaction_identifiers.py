f6e1c0a23669709dfdb0a8a18f1b86d7aa5f08f7
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-10-04 09:55:46 -0400

    Linting updates

--- django-backend/fecfiler/scha_transactions/migrations/0025_update_transaction_identifiers.py
+++ django-backend/fecfiler/scha_transactions/migrations/0025_update_transaction_identifiers.py
@@ -4,50 +4,52 @@ from django.db import migrations
 
 
 def update_transaction_type_identifiers(apps, schema_editor):
-    SchATransaction = apps.get_model("scha_transactions", "SchATransaction")  # noqa
+    sch_a_transaction = apps.get_model("scha_transactions", "SchATransaction")  # noqa
 
     identifiers = {
-        "EAR_REC":"EARMARK_RECEIPT",
-        "TRIB_REC":"TRIBAL_RECEIPT",
-        "INDV_REC":"INDIVIDUAL_RECEIPT",
-        "OTH_REC":"OTHER_RECEIPT",
-        "JF_TRAN":"JOINT_FUNDRAISING_TRANSFER",
-        "JF_TRAN_PAC_MEMO":"JOINT_FUNDRAISING_TRANSFER_PAC_MEMO",
-        "OFFSET_TO_OPEX":"OFFSET_TO_OPERATING_EXPENDITURES",
+        "EAR_REC": "EARMARK_RECEIPT",
+        "TRIB_REC": "TRIBAL_RECEIPT",
+        "INDV_REC": "INDIVIDUAL_RECEIPT",
+        "OTH_REC": "OTHER_RECEIPT",
+        "JF_TRAN": "JOINT_FUNDRAISING_TRANSFER",
+        "JF_TRAN_PAC_MEMO": "JOINT_FUNDRAISING_TRANSFER_PAC_MEMO",
+        "OFFSET_TO_OPEX": "OFFSET_TO_OPERATING_EXPENDITURES",
     }
 
     purpose_descriptions = {
-        "JF":"Joint Fundraising",
+        "JF": "Joint Fundraising",
     }
 
-    for transaction in SchATransaction.objects.all():
+    for transaction in sch_a_transaction.objects.all():
         updated_tid = update_identifier(transaction, identifiers)
         updated_cpd = update_purpose_description(transaction, purpose_descriptions)
         if (updated_tid or updated_cpd):
             transaction.save()
+
 
 def reverse_tid_update(apps, schema_editor):
-    SchATransaction = apps.get_model("scha_transactions", "SchATransaction")  # noqa
+    sch_a_transaction = apps.get_model("scha_transactions", "SchATransaction")  # noqa
 
     identifiers = {
-        "EARMARK_RECEIPT":"EAR_REC",
-        "TRIBAL_RECEIPT":"TRIB_REC",
-        "INDIVIDUAL_RECEIPT":"INDV_REC",
-        "OTHER_RECEIPT":"OTH_REC",
-        "JOINT_FUNDRAISING_TRANSFER":"JF_TRAN",
-        "JOINT_FUNDRAISING_TRANSFER_PAC_MEMO":"JF_TRAN_PAC_MEMO",
-        "OFFSET_TO_OPERATING_EXPENDITURES":"OFFSET_TO_OPEX",
+        "EARMARK_RECEIPT": "EAR_REC",
+        "TRIBAL_RECEIPT": "TRIB_REC",
+        "INDIVIDUAL_RECEIPT": "INDV_REC",
+        "OTHER_RECEIPT": "OTH_REC",
+        "JOINT_FUNDRAISING_TRANSFER": "JF_TRAN",
+        "JOINT_FUNDRAISING_TRANSFER_PAC_MEMO": "JF_TRAN_PAC_MEMO",
+        "OFFSET_TO_OPERATING_EXPENDITURES": "OFFSET_TO_OPEX",
     }
 
     purpose_descriptions = {
-        "Joint Fundraising":"JF",
+        "Joint Fundraising": "JF",
     }
 
-    for transaction in SchATransaction.objects.all():
+    for transaction in sch_a_transaction.objects.all():
         updated_tid = update_identifier(transaction, identifiers)
         updated_cpd = update_purpose_description(transaction, purpose_descriptions)
         if (updated_tid or updated_cpd):
             transaction.save()
+
 
 def update_identifier(transaction, identifiers):
     if (transaction.transaction_type_identifier in identifiers.keys()):
@@ -55,6 +57,7 @@ def update_identifier(transaction, identifiers):
         transaction.transaction_type_identifier = new_identifier
         return True
     return False
+
 
 def update_purpose_description(transaction, purpose_desc):
     updated = False
