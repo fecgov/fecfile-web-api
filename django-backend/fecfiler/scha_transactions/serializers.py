11001d516a973fef5f080ed9bc2f96b86e140f65
David Heitzer <david.heitzer@tcg.com>
2022-10-12 16:27:41 -0400

    159 send contact with schatransaction

--- django-backend/fecfiler/scha_transactions/serializers.py
+++ django-backend/fecfiler/scha_transactions/serializers.py
@@ -1,8 +1,6 @@
 import logging
 
-from django.db import transaction
 from fecfiler.committee_accounts.serializers import CommitteeOwnedSerializer
-from fecfiler.contacts.models import Contact
 from fecfiler.validation import serializers
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import CharField, UUIDField
@@ -23,82 +21,6 @@ class SchATransactionSerializer(
     report_id = UUIDField(required=True, allow_null=False)
 
     contact_id = UUIDField(required=False)
-
-    def create(self, validated_data: dict):
-        with transaction.atomic():
-            self.create_or_update_contact_for_transaction(validated_data)
-            return super().create(validated_data)
-
-    def update(self, instance: Contact, validated_data: dict):
-        with transaction.atomic():
-            self.create_or_update_contact_for_transaction(validated_data)
-            return super().update(instance, validated_data)
-
-    def create_or_update_contact_for_transaction(self, transaction: dict):
-        contact_id = transaction.get("contact_id")
-        if contact_id:
-            contact = Contact.objects.get(id=contact_id)
-        else:
-            contact = Contact(
-                type=transaction['entity_type'],
-                country="USA",  # TODO confirm
-            )
-        self.update_contact_for_transaction(contact, transaction)
-
-    def update_contact_for_transaction(self, contact: Contact, transaction: dict):
-        if contact and transaction:
-            if ((contact.type == Contact.ContactType.INDIVIDUAL)
-                    and (transaction["entity_type"] == "IND")):
-                contributor_last_name = transaction.get("contributor_last_name")
-                if (contact.last_name != contributor_last_name):
-                    contact.last_name = contributor_last_name
-                contributor_first_name = transaction.get("contributor_first_name")
-                if (contact.first_name != contributor_first_name):
-                    contact.first_name = contributor_first_name
-                contributor_middle_name = transaction.get("contributor_middle_name")
-                if (contact.middle_name != contributor_middle_name):
-                    contact.middle_name = contributor_middle_name
-                contributor_prefix = transaction.get("contributor_prefix")
-                if (contact.prefix != contributor_prefix):
-                    contact.prefix = contributor_prefix
-                contributor_suffix = transaction.get("contributor_suffix")
-                if (contact.suffix != contributor_suffix):
-                    contact.suffix = contributor_suffix
-                contributor_employer = transaction.get("contributor_employer")
-                if (contact.employer != contributor_employer):
-                    contact.employer = contributor_employer
-                contributor_occupation = transaction.get("contributor_occupation")
-                if (contact.occupation != contributor_occupation):
-                    contact.occupation = contributor_occupation
-            elif ((contact.type == Contact.ContactType.COMMITTEE)
-                  and (transaction["entity_type"] == "COM")):
-                donor_committee_fec_id = transaction.get("donor_committee_fec_id")
-                if (contact.committee_id != donor_committee_fec_id):
-                    contact.committee_id = donor_committee_fec_id
-                contributor_organization_name = transaction.get(
-                    "contributor_organization_name")
-                if (contact.name != contributor_organization_name):
-                    contact.name = contributor_organization_name
-            elif ((contact.type == Contact.ContactType.ORGANIZATION)
-                  and (transaction["entity_type"] == "ORG")):
-                contributor_organization_name = transaction.get(
-                    "contributor_organization_name")
-                if (contact.name != contributor_organization_name):
-                    contact.name = contributor_organization_name
-            contributor_street_1 = transaction.get("contributor_street_1")
-            if (contact.street_1 != contributor_street_1):
-                contact.street_1 = contributor_street_1
-            contributor_street_2 = transaction.get("contributor_street_2")
-            if (contact.street_2 != contributor_street_2):
-                contact.street_2 = contributor_street_2
-            contributor_city = transaction.get("contributor_city")
-            if (contact.city != contributor_city):
-                contact.city = contributor_city
-            contributor_zip = transaction.get("contributor_zip")
-            if (contact.zip != contributor_zip):
-                contact.zip = contributor_zip
-            contact.save()
-            transaction["contact_id"] = contact.id
 
     def get_schema_name(self, data):
         transaction_type = data.get("transaction_type_identifier", None)
--- django-backend/fecfiler/scha_transactions/views.py
+++ django-backend/fecfiler/scha_transactions/views.py
@@ -1,10 +1,15 @@
-from rest_framework import filters
+from django.db import transaction
+from django.db.models import TextField, Value
+from django.db.models.functions import Coalesce, Concat
 from fecfiler.committee_accounts.views import CommitteeOwnedViewSet
+from fecfiler.contacts.models import Contact
+from fecfiler.contacts.serializers import ContactSerializer
 from fecfiler.f3x_summaries.views import ReportViewMixin
+from rest_framework import filters, status
+from rest_framework.response import Response
+
 from .models import SchATransaction
 from .serializers import SchATransactionSerializer
-from django.db.models import TextField, Value
-from django.db.models.functions import Concat, Coalesce
 
 
 class SchATransactionViewSet(CommitteeOwnedViewSet, ReportViewMixin):
@@ -46,3 +51,81 @@ class SchATransactionViewSet(CommitteeOwnedViewSet, ReportViewMixin):
         "contribution_amount",
     ]
     ordering = ["-id"]
+
+    def create(self, request, *args, **kwargs):
+        with transaction.atomic():
+            return self.create_or_update_contact(request, args, kwargs)
+
+    def update(self, request, *args, **kwargs):
+        with transaction.atomic():
+            return self.create_or_update_contact(request, args, kwargs)
+
+    def create_or_update_contact(self, request, *args, **kwargs):
+        scha_tran_serializer = SchATransactionSerializer(
+            data=request.data["transaction"], context={"request": request})
+        if not scha_tran_serializer.is_valid():
+            return Response(
+                scha_tran_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        scha_tran = SchATransaction(**scha_tran_serializer.validated_data)
+        tran_contact = scha_tran.contact
+        contact_id = None
+        if (tran_contact):
+            contact_id = tran_contact.id
+        if not contact_id:
+            contact_serializer = ContactSerializer(
+                data=request.data["contact"], context={"request": request})
+            if not contact_serializer.is_valid():
+                return Response(
+                    contact_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            contact = Contact(**contact_serializer.validated_data)
+        else:
+            contact = Contact.objects.get(id=contact_id)
+        self.update_contact_for_transaction(contact, scha_tran)
+        scha_tran_serializer.validated_data["contact_id"] = contact.id
+        if scha_tran.id:
+            scha_tran_serializer.update(
+                self.get_object(), scha_tran_serializer.validated_data)
+            return Response(scha_tran_serializer.data)
+        scha_tran_serializer.save()
+        headers = self.get_success_headers(scha_tran_serializer.data)
+        return Response(
+            scha_tran_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def update_contact_for_transaction(
+            self, contact: Contact, scha_tran: SchATransaction):
+        if contact and scha_tran:
+            if ((contact.type == Contact.ContactType.INDIVIDUAL)
+                    and (scha_tran.entity_type == "IND")):
+                if (contact.last_name != scha_tran.contributor_last_name):
+                    contact.last_name = scha_tran.contributor_last_name
+                if (contact.first_name != scha_tran.contributor_first_name):
+                    contact.first_name = scha_tran.contributor_first_name
+                if (contact.middle_name != scha_tran.contributor_middle_name):
+                    contact.middle_name = scha_tran.contributor_middle_name
+                if (contact.prefix != scha_tran.contributor_prefix):
+                    contact.prefix = scha_tran.contributor_prefix
+                if (contact.suffix != scha_tran.contributor_suffix):
+                    contact.suffix = scha_tran.contributor_suffix
+                if (contact.employer != scha_tran.contributor_employer):
+                    contact.employer = scha_tran.contributor_employer
+                if (contact.occupation != scha_tran.contributor_occupation):
+                    contact.occupation = scha_tran.contributor_occupation
+            elif ((contact.type == Contact.ContactType.COMMITTEE)
+                  and (scha_tran.entity_type == "COM")):
+                if (contact.committee_id != scha_tran.donor_committee_fec_id):
+                    contact.committee_id = scha_tran.donor_committee_fec_id
+                if (contact.name != scha_tran.contributor_organization_name):
+                    contact.name = scha_tran.contributor_organization_name
+            elif ((contact.type == Contact.ContactType.ORGANIZATION)
+                  and (scha_tran.entity_type == "ORG")):
+                if (contact.name != scha_tran.contributor_organization_name):
+                    contact.name = scha_tran.contributor_organization_name
+            if (contact.street_1 != scha_tran.contributor_street_1):
+                contact.street_1 = scha_tran.contributor_street_1
+            if (contact.street_2 != scha_tran.contributor_street_2):
+                contact.street_2 = scha_tran.contributor_street_2
+            if (contact.city != scha_tran.contributor_city):
+                contact.city = scha_tran.contributor_city
+            if (contact.zip != scha_tran.contributor_zip):
+                contact.zip = scha_tran.contributor_zip
+            contact.save()
