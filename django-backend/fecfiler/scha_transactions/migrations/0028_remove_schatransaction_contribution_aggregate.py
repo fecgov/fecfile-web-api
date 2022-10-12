1ff49e172f09aedbd2c3dff780a525aaa8406cf0
toddlees <todd.lees@tcg.com>
2022-10-12 10:17:38 -0400

    resolve another migration conflict

--- django-backend/fecfiler/scha_transactions/migrations/0028_remove_schatransaction_contribution_aggregate.py
+++ django-backend/fecfiler/scha_transactions/migrations/0028_remove_schatransaction_contribution_aggregate.py
@@ -6,7 +6,7 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("scha_transactions", "0025_update_transaction_identifiers"),
+        ("scha_transactions", "0027_scha_transaction_aggregation_group"),
     ]
 
     operations = [
