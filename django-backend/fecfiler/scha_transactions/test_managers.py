5f081c185ba5d83dd8ccc63025300aed1c105d86
toddlees <todd.lees@tcg.com>
2022-10-13 18:17:31 -0400

    unit tests

---
+++ django-backend/fecfiler/scha_transactions/test_managers.py
@@ -0,0 +1,27 @@
+from decimal import Decimal
+from django.test import TestCase
+from .models import SchATransaction
+
+
+class SchATransactionTestCase(TestCase):
+    fixtures = [
+        "test_committee_accounts",
+        "test_contacts",
+        "test_manager_data",
+        "test_f3x_summaries",
+    ]
+
+    def setUp(self):
+        pass
+
+    def test_aggregate_one(self):
+        scha_transaction = SchATransaction.objects.get(transaction_id="1")
+        self.assertEquals(scha_transaction.contribution_aggregate, Decimal("1.01"))
+
+    def test_aggregate_two(self):
+        scha_transaction = SchATransaction.objects.get(transaction_id="2")
+        self.assertEquals(scha_transaction.contribution_aggregate, Decimal("3.03"))
+
+    def test_aggregate_three(self):
+        scha_transaction = SchATransaction.objects.get(transaction_id="3")
+        self.assertEquals(scha_transaction.contribution_aggregate, Decimal("6.06"))
