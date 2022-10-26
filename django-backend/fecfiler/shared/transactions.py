f713b70253a193f06a29afd394bb2906001b20e3
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-10-26 16:45:21 -0400

    Adds the shared app and get_from_sched_tables_by_uuid()

---
+++ django-backend/fecfiler/shared/transactions.py
@@ -0,0 +1,13 @@
+from scha_transactions.models import SchATransaction
+
+def get_from_sched_tables_by_uuid(uuid):
+    tables = [SchATransaction]
+    matches = []
+    for table in tables:
+        matches += list(table.objects.filter(id=uuid))
+    
+    if len(matches) > 1:
+      raise LookupError
+    if len(matches) == 1:
+      return matches[0]
+    return None
