bde376de39ff3766781e3a650d3f1ff48925140e
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-11-03 10:56:57 -0400

    Updates memo text id number generation

--- django-backend/fecfiler/memo_text/models.py
+++ django-backend/fecfiler/memo_text/models.py
@@ -19,6 +19,7 @@ class MemoText(SoftDeleteModel, CommitteeOwnedModel, ReportMixin):
     )
     rec_type = models.TextField(null=True, blank=True)
     filer_committee_id_number = models.TextField(null=True, blank=True)
+    is_report_level_memo = models.BooleanField(null=False, blank=False, default=True)
     transaction_id_number = models.TextField(null=True, blank=True)
     transaction_uuid = models.TextField(null=True, blank=True)
     back_reference_tran_id_number = models.TextField(null=True, blank=True)
@@ -30,8 +31,17 @@ class MemoText(SoftDeleteModel, CommitteeOwnedModel, ReportMixin):
 
     # This is intended to be useable without instantiating a transaction object
     @staticmethod
-    def check_for_uid_conflicts(uid):  # noqa
-        return len(MemoText.objects.filter(transaction_id_number=uid)) > 0
+    def check_for_uid_conflicts(uid, report_id):  # noqa
+        return len(MemoText.objects.filter(
+            transaction_id_number=uid,
+            report_id=report_id
+        )) > 0
+
+    def generate_report_id(self):
+        report_memos = MemoText.objects.filter(
+            report_id=self.report_id
+        )
+        return "REPORT_MEMO_TEXT"+str(len(report_memos)+1)
 
     def generate_uid(self):
         unique_id = uuid.uuid4()
@@ -41,11 +51,14 @@ class MemoText(SoftDeleteModel, CommitteeOwnedModel, ReportMixin):
         return hex_id[-21] + hex_id[-19:]
 
     def generate_unique_transaction_id(self):
-        unique_id = self.generate_uid()
+        id_generator = self.generate_uid
+        if self.is_report_level_memo:
+            id_generator = self.generate_report_id
+        unique_id = id_generator()
 
         attempts = 0
-        while MemoText.check_for_uid_conflicts(unique_id):
-            unique_id = self.generate_uid()
+        while MemoText.check_for_uid_conflicts(unique_id, self.report_id):
+            unique_id = id_generator()
             attempts += 1
             logger.info("Transaction unique ID generation: collision detected")
             if attempts > 5:
