959c175a73e286672b3f8898689afc17a162823c
Elaine Krauss <Elaine.Krauss@tcg.com>
2022-12-13 14:48:26 -0500

    Adds new transaction

--- django-backend/fecfiler/scha_transactions/managers.py
+++ django-backend/fecfiler/scha_transactions/managers.py
@@ -55,6 +55,7 @@ class SchATransactionManager(SoftDeleteManager):
             "INDIVIDUAL_NATIONAL_PARTY_RECOUNT_JF_TRANSFER_MEMO",
             "TRIBAL_NATIONAL_PARTY_RECOUNT_JF_TRANSFER_MEMO",
             "INDIVIDUAL_RECEIPT_NON_CONTRIBUTION_ACCOUNT",
+            "INDIVIDUAL_NATIONAL_PARTY_HEADQUARTERS_ACCOUNT",
         ]
         return Case(
             When(contribution_aggregate__lt=Value(Decimal(0)), then=Value(True)),
