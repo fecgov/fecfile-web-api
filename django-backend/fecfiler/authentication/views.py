3ba807211496b219e8ec175505d302962bd7e276
David Heitzer <david.heitzer@tcg.com>
2022-12-14 11:21:14 -0500

    207 cr fixes

--- django-backend/fecfiler/authentication/views.py
+++ django-backend/fecfiler/authentication/views.py
@@ -69,40 +69,6 @@ class AccountViewSet(GenericViewSet, ListModelMixin):
         return queryset
 
 
-# class AccountViewSet(viewsets.ModelViewSet):
-#     lookup_field = "username"
-#     serializer_class = AccountSerializer
-
-#     def get_queryset(self):
-#         queryset = Account.objects.all()
-#         queryset = queryset.filter(self.request.user)
-#         serializer_class = AccountSerializer(Account, many=True)
-#         return JsonResponse(serializer_class.data, safe=False)
-
-#     def get_permissions(self):
-#         if self.request.method in permissions.SAFE_METHODS:
-#             return (permissions.AllowAny(),)
-#         if self.request.method == "POST":
-#             return (permissions.AllowAny(),)
-#         return permissions.IsAuthenticated(), IsAccountOwner()
-
-#     def create(self, request):
-#         serializer = self.serializer_class(data=request.data)
-
-#         if serializer.is_valid():
-#             Account.objects.create_user(**serializer.validated_data)
-#             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
-
-#         return Response(
-#             {
-#                 "status": "Bad request",
-#                 "message": "Account could not be created with received data.",
-#                 "details": str(serializer.errors),
-#             },
-#             status=status.HTTP_400_BAD_REQUEST,
-#         )
-
-
 class LoginDotGovSuccessSpaRedirect(View):
     def get(self, request, *args, **kwargs):
         request.session["user_id"] = request.user.pk
