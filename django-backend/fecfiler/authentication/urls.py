a9c99d15d6aca24943a762f1a226cba4abb871fe
Matt Travers <matt.travers@tcg.com>
2022-12-15 15:04:42 -0500

    Rearanged authentication viewset code

--- django-backend/fecfiler/authentication/urls.py
+++ django-backend/fecfiler/authentication/urls.py
@@ -1,28 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    LoginDotGovSuccessSpaRedirect,
-    LoginDotGovSuccessLogoutSpaRedirect,
-    authenticate_login,
-    LogoutView,
-)
+from .views import AccountViewSet, authenticate_login
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path("auth/logout/", LogoutView.as_view(), name="logout"),
-    path(
-        "auth/login-redirect",
-        LoginDotGovSuccessSpaRedirect.as_view(),
-        name="login-redirect",
-    ),
-    path(
-        "auth/logout-redirect",
-        LoginDotGovSuccessLogoutSpaRedirect.as_view(),
-        name="logout-redirect",
-    ),
+    path("auth/logout/", AccountViewSet.as_view({"post": "logout"})),
+    path("auth/login-redirect", AccountViewSet.as_view({"get": "login_redirect"}),),
+    path("auth/logout-redirect", AccountViewSet.as_view({"get": "logout_redirect"}),),
     path("", include(router.urls)),
-    path("user/login/authenticate", authenticate_login, name="login_authenticate"),
+    path("user/login/authenticate", authenticate_login),
 ]
--- django-backend/fecfiler/authentication/views.py
+++ django-backend/fecfiler/authentication/views.py
@@ -1,9 +1,9 @@
-from django.views.generic import View
 from django.http import HttpResponseRedirect
 from django.contrib.auth import authenticate, logout, login
 from rest_framework.decorators import (
     authentication_classes,
     permission_classes,
+    action,
     api_view,
 )
 from fecfiler.settings import (
@@ -18,7 +18,7 @@ from fecfiler.settings import (
 )
 
 from rest_framework.response import Response
-from rest_framework import filters, permissions, views, status
+from rest_framework import filters, status
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.mixins import ListModelMixin
 from django.db.models import Value, CharField
@@ -31,6 +31,55 @@ from django.http import JsonResponse
 import logging
 
 logger = logging.getLogger(__name__)
+
+
+def login_dot_gov_logout(request):
+    client_id = OIDC_RP_CLIENT_ID
+    post_logout_redirect_uri = LOGOUT_REDIRECT_URL
+    state = request.get_signed_cookie("oidc_state")
+
+    params = {
+        "client_id": client_id,
+        "post_logout_redirect_uri": post_logout_redirect_uri,
+        "state": state,
+    }
+    query = urlencode(params)
+    op_logout_url = OIDC_OP_LOGOUT_ENDPOINT
+    redirect_url = "{url}?{query}".format(url=op_logout_url, query=query)
+
+    return redirect_url
+
+
+def generate_username(uuid):
+    return uuid
+
+
+def update_last_login_time(account):
+    account.last_login = datetime.now()
+    account.save()
+
+
+def handle_valid_login(account):
+    update_last_login_time(account)
+
+    logger.debug("Successful login: {}".format(account))
+    return JsonResponse(
+        {"is_allowed": True, "committee_id": account.cmtee_id, "email": account.email},
+        status=200,
+        safe=False,
+    )
+
+
+def handle_invalid_login(username):
+    logger.debug("Unauthorized login attempt: {}".format(username))
+    return JsonResponse(
+        {
+            "is_allowed": False,
+            "status": "Unauthorized",
+            "message": "ID/Password combination invalid.",
+        },
+        status=401,
+    )
 
 
 class AccountViewSet(GenericViewSet, ListModelMixin):
@@ -68,9 +117,8 @@ class AccountViewSet(GenericViewSet, ListModelMixin):
 
         return queryset
 
-
-class LoginDotGovSuccessSpaRedirect(View):
-    def get(self, request, *args, **kwargs):
+    @action(detail=True, methods=["get"])
+    def login_redirect(self, request):
         request.session["user_id"] = request.user.pk
         redirect = HttpResponseRedirect(LOGIN_REDIRECT_CLIENT_URL)
         redirect.set_cookie(
@@ -87,9 +135,13 @@ class LoginDotGovSuccessSpaRedirect(View):
         )
         return redirect
 
+    @action(detail=True, methods=["post"])
+    def logout(self, request):
+        logout(request)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
 
-class LoginDotGovSuccessLogoutSpaRedirect(View):
-    def get(self, request, *args, **kwargs):
+    @action(detail=True, methods=["get"])
+    def logout_redirect(self, request):
         response = HttpResponseRedirect(LOGIN_REDIRECT_CLIENT_URL)
         response.delete_cookie(
             FFAPI_COMMITTEE_ID_COOKIE_NAME, domain=FFAPI_COOKIE_DOMAIN
@@ -97,55 +149,6 @@ class LoginDotGovSuccessLogoutSpaRedirect(View):
         response.delete_cookie(FFAPI_EMAIL_COOKIE_NAME, domain=FFAPI_COOKIE_DOMAIN)
         response.delete_cookie("csrftoken", domain=FFAPI_COOKIE_DOMAIN)
         return response
-
-
-def login_dot_gov_logout(request):
-    client_id = OIDC_RP_CLIENT_ID
-    post_logout_redirect_uri = LOGOUT_REDIRECT_URL
-    state = request.get_signed_cookie("oidc_state")
-
-    params = {
-        "client_id": client_id,
-        "post_logout_redirect_uri": post_logout_redirect_uri,
-        "state": state,
-    }
-    query = urlencode(params)
-    op_logout_url = OIDC_OP_LOGOUT_ENDPOINT
-    redirect_url = "{url}?{query}".format(url=op_logout_url, query=query)
-
-    return redirect_url
-
-
-def generate_username(uuid):
-    return uuid
-
-
-def update_last_login_time(account):
-    account.last_login = datetime.now()
-    account.save()
-
-
-def handle_invalid_login(username):
-    logger.debug("Unauthorized login attempt: {}".format(username))
-    return JsonResponse(
-        {
-            "is_allowed": False,
-            "status": "Unauthorized",
-            "message": "ID/Password combination invalid.",
-        },
-        status=401,
-    )
-
-
-def handle_valid_login(account):
-    update_last_login_time(account)
-
-    logger.debug("Successful login: {}".format(account))
-    return JsonResponse(
-        {"is_allowed": True, "committee_id": account.cmtee_id, "email": account.email},
-        status=200,
-        safe=False,
-    )
 
 
 @api_view(["POST", "GET"])
@@ -169,11 +172,3 @@ def authenticate_login(request):
         return handle_valid_login(account)
     else:
         return handle_invalid_login(username)
-
-
-class LogoutView(views.APIView):
-    permission_classes = (permissions.IsAuthenticated,)
-
-    def post(self, request, format=None):
-        logout(request)
-        return Response({}, status=status.HTTP_204_NO_CONTENT)
