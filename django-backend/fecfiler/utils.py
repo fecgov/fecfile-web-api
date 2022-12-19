1a3389897ed29647f37e29b1bb15fa612ef27fbe
David Heitzer <david.heitzer@tcg.com>
2022-12-19 17:24:16 -0500

    added documentation

--- django-backend/fecfiler/utils.py
+++ django-backend/fecfiler/utils.py
@@ -7,7 +7,11 @@ def custom_exception_handler(exc, context):
     # to get the standard error response.
     response = exception_handler(exc, context)
 
-    # Now add the HTTP status code to the response.
+    # Delete user cookies on forbidden http response.
+    # this will ensure that when the user is redirected
+    # to the login page due to the 403, any cookies
+    # (such as indicating committee id) are removed to
+    # allow for a clean new login.
     if response is not None and response.status_code == 403:
         delete_user_logged_in_cookies(response)
 
