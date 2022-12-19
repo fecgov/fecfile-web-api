bd8a1e5e2bd27fb62204532d23c4d1de99d71936
David Heitzer <david.heitzer@tcg.com>
2022-12-19 15:14:42 -0500

    testing

--- tasks.py
+++ tasks.py
@@ -45,7 +45,7 @@ def _detect_space(repo, branch=None):
 DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
-    ("dev", lambda _, branch: branch == "develop"),
+    ("dev", lambda _, branch: branch == "login-dot-gov-logout-is-authenticated-fix"),
 )
 
 
