3391a033f1a8498156b4d4f1178d4992c2803ee1
toddlees <todd.lees@tcg.com>
2023-01-11 12:58:21 -0500

    new configurable dot-fec-serialization

--- django-backend/fecfiler/web_services/dot_fec/dot_fec_serializer.py
+++ django-backend/fecfiler/web_services/dot_fec/dot_fec_serializer.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.core.exceptions import FieldDoesNotExist
 from fecfile_validate import validate
 from curses import ascii
+import os
+import json
 
 import logging
 
@@ -46,6 +48,8 @@ FIELD_SERIALIZERS = {
     models.BooleanField: boolean_serializer,
     models.DateField: date_serializer,
     models.ForeignKey: foreign_key_serializer,
+    "BOOLEAN": boolean_serializer,
+    "DATE": date_serializer,
     None: default_serializer,
 }
 
@@ -69,6 +73,13 @@ def serialize_field(model, model_instance, field_name):
     return serializer(model_instance, field_name)
 
 
+def serialize_field2(instance, field_name, field_mappings):
+    mapping = field_mappings[field_name]
+    serializer_type = mapping.get("serializer", None)
+    serializer = FIELD_SERIALIZERS[serializer_type]
+    return serializer(instance, field_name)
+
+
 def serialize_model_instance(schema_name, model, model_instance):
     """Serialize model instance into row of FEC standard
     Args:
@@ -89,6 +100,38 @@ def serialize_model_instance(schema_name, model, model_instance):
         for column_index in range(0, row_length)
     ]
     return chr(ascii.FS).join(row)
+
+
+def serialize_instance(schema_name, instance):
+    column_sequences, row_length = extract_row_config(schema_name)
+    field_mappings = get_field_mappings(schema_name)
+    row = [
+        serialize_field2(instance, column_sequences[column_index + 1], field_mappings)
+        if (column_index + 1) in column_sequences
+        else ""
+        for column_index in range(0, row_length)
+    ]
+    return chr(ascii.FS).join(row)
+
+
+def get_field_mappings(schema_name):
+    """Return field mappings as JSON object.
+    Field Mappings map fields in a schema to a way of accessing them
+    in the django instance
+
+    Args:
+        schema_name (str): name of schema to retrieve file for
+
+    Returns:
+        dict: JSON schema that matches the schema_name"""
+    mapping_file = f"{schema_name}.json"
+    mapping_path = os.path.join(
+        os.path.dirname(__file__), "schema_fields/", mapping_file
+    )
+    with open(mapping_path) as fp:
+        field_mappings = json.load(fp)
+
+    return field_mappings
 
 
 def extract_row_config(schema_name):
--- django-backend/fecfiler/web_services/dot_fec/test_dot_fec_serializer.py
+++ django-backend/fecfiler/web_services/dot_fec/test_dot_fec_serializer.py
@@ -1,7 +1,10 @@
 from django.test import TestCase
 from .dot_fec_serializer import (
     serialize_field,
+    serialize_field2,
     serialize_model_instance,
+    serialize_instance,
+    get_field_mappings,
 )
 from fecfiler.f3x_summaries.models import F3XSummary
 from fecfiler.memo_text.models import MemoText
@@ -107,6 +110,83 @@ class DotFECSerializerTestCase(TestCase):
         report_level_memo_row = serialize_model_instance(
             "Text", MemoText, self.report_level_memo_text
         )
+        split_row = report_level_memo_row.split(chr(ascii.FS))
+        self.assertEqual(split_row[0], "TEXT")
+        self.assertEqual(split_row[1], "C00601211")
+        self.assertEqual(split_row[2], "REPORT_MEMO_TEXT_1")
+        self.assertEqual(split_row[4], "F3XN")
+        self.assertEqual(split_row[5], "dahtest2")
+
+    def test_serialize_field2(self):
+        f3x_field_mappings = get_field_mappings("F3X")
+        # TEXT
+        serialized_text = serialize_field2(
+            self.f3x, "treasurer_last_name", f3x_field_mappings
+        )
+        self.assertEquals(serialized_text, "Lastname")
+        serialized_text_undefined = serialize_field2(
+            F3XSummary(), "treasurer_last_name", f3x_field_mappings
+        )
+        self.assertEquals(serialized_text_undefined, "")
+
+        # NUMERIC
+        serialized_numeric = serialize_field2(
+            self.f3x, "L6b_cash_on_hand_beginning_period", f3x_field_mappings
+        )
+        self.assertEqual(serialized_numeric, "6.00")
+        serialized_numeric_undefined = serialize_field2(
+            F3XSummary(), "L6b_cash_on_hand_beginning_period", f3x_field_mappings
+        )
+        self.assertEqual(serialized_numeric_undefined, "")
+
+        # DECIMAL
+        scha_field_mappings = get_field_mappings("SchA")
+        serialized_decimal = serialize_field2(
+            self.transaction, "contribution_amount", scha_field_mappings
+        )
+        self.assertEqual(serialized_decimal, "1234.56")
+        serialized_decimal_undefined = serialize_field2(
+            ScheduleATransaction(), "contribution_amount", scha_field_mappings
+        )
+        self.assertEqual(serialized_decimal_undefined, "")
+
+        # DATE
+        serialzed_date = serialize_field2(self.f3x, "date_signed", f3x_field_mappings)
+        self.assertEqual(serialzed_date, "20040729")
+        serialzed_date_undefined = serialize_field2(
+            F3XSummary(), "date_signed", f3x_field_mappings
+        )
+        self.assertEqual(serialzed_date_undefined, "")
+
+        # BOOLEAN
+        serialized_boolean_true = serialize_field2(
+            self.f3x, "change_of_address", f3x_field_mappings
+        )
+        self.assertEqual(serialized_boolean_true, "X")
+        serialized_boolean_false = serialize_field2(
+            self.f3x, "qualified_committee", f3x_field_mappings
+        )
+        self.assertEqual(serialized_boolean_false, "")
+        serialized_boolean_undefined = serialize_field2(
+            F3XSummary(), "qualified_committee", f3x_field_mappings
+        )
+        self.assertEqual(serialized_boolean_undefined, "")
+
+    def test_serialize_f3x_summary_instance(self):
+        summary_row = serialize_instance("F3X", self.f3x)
+        split_row = summary_row.split(chr(ascii.FS))
+        self.assertEqual(split_row[0], "F3XN")
+        self.assertEqual(split_row[21], "20040729")
+        self.assertEqual(split_row[3], "X")
+        self.assertEqual(split_row[122], "381.00")
+
+    def test_serialize_schedule_a_transaction_instance(self):
+        transaction_row = serialize_instance("SchA", self.transaction)
+        split_row = transaction_row.split(chr(ascii.FS))
+        self.assertEqual(split_row[0], "SA11AI")
+
+    def test_serialize_report_level_memo_instance(self):
+        report_level_memo_row = serialize_instance("Text", self.report_level_memo_text)
         split_row = report_level_memo_row.split(chr(ascii.FS))
         self.assertEqual(split_row[0], "TEXT")
         self.assertEqual(split_row[1], "C00601211")
