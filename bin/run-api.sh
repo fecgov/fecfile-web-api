63528e0eb5effbc768888254b56a0bee6450c4a6
David Heitzer <david.heitzer@tcg.com>
2023-02-06 09:52:35 -0500

    removed gunicorn timeout

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -3,4 +3,4 @@ cd django-backend
 # Run migrations and application
 ./manage.py migrate --no-input --traceback --verbosity 3 > migrate.out &&
 	python wait_for_db.py && 
-	gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 -t 200
+	gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9
