24a7452334fc997d139a01ae7c65c0510e6ce1b6
David Heitzer <david.heitzer@tcg.com>
2023-02-08 13:02:05 -0500

    202 added filings view

--- django-backend/fecfiler/mock_openfec/urls.py
+++ django-backend/fecfiler/mock_openfec/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import MockCommitteeDetailViewSet
+from .views import MockCommitteeDetailViewSet, MockRecentFilingsViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r"committee", MockCommitteeDetailViewSet,
                 basename="committee")
+router.register(r"filings", MockRecentFilingsViewSet,
+                basename="filings")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
--- django-backend/fecfiler/mock_openfec/views.py
+++ django-backend/fecfiler/mock_openfec/views.py
@@ -6,6 +6,10 @@ from rest_framework.response import Response
 import requests
 from fecfiler.settings import FEC_API, FEC_API_KEY
 
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class MockCommitteeDetailViewSet(viewsets.ModelViewSet):
 
@@ -30,3 +34,42 @@ class MockCommitteeDetailViewSet(viewsets.ModelViewSet):
             }
         }
         return Response(retval)
+
+
+class MockRecentFilingsViewSet(viewsets.ModelViewSet):
+
+    def retrieve(self, request, pk=None):
+
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        params = {
+            'api_key': FEC_API_KEY,
+            'committee_id': pk,
+            'sort': '-receipt_date'
+        }
+
+        resp = requests.get(f'{FEC_API}efile/filings/', headers=headers, params=params)
+        retval = get_recent_f1_from_openfec_resp(resp)
+        if not retval:
+            params['per_page'] = 1
+            params['page'] = 1
+            params['form_type'] = 'F1'
+            resp = requests.get(f'{FEC_API}filings/', headers=headers, params=params)
+            retval = get_recent_f1_from_openfec_resp(resp)
+
+        return Response(retval)
+
+
+def get_recent_f1_from_openfec_resp(resp: requests.Response):
+    if resp:
+        try:
+            filing_list = resp.json()['results']
+            return next((
+                filing for filing in filing_list if filing['form_type'].startswith('F1')
+            ), None)
+        except Exception as error:
+            logger.error(
+                f'Failed to process response from {resp.url} due to error: {str(error)}'
+            )
+    return None
