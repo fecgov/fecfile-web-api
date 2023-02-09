707a1e7d9a239fd7e90efbefbd8af87382fc96b1
David Heitzer <david.heitzer@tcg.com>
2023-02-08 23:55:33 -0500

    202 add test coverage

--- django-backend/fecfiler/mock_openfec/test_views.py
+++ django-backend/fecfiler/mock_openfec/test_views.py
@@ -15,7 +15,7 @@ class MockCommitteeDetailViewSetTest(TestCase):
         self.user = Account.objects.get(cmtee_id="C12345678")
         self.factory = RequestFactory()
 
-    def test_get_committee_data(self):
+    def test_get_committee_data_happy_path(self):
         request = self.factory.get(
             "/api/v1/mock_openfec/committee/C00100230/"
         )
@@ -24,6 +24,20 @@ class MockCommitteeDetailViewSetTest(TestCase):
             "get": "retrieve"
         })(request, pk='C00100230')
         self.assertEqual(response.status_code, 200)
+
+    def test_get_committee_data_cid_not_found(self):
+        request = self.factory.get(
+            "/api/v1/mock_openfec/committee/C12345678/"
+        )
+        request.user = self.user
+        with patch('fecfiler.mock_openfec.views.requests') as mock_requests:
+            mock_requests.get.return_value = mock_response = Mock()
+            mock_response.status_code = 200
+            mock_response.json.return_value = None
+            response = MockCommitteeDetailViewSet.as_view({
+                "get": "retrieve"
+            })(request, pk='C12345678')
+            self.assertEqual(response.status_code, 200)
 
 
 class MockRecentFilingsViewSetTest(TestCase):
