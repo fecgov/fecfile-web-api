80c1bc6bceb1dcaf519b57d26358db495f097da1
David Heitzer <david.heitzer@tcg.com>
2023-02-09 18:28:42 -0500

    206 url teest fiixes

--- django-backend/fecfiler/openfec/test_views.py
+++ django-backend/fecfiler/openfec/test_views.py
@@ -16,7 +16,7 @@ class OpenfecViewSetTest(TestCase):
 
     def test_get_committee_no_override(self):
         request = self.factory.get(
-            "/api/v1/openfec/committee/C12345678/"
+            "/api/v1/openfec/C12345678/committee/"
         )
         request.user = self.user
         with patch('fecfiler.openfec.views.requests') as mock_requests:
@@ -33,7 +33,7 @@ class OpenfecViewSetTest(TestCase):
             base.FEC_API_COMMITTEE_LOOKUP_IDS_OVERRIDE = 'C12345678'
             base.BASE_DIR = 'fecfiler/'
             request = self.factory.get(
-                "/api/v1/openfec/committee/C12345678/"
+                "/api/v1/openfec/C12345678/committee/"
             )
             request.user = self.user
             response = OpenfecViewSet.as_view({
@@ -46,7 +46,7 @@ class OpenfecViewSetTest(TestCase):
             base.FEC_API_COMMITTEE_LOOKUP_IDS_OVERRIDE = 'C00100230'
             base.BASE_DIR = 'fecfiler/'
             request = self.factory.get(
-                "/api/v1/openfec/committee/C00100230/"
+                "/api/v1/openfec/C00100230/committee/"
             )
             request.user = self.user
             response = OpenfecViewSet.as_view({
@@ -65,7 +65,7 @@ class OpenfecViewSetTest(TestCase):
 
     def test_get_filings_invalid_resp(self):
         request = self.factory.get(
-            "/api/v1/openfec/filings/C00100230/"
+            "/api/v1/openfec/C00100230/filings/"
         )
         request.user = self.user
         with patch('fecfiler.openfec.views.requests') as mock_requests:
@@ -79,7 +79,7 @@ class OpenfecViewSetTest(TestCase):
 
     def test_get_filings_happy_path(self):
         request = self.factory.get(
-            "/api/v1/openfec/filings/C00100230/"
+            "/api/v1/openfec/C00100230/filings/"
         )
         request.user = self.user
         with patch('fecfiler.openfec.views.requests') as mock_requests:
