07b0d04a94fb2cd33af007c9a481a0ee730b1e70
Elaine Krauss <elaine.krauss@tcg.com>
2023-03-10 16:12:09 -0500

    Removes a blank line to make linting happy

--- django-backend/fecfiler/web_services/summary/test_summary.py
+++ django-backend/fecfiler/web_services/summary/test_summary.py
@@ -26,7 +26,6 @@ class F3XSerializerTestCase(TestCase):
         summary = summary_service.calculate_summary()
         self.assertEqual(summary["b"]["line_15"], Decimal("2225.79"))
 
-
     def test_report_with_no_transactions(self):
         f3x = F3XSummary.objects.get(id="a07c8c65-1b2d-4e6e-bcaa-fa8d39e50965")
         summary_service = SummaryService(f3x)
