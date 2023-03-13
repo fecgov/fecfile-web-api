35484d86907bebd33251976fbbee861afa1da996
Elaine Krauss <elaine.krauss@tcg.com>
2023-03-13 18:13:41 -0400

    WIP Adds lines 11 and 37 to column b calculations

--- django-backend/fecfiler/web_services/summary/summary.py
+++ django-backend/fecfiler/web_services/summary/summary.py
@@ -70,15 +70,36 @@ class SummaryService:
             date__lt=report_date,
         )
 
+        # line 11ai
+        sa11ai_query = Q(~Q(memo_code=True), itemized=True, form_type="SA11AI")
+        line_11ai = self._create_contribution_sum(sa11ai_query)
+        # line 11aii
+        sa11aii_query = Q(~Q(memo_code=True), itemized=False, form_type="SA11AI")
+        line_11aii = self._create_contribution_sum(sa11aii_query)
+        # line 11b
+        sa11b_query = Q(~Q(memo_code=True), form_type="SA11B")
+        line_11b = self._create_contribution_sum(sa11b_query)
+        # line 11c
+        sa11c_query = Q(~Q(memo_code=True), form_type="SA11C")
+        line_11c = self._create_contribution_sum(sa11c_query)
         # line 15
         sa15_query = Q(~Q(memo_code=True), form_type="SA15")
         line_15 = self._create_contribution_sum(sa15_query)
 
         # build summary
         summary = ytd_transactions.aggregate(
+            line_11ai=line_11ai,
+            line_11aii=line_11aii,
+            line_11b=line_11b,
+            line_11c=line_11c,
             line_15=line_15,
             line_37=line_15
         )
+        summary["line_11aiii"] = summary["line_11ai"] + summary["line_11aii"]
+        summary["line_11d"] = (
+            summary["line_11aiii"] + summary["line_11b"] + summary["line_11c"]
+        )
+        summary["line_33"] = summary["line_11d"]
 
         return summary
 
