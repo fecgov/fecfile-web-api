bdd37b8522f7f7f8198b1de3b26019e1da0daeb9
Elaine Krauss <elaine.krauss@tcg.com>
2023-05-05 15:18:06 -0400

    Adds performance profiling tools

--- django-backend/fecfiler/settings/local.py
+++ django-backend/fecfiler/settings/local.py
@@ -22,6 +22,11 @@ LOGGING = {
 # E2E Testing Login API
 E2E_TESTING_LOGIN = True
 
+MIDDLEWARE.append(
+    "django_cprofile_middleware.middleware.ProfilerMiddleware"
+)
+DJANGO_CPROFILE_MIDDLEWARE_REQUIRE_STAFF = False
+
 try:
     from .local import *  # NOSONAR # noqa F401, F403
 except ImportError:
