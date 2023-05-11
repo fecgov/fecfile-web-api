c55d7fd2f05ccedd40ba5912abd15808112eae25
Elaine Krauss <elaine.krauss@tcg.com>
2023-05-11 10:40:41 -0400

    Strips out cprofile middleware for now

--- django-backend/fecfiler/settings/local.py
+++ django-backend/fecfiler/settings/local.py
@@ -22,11 +22,6 @@ LOGGING = {
 # E2E Testing Login API
 E2E_TESTING_LOGIN = True
 
-MIDDLEWARE.append(  # NOSONAR # noqa F405
-    "django_cprofile_middleware.middleware.ProfilerMiddleware"
-)
-DJANGO_CPROFILE_MIDDLEWARE_REQUIRE_STAFF = False
-
 try:
     from .local import *  # NOSONAR # noqa F401, F403
 except ImportError:
