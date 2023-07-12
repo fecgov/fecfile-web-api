ced67c6ed315b490f2062f366d11d78adc5382c9
Matt Travers <matt.travers@tcg.com>
2023-07-12 16:57:19 -0400

    Add unit tests

---
+++ django-backend/fecfiler/transactions/tests/test_views.py
@@ -0,0 +1,31 @@
+from django.test import TestCase
+from django.test.client import RequestFactory
+from fecfiler.authentication.models import Account
+import json
+
+from fecfiler.transactions.views import save_transaction_pair
+
+
+class TransactionViewsTestCase(TestCase):
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = Account.objects.get(email="test@test.com")
+
+    def test_save_new_transaction_pair(self):
+        # payload = json.load(
+        #     open("fecfiler/transactions/fixtures/test_transaction_pair_payload.json")
+        # )
+        payload = json.load(
+            open("fecfiler/transactions/fixtures/test_transaction_pair_payload.json")
+        )
+        request = self.factory.post(
+            "/api/v1/transactions/save-pair",
+            json.dumps(payload),
+            content_type="application/json",
+        )
+        request.user = self.user
+        request.data = payload
+        request.query_params = {}
+        response = save_transaction_pair(request)
+        self.assertEqual("foo", response.data["contributor_first_name"])
+
