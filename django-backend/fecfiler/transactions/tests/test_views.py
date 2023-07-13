8b63b32cbd312ddd71adf5ce58871e39acfa5f24
Matt Travers <matt.travers@tcg.com>
2023-07-13 07:08:20 -0400

    Update view unit test

--- django-backend/fecfiler/transactions/tests/test_views.py
+++ django-backend/fecfiler/transactions/tests/test_views.py
@@ -7,16 +7,18 @@ from fecfiler.transactions.views import save_transaction_pair
 
 
 class TransactionViewsTestCase(TestCase):
+    fixtures = [
+        "test_committee_accounts",
+        "test_f3x_summaries",
+    ]
+
     def setUp(self):
         self.factory = RequestFactory()
-        self.user = Account.objects.get(email="test@fec.gov")
+        self.user = Account.objects.first()
 
     def test_save_new_transaction_pair(self):
-        # payload = json.load(
-        #     open("fecfiler/transactions/fixtures/test_transaction_pair_payload.json")
-        # )
         payload = json.load(
-            open("fecfiler/transactions/fixtures/test_transaction_pair_payload.json")
+            open("fecfiler/transactions/fixtures/payload_transaction_pair.json")
         )
         request = self.factory.post(
             "/api/v1/transactions/save-pair",
@@ -27,5 +29,6 @@ class TransactionViewsTestCase(TestCase):
         request.data = payload
         request.query_params = {}
         response = save_transaction_pair(request)
-        self.assertEqual("foo", response.data["contributor_first_name"])
+        self.assertEqual("John", response.data["contributor_first_name"])
+        self.assertEqual("Smith", response.data["contributor_last_name"])
 
