bd998c7fd5e60f0c8492e2f7e39e37f6200acff3
Elaine Krauss <elaine.krauss@tcg.com>
2023-09-15 14:48:00 -0400

    adds the __init__.py file for the new reports app

---
+++ django-backend/fecfiler/reports/__init__.py
@@ -0,0 +1,11 @@
+"""Django App for F3X Reports
+
+Provides F3X Report record
+:py:class:`fecfiler.reports.f3x_report.models.F3XReport`
+
+Also provides abstract model and viewset for
+apps that need report-related records
+
+:py:class:`fecfiler.committee_accounts.models.ReportMixin`
+:py:class:`fecfiler.committee_accounts.views.ReportViewMixin`
+"""
