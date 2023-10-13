d7a9ce93f00389651df285c5c0766a32dcf91482
Matt Travers <matt.travers@tcg.com>
2023-10-13 11:02:03 -0400

    reverting migration changes

--- django-backend/fecfiler/reports/migrations/0001_initial.py
+++ django-backend/fecfiler/reports/migrations/0001_initial.py
@@ -723,7 +723,7 @@ class Migration(migrations.Migration):
                         blank=True,
                         null=True,
                         on_delete=django.db.models.deletion.CASCADE,
-                        to="reports.ReportF3X",
+                        to="reports.reportf3x",
                     ),
                 ),
             ],
