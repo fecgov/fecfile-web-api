8c9529a277ff984753f96d0c3e5a1a77baee0be6
Matt Travers <matt.travers@tcg.com>
2023-10-13 10:34:36 -0400

    Fix caps in F3X and F24 foreign key migration declaration

--- django-backend/fecfiler/reports/migrations/0001_initial.py
+++ django-backend/fecfiler/reports/migrations/0001_initial.py
@@ -723,7 +723,7 @@ class Migration(migrations.Migration):
                         blank=True,
                         null=True,
                         on_delete=django.db.models.deletion.CASCADE,
-                        to="reports.reportf3x",
+                        to="reports.reportF3X",
                     ),
                 ),
             ],
--- django-backend/fecfiler/reports/migrations/0003_move_fields_between_tables.py
+++ django-backend/fecfiler/reports/migrations/0003_move_fields_between_tables.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 blank=True,
                 null=True,
                 on_delete=django.db.models.deletion.CASCADE,
-                to="reports.reportf24",
+                to="reports.reportF24",
             ),
         ),
         migrations.RemoveField(model_name="reportf24", name="date_signed",),
