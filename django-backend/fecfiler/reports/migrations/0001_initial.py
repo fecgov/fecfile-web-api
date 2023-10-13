eebe44f481772c7d02d2c86dfc9901455b1a3d69
Matt Travers <matt.travers@tcg.com>
2023-10-13 10:44:04 -0400

    CircleCI test change

--- django-backend/fecfiler/reports/migrations/0001_initial.py
+++ django-backend/fecfiler/reports/migrations/0001_initial.py
@@ -723,7 +723,7 @@ class Migration(migrations.Migration):
                         blank=True,
                         null=True,
                         on_delete=django.db.models.deletion.CASCADE,
-                        to="reports.reportF3X",
+                        to="reports.ReportF3X",
                     ),
                 ),
             ],
--- django-backend/fecfiler/reports/migrations/0003_move_fields_between_tables.py
+++ django-backend/fecfiler/reports/migrations/0003_move_fields_between_tables.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
                 blank=True,
                 null=True,
                 on_delete=django.db.models.deletion.CASCADE,
-                to="reports.reportF24",
+                to="reports.ReportF24",
             ),
         ),
         migrations.RemoveField(model_name="reportf24", name="date_signed",),
