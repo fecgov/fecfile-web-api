d0b1f5447e8f58478faaa707401457f2b6182410
toddlees <todd.lees@tcg.com>
2023-11-06 08:51:03 -0500

    remove old tests

--- django-backend/fecfiler/transactions/schedule_a/tests/test_serializers.py
+++ django-backend/fecfiler/transactions/schedule_a/tests/test_serializers.py
@@ -1,138 +1,138 @@
-from django.test import TestCase
-from fecfiler.authentication.models import Account
-from fecfiler.transactions.schedule_a.serializers import ScheduleATransactionSerializer
-from rest_framework.request import HttpRequest, Request
-from fecfiler.transactions.models import Transaction
+# from django.test import TestCase
+# from fecfiler.authentication.models import Account
+# from fecfiler.transactions.schedule_a.serializers import ScheduleATransactionSerializer
+# from rest_framework.request import HttpRequest, Request
+# from fecfiler.transactions.models import Transaction
 
 
-class ScheduleATransactionSerializerBaseTestCase(TestCase):
-    fixtures = [
-        "test_committee_accounts",
-        "test_f3x_reports",
-        "test_transaction_serializer",
-        "test_memo_text",
-    ]
+# class ScheduleATransactionSerializerBaseTestCase(TestCase):
+#     fixtures = [
+#         "test_committee_accounts",
+#         "test_f3x_reports",
+#         "test_transaction_serializer",
+#         "test_memo_text",
+#     ]
 
-    def setUp(self):
-        self.new_contact = {
-            "id": "9bb5c8b2-31f3-488f-84e1-a63b0133a000",
-            "type": "IND",
-            "last_name": "contact_1",
-            "first_name": "New",
-            "street_1": "Street",
-            "city": "City",
-            "state": "CA",
-            "zip": "12345678",
-            "country": "Country",
-            "created": "2022-02-09T00:00:00.000Z",
-            "updated": "2022-02-09T00:00:00.000Z",
-            "committee_account_id": "735db943-9446-462a-9be0-c820baadb622",
-        }
+#     def setUp(self):
+#         self.new_contact = {
+#             "id": "9bb5c8b2-31f3-488f-84e1-a63b0133a000",
+#             "type": "IND",
+#             "last_name": "contact_1",
+#             "first_name": "New",
+#             "street_1": "Street",
+#             "city": "City",
+#             "state": "CA",
+#             "zip": "12345678",
+#             "country": "Country",
+#             "created": "2022-02-09T00:00:00.000Z",
+#             "updated": "2022-02-09T00:00:00.000Z",
+#             "committee_account_id": "735db943-9446-462a-9be0-c820baadb622",
+#         }
 
-        self.updated_contact = {
-            "id": "00000000-6486-4062-944f-aa0c4cbe4073",
-            "type": "IND",
-            "last_name": "contact_1",
-            "first_name": "Updated",
-            "street_1": "Street",
-            "city": "City",
-            "state": "CA",
-            "zip": "12345678",
-            "country": "Country",
-            "created": "2022-02-09T00:00:00.000Z",
-            "updated": "2022-02-09T00:00:00.000Z",
-            "committee_account_id": "735db943-9446-462a-9be0-c820baadb622",
-        }
+#         self.updated_contact = {
+#             "id": "00000000-6486-4062-944f-aa0c4cbe4073",
+#             "type": "IND",
+#             "last_name": "contact_1",
+#             "first_name": "Updated",
+#             "street_1": "Street",
+#             "city": "City",
+#             "state": "CA",
+#             "zip": "12345678",
+#             "country": "Country",
+#             "created": "2022-02-09T00:00:00.000Z",
+#             "updated": "2022-02-09T00:00:00.000Z",
+#             "committee_account_id": "735db943-9446-462a-9be0-c820baadb622",
+#         }
 
-        self.new_memo_text = {
-            "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
-            "transaction_id_number": "ABCDEF0123456789",
-            "rec_type": "",
-            "memo4000": "new memo text",
-        }
-        self.valid_schedule_a_transaction = {
-            "form_type": "SA",
-            "transaction_type_identifier": "SchA",
-            "transaction_id": "ABCDEF0123456789",
-            "entity_type": "IND",
-            "contributor_organization_name": "John Smith Co",
-            "contributor_last_name": "John",
-            "contributor_first_name": "Smith",
-            "contributor_state": "AK",
-            "contributor_city": "Homer",
-            "contributor_zip": "1234",
-            "contributor_street_1": "1 Homer Spit Road",
-            "contribution_date": "2009-01-01",
-            "contribution_amount": 1234,
-            "aggregation_group": "GENERAL",
-            "contributor_occupation": "professional",
-            "contributor_employer": "boss",
-            "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
-            "contact_1": self.new_contact,
-            "schema_name": "SchA",
-        }
-        self.update_contact_schedule_a_transaction = {
-            "form_type": "SA",
-            "transaction_type_identifier": "SchA",
-            "transaction_id": "ABCDEF0123456789",
-            "entity_type": "IND",
-            "contributor_organization_name": "John Smith Co",
-            "contributor_last_name": "TEST_LAST_NAME",
-            "contributor_first_name": "Smith",
-            "contributor_state": "AK",
-            "contributor_city": "Homer",
-            "contributor_zip": "1234",
-            "contributor_street_1": "1 Homer Spit Road",
-            "contribution_date": "2024-01-01",
-            "contribution_amount": 1234,
-            "aggregation_group": "GENERAL",
-            "contributor_occupation": "professional",
-            "contributor_employer": "boss",
-            "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
-            "contact_1": self.updated_contact,
-            "contact_1_id": "00000000-6486-4062-944f-aa0c4cbe4073",
-            "schema_name": "SchA",
-        }
+#         self.new_memo_text = {
+#             "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
+#             "transaction_id_number": "ABCDEF0123456789",
+#             "rec_type": "",
+#             "memo4000": "new memo text",
+#         }
+#         self.valid_schedule_a_transaction = {
+#             "form_type": "SA",
+#             "transaction_type_identifier": "SchA",
+#             "transaction_id": "ABCDEF0123456789",
+#             "entity_type": "IND",
+#             "contributor_organization_name": "John Smith Co",
+#             "contributor_last_name": "John",
+#             "contributor_first_name": "Smith",
+#             "contributor_state": "AK",
+#             "contributor_city": "Homer",
+#             "contributor_zip": "1234",
+#             "contributor_street_1": "1 Homer Spit Road",
+#             "contribution_date": "2009-01-01",
+#             "contribution_amount": 1234,
+#             "aggregation_group": "GENERAL",
+#             "contributor_occupation": "professional",
+#             "contributor_employer": "boss",
+#             "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
+#             "contact_1": self.new_contact,
+#             "schema_name": "SchA",
+#         }
+#         self.update_contact_schedule_a_transaction = {
+#             "form_type": "SA",
+#             "transaction_type_identifier": "SchA",
+#             "transaction_id": "ABCDEF0123456789",
+#             "entity_type": "IND",
+#             "contributor_organization_name": "John Smith Co",
+#             "contributor_last_name": "TEST_LAST_NAME",
+#             "contributor_first_name": "Smith",
+#             "contributor_state": "AK",
+#             "contributor_city": "Homer",
+#             "contributor_zip": "1234",
+#             "contributor_street_1": "1 Homer Spit Road",
+#             "contribution_date": "2024-01-01",
+#             "contribution_amount": 1234,
+#             "aggregation_group": "GENERAL",
+#             "contributor_occupation": "professional",
+#             "contributor_employer": "boss",
+#             "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
+#             "contact_1": self.updated_contact,
+#             "contact_1_id": "00000000-6486-4062-944f-aa0c4cbe4073",
+#             "schema_name": "SchA",
+#         }
 
-        self.mock_request = Request(HttpRequest())
-        user = Account()
-        user.cmtee_id = "C12345678"
-        self.mock_request.user = user
+#         self.mock_request = Request(HttpRequest())
+#         user = Account()
+#         user.cmtee_id = "C12345678"
+#         self.mock_request.user = user
 
-    def test_serializer_validate(self):
-        valid_serializer = ScheduleATransactionSerializer(
-            data=self.valid_schedule_a_transaction,
-            context={"request": self.mock_request},
-        )
-        self.assertTrue(valid_serializer.is_valid(raise_exception=True))
-        invalid_transaction = self.valid_schedule_a_transaction.copy()
-        invalid_transaction["form_type"] = "invalidformtype"
-        del invalid_transaction["contributor_first_name"]
-        invalid_serializer = ScheduleATransactionSerializer(
-            data=invalid_transaction, context={"request": self.mock_request},
-        )
-        self.assertFalse(invalid_serializer.is_valid())
-        self.assertIsNotNone(invalid_serializer.errors["form_type"])
-        self.assertIsNotNone(invalid_serializer.errors["contributor_first_name"])
+#     def test_serializer_validate(self):
+#         valid_serializer = ScheduleATransactionSerializer(
+#             data=self.valid_schedule_a_transaction,
+#             context={"request": self.mock_request},
+#         )
+#         self.assertTrue(valid_serializer.is_valid(raise_exception=True))
+#         invalid_transaction = self.valid_schedule_a_transaction.copy()
+#         invalid_transaction["form_type"] = "invalidformtype"
+#         del invalid_transaction["contributor_first_name"]
+#         invalid_serializer = ScheduleATransactionSerializer(
+#             data=invalid_transaction, context={"request": self.mock_request},
+#         )
+#         self.assertFalse(invalid_serializer.is_valid())
+#         self.assertIsNotNone(invalid_serializer.errors["form_type"])
+#         self.assertIsNotNone(invalid_serializer.errors["contributor_first_name"])
 
-    def test_transaction_contact_updated(self):
-        transaction = self.update_contact_schedule_a_transaction.copy()
-        serializer = ScheduleATransactionSerializer(
-            data=transaction, context={"request": self.mock_request},
-        )
-        self.assertTrue(serializer.is_valid(raise_exception=True))
-        serializer.create(serializer.to_internal_value(transaction))
-        created_instance = Transaction.objects.filter(
-            schedule_a__contributor_last_name="TEST_LAST_NAME"
-        )
-        self.assertEqual(created_instance.count(), 1)
+#     def test_transaction_contact_updated(self):
+#         transaction = self.update_contact_schedule_a_transaction.copy()
+#         serializer = ScheduleATransactionSerializer(
+#             data=transaction, context={"request": self.mock_request},
+#         )
+#         self.assertTrue(serializer.is_valid(raise_exception=True))
+#         serializer.create(serializer.to_internal_value(transaction))
+#         created_instance = Transaction.objects.filter(
+#             schedule_a__contributor_last_name="TEST_LAST_NAME"
+#         )
+#         self.assertEqual(created_instance.count(), 1)
 
-        updated_transaction = self.update_contact_schedule_a_transaction.copy()
-        updated_transaction["contributor_last_name"] = "TEST_LAST_NAME_UPDATED"
-        serializer.update(
-            created_instance[0], serializer.to_internal_value(updated_transaction)
-        )
-        updated_instance = Transaction.objects.filter(
-            schedule_a__contributor_last_name="TEST_LAST_NAME_UPDATED"
-        )
-        self.assertEqual(updated_instance.count(), 1)
+#         updated_transaction = self.update_contact_schedule_a_transaction.copy()
+#         updated_transaction["contributor_last_name"] = "TEST_LAST_NAME_UPDATED"
+#         serializer.update(
+#             created_instance[0], serializer.to_internal_value(updated_transaction)
+#         )
+#         updated_instance = Transaction.objects.filter(
+#             schedule_a__contributor_last_name="TEST_LAST_NAME_UPDATED"
+#         )
+#         self.assertEqual(updated_instance.count(), 1)
--- django-backend/fecfiler/transactions/schedule_c1/tests/test_serializers.py
+++ django-backend/fecfiler/transactions/schedule_c1/tests/test_serializers.py
@@ -1,78 +1,78 @@
-from django.test import TestCase
-from fecfiler.authentication.models import Account
-from rest_framework.request import HttpRequest, Request
+# from django.test import TestCase
+# from fecfiler.authentication.models import Account
+# from rest_framework.request import HttpRequest, Request
 
-from fecfiler.transactions.schedule_c1.serializers import (
-    ScheduleC1TransactionSerializer,
-)
+# from fecfiler.transactions.schedule_c1.serializers import (
+#     ScheduleC1TransactionSerializer,
+# )
 
 
-class ScheduleC1TransactionSerializerTestCase(TestCase):
-    def setUp(self):
-        self.new_contact = {
-            "id": "9bb5c8b2-31f3-488f-84e1-a63b0133a000",
-            "type": "ORG",
-            "name": "Bank",
-            "street_1": "Street",
-            "city": "City",
-            "state": "CA",
-            "zip": "12345678",
-            "country": "Country",
-            "created": "2022-02-09T00:00:00.000Z",
-            "updated": "2022-02-09T00:00:00.000Z",
-            "committee_account_id": "735db943-9446-462a-9be0-c820baadb622",
-        }
-        self.valid_schedule_c1_transaction = {
-            "form_type": "SC1/9",
-            "transaction_type_identifier": "SCHEDULE_C1",
-            "transaction_id": "ABCDEF0123456789",
-            "back_reference_tran_id_number": "BBCDEF0123456789",
-            "lender_organization_name": "Big Bank",
-            "lender_state": "AK",
-            "lender_city": "Homer",
-            "lender_zip": "1234",
-            "lender_street_1": "1 Homer Spit Road",
-            "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
-            "contact": self.new_contact,
-            "schema_name": "SchC1",
-            "loan_amount": "1000.00",
-            "loan_incurred_date": "2022-10-05",
-            "loan_due_date": "2023-11-09",
-            "treasurer_last_name": "Tlastname",
-            "treasurer_first_name": "Tfirstname",
-            "treasurer_date_signed": "2023-12-03",
-            "authorized_last_name": "Alastname",
-            "authorized_first_name": "Afirstname",
-            "authorized_title": "Atitle",
-            "authorized_date_signed": "2023-08-17",
-        }
+# class ScheduleC1TransactionSerializerTestCase(TestCase):
+#     def setUp(self):
+#         self.new_contact = {
+#             "id": "9bb5c8b2-31f3-488f-84e1-a63b0133a000",
+#             "type": "ORG",
+#             "name": "Bank",
+#             "street_1": "Street",
+#             "city": "City",
+#             "state": "CA",
+#             "zip": "12345678",
+#             "country": "Country",
+#             "created": "2022-02-09T00:00:00.000Z",
+#             "updated": "2022-02-09T00:00:00.000Z",
+#             "committee_account_id": "735db943-9446-462a-9be0-c820baadb622",
+#         }
+#         self.valid_schedule_c1_transaction = {
+#             "form_type": "SC1/9",
+#             "transaction_type_identifier": "SCHEDULE_C1",
+#             "transaction_id": "ABCDEF0123456789",
+#             "back_reference_tran_id_number": "BBCDEF0123456789",
+#             "lender_organization_name": "Big Bank",
+#             "lender_state": "AK",
+#             "lender_city": "Homer",
+#             "lender_zip": "1234",
+#             "lender_street_1": "1 Homer Spit Road",
+#             "report_id": "b6d60d2d-d926-4e89-ad4b-c47d152a66ae",
+#             "contact": self.new_contact,
+#             "schema_name": "SchC1",
+#             "loan_amount": "1000.00",
+#             "loan_incurred_date": "2022-10-05",
+#             "loan_due_date": "2023-11-09",
+#             "treasurer_last_name": "Tlastname",
+#             "treasurer_first_name": "Tfirstname",
+#             "treasurer_date_signed": "2023-12-03",
+#             "authorized_last_name": "Alastname",
+#             "authorized_first_name": "Afirstname",
+#             "authorized_title": "Atitle",
+#             "authorized_date_signed": "2023-08-17",
+#         }
 
-        self.mock_request = Request(HttpRequest())
-        user = Account()
-        user.cmtee_id = "C00277616"
-        self.mock_request.user = user
+#         self.mock_request = Request(HttpRequest())
+#         user = Account()
+#         user.cmtee_id = "C00277616"
+#         self.mock_request.user = user
 
-    def test_serializer_validate(self):
-        valid_serializer = ScheduleC1TransactionSerializer(
-            data=self.valid_schedule_c1_transaction,
-            context={"request": self.mock_request},
-        )
-        self.assertTrue(valid_serializer.is_valid(raise_exception=True))
-        invalid_transaction = self.valid_schedule_c1_transaction.copy()
-        invalid_transaction["form_type"] = "invalidformtype"
-        del invalid_transaction["lender_organization_name"]
-        invalid_serializer = ScheduleC1TransactionSerializer(
-            data=invalid_transaction, context={"request": self.mock_request},
-        )
-        self.assertFalse(invalid_serializer.is_valid())
-        self.assertIsNotNone(invalid_serializer.errors["form_type"])
-        self.assertIsNotNone(invalid_serializer.errors["lender_organization_name"])
+#     def test_serializer_validate(self):
+#         valid_serializer = ScheduleC1TransactionSerializer(
+#             data=self.valid_schedule_c1_transaction,
+#             context={"request": self.mock_request},
+#         )
+#         self.assertTrue(valid_serializer.is_valid(raise_exception=True))
+#         invalid_transaction = self.valid_schedule_c1_transaction.copy()
+#         invalid_transaction["form_type"] = "invalidformtype"
+#         del invalid_transaction["lender_organization_name"]
+#         invalid_serializer = ScheduleC1TransactionSerializer(
+#             data=invalid_transaction, context={"request": self.mock_request},
+#         )
+#         self.assertFalse(invalid_serializer.is_valid())
+#         self.assertIsNotNone(invalid_serializer.errors["form_type"])
+#         self.assertIsNotNone(invalid_serializer.errors["lender_organization_name"])
 
-    def test_no_lender_organization_name(self):
-        missing_type = self.valid_schedule_c1_transaction.copy()
-        del missing_type["lender_organization_name"]
-        missing_type_serializer = ScheduleC1TransactionSerializer(
-            data=missing_type, context={"request": self.mock_request},
-        )
-        self.assertFalse(missing_type_serializer.is_valid())
-        self.assertIsNotNone(missing_type_serializer.errors["lender_organization_name"])
+#     def test_no_lender_organization_name(self):
+#         missing_type = self.valid_schedule_c1_transaction.copy()
+#         del missing_type["lender_organization_name"]
+#         missing_type_serializer = ScheduleC1TransactionSerializer(
+#             data=missing_type, context={"request": self.mock_request},
+#         )
+#         self.assertFalse(missing_type_serializer.is_valid())
+#         self.assertIsNotNone(missing_type_serializer.errors["lender_organization_name"])
