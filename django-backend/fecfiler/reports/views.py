8095d6c7f7a5712994462730ac16c94a462267a2
toddlees <todd.lees@tcg.com>
2023-11-28 08:35:06 -0500

    show current state

--- django-backend/fecfiler/reports/views.py
+++ django-backend/fecfiler/reports/views.py
@@ -53,7 +53,7 @@ def get_status_mapping():
 
     return Case(
         When(success, then=Value("Submission success")),
-        When(failed, then=F("upload_submission__fecfile_error")),
+        When(failed, then=F("upload_submission__fecfile_task_state")),
         When(upload_exists, then=Value("Submission pending")),
         default=Value("In progress"),
         output_field=CharField(),
