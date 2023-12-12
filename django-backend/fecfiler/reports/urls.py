373fea2563e65046996905ec8e2a7202bec0f434
Elaine Krauss <elaine.krauss@tcg.com>
2023-12-12 16:33:25 -0500

    Fixes form-99 router

--- django-backend/fecfiler/reports/urls.py
+++ django-backend/fecfiler/reports/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path, include
+from fecfiler.reports.form_99.views import Form99ViewSet
 from rest_framework.routers import DefaultRouter
 from .form_3x.views import Form3XViewSet
 from .form_24.views import Form24ViewSet
@@ -8,7 +9,7 @@ from .views import ReportViewSet
 router = DefaultRouter()
 router.register(r"form-3x", Form3XViewSet, basename="form-3x")
 router.register(r"form-24", Form24ViewSet, basename="form-24")
-router.register(r"form-99", Form24ViewSet, basename="form-99")
+router.register(r"form-99", Form99ViewSet, basename="form-99")
 router.register(r"", ReportViewSet, basename="reports")
 
 # The API URLs are now determined automatically by the router.
