3a6af68d5d11ec2a7504ac8968957f5bd86d4199
Laura Beaufort <lbeaufort@fec.gov>
2024-01-11 22:26:10 -0500

    Add committee ID to responses

--- django-backend/fecfiler/committee_accounts/views.py
+++ django-backend/fecfiler/committee_accounts/views.py
@@ -12,4 +12,7 @@ class CommitteeOwnedViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         committee_id = self.request.user.cmtee_id
         queryset = super().get_queryset()
+        structlog.contextvars.bind_contextvars(
+            committee_id=self.request.user.cmtee_id
+        )
         return queryset.filter(committee_account__committee_id=committee_id)
