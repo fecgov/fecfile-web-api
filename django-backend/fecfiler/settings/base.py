9240abeba7dd525bc74a0db476d22db8a584b856
Laura Beaufort <lbeaufort@fec.gov>
2024-01-11 22:26:10 -0500

    Add committee ID to responses

--- django-backend/fecfiler/settings/base.py
+++ django-backend/fecfiler/settings/base.py
@@ -207,7 +207,6 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
-    "EXCEPTION_HANDLER": "fecfiler.utils.custom_exception_handler",
 }
 
 
@@ -222,7 +221,10 @@ def get_env_logging_config(prod=False):
             },
             "plain_console": {
                 "()": structlog.stdlib.ProcessorFormatter,
-                "processor": structlog.dev.ConsoleRenderer(),
+                "processor": structlog.dev.ConsoleRenderer(
+                    colors=True,
+                    exception_formatter=structlog.dev.rich_traceback
+                ),
             },
             "key_value": {
                 "()": structlog.stdlib.ProcessorFormatter,
@@ -279,6 +281,7 @@ def get_env_logging_processors(prod=False):
     """
 
     if prod:
+        # JSON in production
         return [
             structlog.contextvars.merge_contextvars,
             structlog.stdlib.filter_by_level,
@@ -287,10 +290,8 @@ def get_env_logging_processors(prod=False):
             structlog.stdlib.add_log_level,
             structlog.stdlib.PositionalArgumentsFormatter(),
             structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
             structlog.processors.UnicodeDecoder(),
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
-            # JSON in production
             structlog.processors.JSONRenderer(),
         ]
     else:
@@ -302,7 +303,6 @@ def get_env_logging_processors(prod=False):
             structlog.stdlib.add_log_level,
             structlog.stdlib.PositionalArgumentsFormatter(),
             structlog.processors.StackInfoRenderer(),
-            structlog.processors.format_exc_info,
             structlog.processors.UnicodeDecoder(),
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
         ]
