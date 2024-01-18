4ef57a90bb428c304254e0b2eced28e902969e0e
Matt Travers <matt.travers@tcg.com>
2024-01-18 14:52:51 -0500

    Update migration file to match develop updates

--- django-backend/fecfiler/reports/migrations/0003_form1m_date_committee_met_requirements_and_more.py
+++ django-backend/fecfiler/reports/migrations/0003_form1m_date_committee_met_requirements_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('reports', '0010_form1m_committee_name_form1m_contact_affiliated_and_more'),
+        ('reports', '0002_initial'),
     ]
 
     operations = [
