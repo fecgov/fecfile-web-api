45044b0ac4d1fb630946516fffcdcf86c1de79dc
Laura Beaufort <lbeaufort@fec.gov>
2024-01-30 10:37:50 -0500

    Fine-tune json exception settings

--- django-backend/fecfiler/settings/base.py
+++ django-backend/fecfiler/settings/base.py
@@ -293,7 +293,12 @@ def get_env_logging_processors(prod=False):
             structlog.stdlib.add_logger_name,
             structlog.stdlib.add_log_level,
             structlog.stdlib.PositionalArgumentsFormatter(),
-            structlog.processors.format_exc_info,
+            structlog.processors.ExceptionRenderer(
+                structlog.tracebacks.ExceptionDictTransformer(
+                    show_locals=False,
+                    max_frames=6
+                )
+            ),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.UnicodeDecoder(),
             structlog.stdlib.ProcessorFormatter.wrap_for_formatter,
