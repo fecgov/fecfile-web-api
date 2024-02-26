6347a873961c95641a4860b6ee6c3b16d9baad1c
toddlees <todd.lees@tcg.com>
2024-02-26 12:53:38 -0500

    see mock_openfec value

--- django-backend/fecfiler/mock_openfec/test_load_committee_data.py
+++ django-backend/fecfiler/mock_openfec/test_load_committee_data.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.core.management import call_command
-from fecfiler.settings import MOCK_OPENFEC_REDIS_URL
+from fecfiler.settings import MOCK_OPENFEC_REDIS_URL, MOCK_OPENFEC
 import redis
 from fecfiler.mock_openfec.mock_endpoints import COMMITTEE_DATA_REDIS_KEY
 
@@ -12,6 +12,7 @@ class LoadTestDataCommandTest(TestCase):
 
     def test_load_test_data(self):
         call_command("load_committee_data")
+        print(f"MOCK_OPENFEC {MOCK_OPENFEC}")
         redis_instance = redis.Redis.from_url(MOCK_OPENFEC_REDIS_URL)
         committee_data = redis_instance.get(COMMITTEE_DATA_REDIS_KEY)
         self.assertEqual(len(committee_data), 1)
