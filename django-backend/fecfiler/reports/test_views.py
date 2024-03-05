44e2d7efd8881272cd1b2178524dc9b53e83eac5
Elaine Krauss <elaine.krauss@tcg.com>
2024-03-05 16:33:04 -0500

    Cleans up test_views

--- django-backend/fecfiler/reports/test_views.py
+++ django-backend/fecfiler/reports/test_views.py
@@ -3,11 +3,15 @@ from django.test import RequestFactory, TestCase
 from fecfiler.reports.views import ReportViewSet
 from fecfiler.user.models import User
 from django.core.management import call_command
-from rest_framework.response import Response
 
 
 class CommitteeMemberViewSetTest(TestCase):
-    fixtures = ["C01234567_user_and_committee", "test_f3x_reports", "test_f24_reports", "test_f99_reports"]
+    fixtures = [
+        "C01234567_user_and_committee",
+        "test_f3x_reports",
+        "test_f24_reports",
+        "test_f99_reports"
+    ]
 
     def setUp(self):
         self.user = User.objects.get(id="12345678-aaaa-bbbb-cccc-111122223333")
@@ -48,22 +52,3 @@ class CommitteeMemberViewSetTest(TestCase):
             self.assertTrue(False)
         except:
             self.assertTrue(True)
-
-
-
-    # def test_remove_member(self):
-    #     membership_uuid = UUID("136a21f2-66fe-4d56-89e9-0d1d4612741c")
-    #     view = CommitteeMembershipViewSet()
-    #     request = self.factory.get(
-    #         "/api/v1/committee-members/{membership_uuid}/remove-member"
-    #     )
-    #     request.user = self.user
-    #     request.session = {
-    #         "committee_uuid": UUID('11111111-2222-3333-4444-555555555555')}
-    #     request.method = "DELETE"
-    #     request.query_params = dict()
-    #     view.kwargs = {"pk": membership_uuid}
-    #     view.request = request
-    #     response = view.remove_member(
-    #         request, membership_uuid)
-    #     self.assertEqual(response.status_code, 200)
