6b419c912aa18d31b8e68e52730bc576a0db8d67
David Heitzer <david.heitzer@tcg.com>
2024-03-18 11:27:50 -0400

    1721 logout fix

--- django-backend/fecfiler/authentication/views.py
+++ django-backend/fecfiler/authentication/views.py
@@ -13,6 +13,7 @@ from fecfiler.settings import (
     OIDC_OP_LOGOUT_ENDPOINT,
     ALTERNATIVE_LOGIN,
     FFAPI_COOKIE_DOMAIN,
+    FFAPI_LOGIN_DOT_GOV_COOKIE_NAME,
 )
 
 from rest_framework.response import Response
@@ -63,6 +64,7 @@ def handle_invalid_login(username):
 
 
 def delete_user_logged_in_cookies(response):
+    response.delete_cookie(FFAPI_LOGIN_DOT_GOV_COOKIE_NAME, domain=FFAPI_COOKIE_DOMAIN)
     response.delete_cookie("oidc_state")
     response.delete_cookie("csrftoken", domain=FFAPI_COOKIE_DOMAIN)
 
@@ -71,6 +73,12 @@ def delete_user_logged_in_cookies(response):
 @require_http_methods(["GET"])
 def login_redirect(request):
     redirect = HttpResponseRedirect(LOGIN_REDIRECT_CLIENT_URL)
+    redirect.set_cookie(
+        FFAPI_LOGIN_DOT_GOV_COOKIE_NAME,
+        "true",
+        domain=FFAPI_COOKIE_DOMAIN,
+        secure=True,
+    )
     return redirect
 
 
