c25d008672e935830027a0e22a05cfbc9dbb6336
David Heitzer <david.heitzer@tcg.com>
2024-04-04 10:29:29 -0400

    789 fix tests

--- django-backend/fecfiler/openfec/test_views.py
+++ django-backend/fecfiler/openfec/test_views.py
@@ -46,7 +46,7 @@ class OpenfecViewSetTest(TestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(response.data["results"][0]["committee_id"], "C12345678")
             self.assertEqual(
-                response.data["results"][0]["committee_name"], "Test Committee"
+                response.data["results"][0]["name"], "Test Committee"
             )
             self.assertEqual(response.data["results"][0]["committee_type"], "O")
 
