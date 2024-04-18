d6b2bddd429ce457fc3998b9a31105002a252166
David Heitzer <david.heitzer@tcg.com>
2024-04-18 10:57:18 -0400

    1755 cr comments

--- django-backend/fecfiler/feedback/test_views.py
+++ django-backend/fecfiler/feedback/test_views.py
@@ -1,7 +1,10 @@
 from unittest import mock
 
+import github3
 from django.test import RequestFactory, TestCase
 from fecfiler.user.models import User
+from github3 import GitHub
+from github3.repos import Repository
 from rest_framework.test import APIClient
 
 
@@ -12,8 +15,12 @@ class UserViewSetTest(TestCase):
         self.user = User.objects.get(id="12345678-aaaa-bbbb-cccc-111122223333")
         self.factory = RequestFactory()
 
-    @mock.patch("github3.login")
-    def test_submit_feedback_happy_path(self, mock_github3):
+    @mock.patch.object(github3, "login", return_value=GitHub)
+    @mock.patch.object(GitHub, "repository", return_value=Repository)
+    @mock.patch.object(Repository, "create_issue")
+    def test_submit_feedback_happy_path(
+        self, mock_repo_create_issue, mock_github3_repo, mock_github3_login
+    ):
         client = APIClient()
         client.force_authenticate(user=self.user)
         test_headers = {"HTTP_USER_AGENT": "test_user_agent"}
@@ -21,13 +28,22 @@ class UserViewSetTest(TestCase):
             "action": "test_action",
             "feedback": "test_feedback",
             "about": "test_about",
-            "location": "test_location",
+            "location": "<b>test_location</b>",
         }
 
         response = client.post(
             "/api/v1/feedback/submit/", data=test_post_data, **test_headers
         )
 
-        mock_github3.assert_called_once()
+        mock_github3_login.assert_called_once()
+        mock_github3_repo.assert_called_once()
+        mock_repo_create_issue.assert_called_once()
+        title, body = mock_repo_create_issue.call_args
+        body_content = body["body"]
+        self.assertEqual("User feedback on &lt;b&gt;test_location&lt;/b&gt;", title[0])
+        self.assertTrue("test_action" in body_content)
+        self.assertTrue("test_feedback" in body_content)
+        self.assertTrue("test_about" in body_content)
+        self.assertTrue("&lt;b&gt;test_location&lt;/b&gt;" in body_content)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, {"status": "feedback submitted"})
--- django-backend/fecfiler/feedback/views.py
+++ django-backend/fecfiler/feedback/views.py
@@ -4,6 +4,7 @@ from fecfiler.settings import FECFILE_GITHUB_TOKEN
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from django.utils.html import escape
 
 from .serializers import FeedbackSerializer
 
@@ -17,27 +18,35 @@ class FeedbackViewSet(viewsets.ViewSet):
         url_path="submit",
     )
     def submit_feedback(self, request):
-        serializer = FeedbackSerializer(data=request.data, context={"request": request})
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        title = "User feedback on " + serializer.validated_data["location"]
+        try:
+            serializer = FeedbackSerializer(
+                data=request.data, context={"request": request}
+            )
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            title = "User feedback on " + serializer.validated_data["location"]
 
-        body = (
-            "## What were you trying to do and how can we improve it?\n %s \n\n"
-            "## General feedback?\n %s \n\n"
-            "## Tell us about yourself\n %s \n\n"
-            "## Details\n"
-            "* URL: %s \n"
-            "* User Agent: %s"
-        ) % (
-            serializer.validated_data["action"],
-            serializer.validated_data["feedback"],
-            serializer.validated_data["about"],
-            serializer.validated_data["location"],
-            request.META["HTTP_USER_AGENT"],
-        )
+            body = (
+                "## What were you trying to do and how can we improve it?\n %s \n\n"
+                "## General feedback?\n %s \n\n"
+                "## Tell us about yourself\n %s \n\n"
+                "## Details\n"
+                "* URL: %s \n"
+                "* User Agent: %s"
+            ) % (
+                serializer.validated_data["action"],
+                serializer.validated_data["feedback"],
+                serializer.validated_data["about"],
+                serializer.validated_data["location"],
+                request.META["HTTP_USER_AGENT"],
+            )
 
-        client = github3.login(token=FECFILE_GITHUB_TOKEN)
-        client.repository("fecgov", "fecfile-feedback").create_issue(title, body=body)
+            client = github3.login(token=FECFILE_GITHUB_TOKEN)
+            client.repository("fecgov", "fecfile-feedback").create_issue(
+                escape(title), body=escape(body)
+            )
 
-        return Response({"status": "feedback submitted"})
+            return Response({"status": "feedback submitted"})
+        except Exception as error:
+            logger.error(f"An error occured while submitting feedback: {str(error)}")
+            raise error
