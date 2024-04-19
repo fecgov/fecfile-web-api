c7ec3a824bfb9fc2d6941a8149d4524f5ac95be8
Sasha Dresden <sasha.dresden@tcg.com>
2024-04-19 15:43:37 -0400

    Move committee info from forms to report

--- django-backend/fecfiler/reports/form_24/tests/test_models.py
+++ django-backend/fecfiler/reports/form_24/tests/test_models.py
@@ -8,23 +8,18 @@ class F24TestCase(TestCase):
 
     def setUp(self):
         self.valid_form_24 = Form24(
-            street_1="22 Test Street",
-            street_2="Unit B",
-            city="Testopolis",
-            state="AL",
-            zip="12345",
             report_type_24_48="24",
             original_amendment_date="2023-11-01",
         )
 
     def test_save_and_delete(self):
         self.valid_form_24.save()
-        form_24_from_db = Form24.objects.get(street_1="22 Test Street")
+        form_24_from_db = Form24.objects.get(report_type_24_48="24")
         self.assertIsInstance(form_24_from_db, Form24)
         self.assertEquals(
             form_24_from_db.original_amendment_date, datetime.date(2023, 11, 1)
         )
         form_24_from_db.delete()
         self.assertRaises(
-            Form24.DoesNotExist, Form24.objects.get, street_1="22 Test Street"
+            Form24.DoesNotExist, Form24.objects.get, report_type_24_48="24"
         )
