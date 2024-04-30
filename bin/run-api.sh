f442332b4f801f5d5aa7d6c0b3a4da961c5f9e1a
David Heitzer <david.heitzer@tcg.com>
2024-04-30 13:51:36 -0400

    632 circle testing

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -3,4 +3,4 @@ cd django-backend
 # Run migrations and application
 ./manage.py migrate --no-input --traceback --verbosity 3 &&
 	python manage.py create_committee_views &&
-	gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9
+	exec gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9
