9bdcf394ac397492c0b1c814e600dd908da8e681
David Heitzer <david.heitzer@tcg.com>
2024-06-12 15:53:50 -0400

    816 dev testing

--- tasks.py
+++ tasks.py
@@ -50,7 +50,7 @@ def _detect_space(repo, branch=None):
 DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
-    ("dev", lambda _, branch: branch == "develop"),
+    ("dev", lambda _, branch: branch == "feature/816-testing"),
 )
 
 
@@ -81,9 +81,7 @@ def _login_to_cf(ctx, space):
             "   cf create-service cloud-gov-service-account space-deployer"
             " [my-service-account-name]"
         )
-        print(
-            "   cf create-service-key  [my-server-account-name] [my-service-key-name]"
-        )
+        print("   cf create-service-key  [my-server-account-name] [my-service-key-name]")
         print("   cf service-key  [my-server-account-name] [my-service-key-name]")
 
         exit(1)
@@ -97,7 +95,9 @@ def _do_deploy(ctx, space, app):
 
     cmd = "push --strategy rolling" if existing_deploy.ok else "push"
     new_deploy = ctx.run(
-        f"cf {cmd} {app} -f {manifest_filename}", echo=True, warn=True,
+        f"cf {cmd} {app} -f {manifest_filename}",
+        echo=True,
+        warn=True,
     )
     return new_deploy
 
@@ -137,17 +137,13 @@ def _rollback(ctx, app):
         hide=True,
         warn=True,
     )
-    active_deployments = (
-        json.loads(status.stdout).get("pagination").get("total_results")
-    )
+    active_deployments = json.loads(status.stdout).get("pagination").get("total_results")
     # Try to roll back
     if active_deployments > 0:
         print("Attempting to roll back any deployment in progress...")
         # Show the in-between state
         ctx.run(f"cf app {app}", echo=True, warn=True)
-        cancel_deploy = ctx.run(
-            f"cf cancel-deployment {app}", echo=True, warn=True
-        )
+        cancel_deploy = ctx.run(f"cf cancel-deployment {app}", echo=True, warn=True)
         if cancel_deploy.ok:
             print("Successfully cancelled deploy. Check logs.")
         else:
