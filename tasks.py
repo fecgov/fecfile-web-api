6ed57e963c41f5cea86605f9282304045a9b4bee
David Heitzer <david.heitzer@tcg.com>
2024-06-13 15:53:36 -0400

    816 dev testing

--- tasks.py
+++ tasks.py
@@ -50,7 +50,7 @@ def _detect_space(repo, branch=None):
 DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
-    ("dev", lambda _, branch: branch == "feature/816-testing"),
+    ("dev", lambda _, branch: branch == "feature/816"),
 )
 
 
