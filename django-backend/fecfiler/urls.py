f5366ff376f8b0afccf177f6a81ba5eff429873e
Elaine Krauss <elaine.krauss@tcg.com>
2024-06-21 10:24:04 -0400

    Adds a new status endpoint

--- django-backend/fecfiler/urls.py
+++ django-backend/fecfiler/urls.py
@@ -1,16 +1,21 @@
 from django.conf.urls import include
 from django.urls import re_path
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 BASE_V1_URL = r"^api/v1/"
 
 
 @api_view(["GET"])
-def test_celery(request):
+def test_celery(_request):
     from fecfiler.celery import debug_task
 
     debug_task.delay()
+    return Response(status=200)
+
+@api_view(["GET"])
+@permission_classes([])
+def get_api_status(_request):
     return Response(status=200)
 
 
@@ -27,4 +32,5 @@ urlpatterns = [
     re_path(BASE_V1_URL, include("fecfiler.feedback.urls")),
     re_path(r"^oidc/", include("mozilla_django_oidc.urls")),
     re_path(r"^celery-test/", test_celery),
+    re_path(r"^status/", get_api_status)
 ]
