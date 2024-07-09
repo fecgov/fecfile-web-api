f90d3c13cc9688115a3093918eb8e8fab67cda35
Sasha Dresden <sasha.dresden@proton.me>
2024-07-09 11:23:46 -0400

    Update logic of calendar_ytd_per_election_office comparison for itemization to also check parent and grandparent transaction's calendar_ytd_per_election_office

--- django-backend/fecfiler/transactions/managers.py
+++ django-backend/fecfiler/transactions/managers.py
@@ -56,7 +56,19 @@ class TransactionManager(SoftDeleteManager):
             ),
             When(
                 transaction_type_identifier__in=schedule_e_over_two_hundred_types,
-                then=Q(_calendar_ytd_per_election_office__gt=Value(Decimal(200))),
+                then=Q(
+                    Q(
+                        parent_transaction__parent_transaction___calendar_ytd_per_election_office__gt=Value(  # noqa
+                            Decimal(200)
+                        )
+                    )
+                    | Q(
+                        parent_transaction___calendar_ytd_per_election_office__gt=Value(
+                            Decimal(200)
+                        )
+                    )
+                    | Q(_calendar_ytd_per_election_office__gt=Value(Decimal(200)))
+                ),
             ),
             default=Value(True),
             output_field=BooleanField(),
