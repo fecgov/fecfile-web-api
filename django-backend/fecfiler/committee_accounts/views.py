dfb39c4445606c71c739fcb01af8f7bbe65dcc9b
Elaine Krauss <elaine.krauss@tcg.com>
2024-07-30 10:57:44 -0400

    Updates endpoints

--- django-backend/fecfiler/committee_accounts/views.py
+++ django-backend/fecfiler/committee_accounts/views.py
@@ -15,7 +15,6 @@ from fecfiler.openfec.views import retrieve_recent_f1
 from fecfiler.mock_openfec.mock_endpoints import recent_f1
 from fecfiler.web_services.dot_fec.dot_fec_serializer import FS_STR
 from fecfiler.settings import MOCK_OPENFEC_REDIS_URL
-import requests
 from .serializers import CommitteeAccountSerializer, CommitteeMembershipSerializer
 from django.db.models.fields import TextField
 from django.db.models.functions import Coalesce, Concat
@@ -211,18 +210,10 @@ def register_committee(committee_id, user):
 
     if MOCK_OPENFEC_REDIS_URL:
         f1 = recent_f1(committee_id)
-        f1_email = (f1 or {}).get("email")
     else:
         f1 = retrieve_recent_f1(committee_id)
-        dot_fec_url = (f1 or {}).get("fec_url")
-        if dot_fec_url:
-            response = requests.get(
-                dot_fec_url,
-                headers={"User-Agent": "fecfile-api"},
-            )
-            dot_fec_content = response.content.decode("utf-8")
-            f1_line = dot_fec_content.split("\n")[1]
-            f1_email = f1_line.split(FS_STR)[11]
+
+    f1_email = (f1 or {}).get("email")
 
     failure_reason = None
 
