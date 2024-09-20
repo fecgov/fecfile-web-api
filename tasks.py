e124e7c7b304a53355a043761c9c8ae1114d6e99
Elaine Krauss <elaine.krauss@tcg.com>
2024-09-20 11:15:03 -0400

    More updating of deploy logic

--- tasks.py
+++ tasks.py
@@ -49,7 +49,7 @@ def _detect_space(repo, branch=None):
 
 
 DEPLOY_RULES = (
-    ("test", lambda _, branch: branch == "main"),
+    ("test", lambda _, branch: branch == "feature/1572-test-environment-b"),
     ("stage", lambda _, branch: branch.startswith("release")),
     ("dev", lambda _, branch: branch == "develop"),
 )
