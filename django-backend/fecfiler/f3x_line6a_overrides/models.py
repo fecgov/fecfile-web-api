b1bb6a7dd31ad3c6a981603025b084dc9964d19e
David Heitzer <david.heitzer@tcg.com>
2024-10-22 13:45:02 -0400

    1118 update summaries

--- django-backend/fecfiler/f3x_line6a_overrides/models.py
+++ django-backend/fecfiler/f3x_line6a_overrides/models.py
@@ -1,4 +1,5 @@
-from django.db import models
+from django.db import models, transaction as db_transaction
+from fecfiler.reports.models import Report
 import uuid
 
 
@@ -17,6 +18,13 @@ class F3xLine6aOverride(models.Model):
     )
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        with db_transaction.atomic():
+            super(F3xLine6aOverride, self).save(*args, **kwargs)
+            Report.objects.get_queryset().filter(
+                coverage_from_date__year__gte=self.year,
+            ).update(calculation_status=None)
 
     class Meta:
         db_table = "f3x_line6a_overrides"
--- django-backend/fecfiler/f3x_line6a_overrides/test_models.py
+++ django-backend/fecfiler/f3x_line6a_overrides/test_models.py
@@ -1,10 +1,24 @@
 from decimal import Decimal
 from django.test import TestCase
 from .models import F3xLine6aOverride
+from datetime import datetime
+from fecfiler.reports.tests.utils import create_form3x
+from fecfiler.committee_accounts.models import CommitteeAccount
+from fecfiler.committee_accounts.utils import create_committee_view
+from fecfiler.contacts.tests.utils import create_test_individual_contact
+from fecfiler.web_services.summary.tasks import CalculationState
+from fecfiler.reports.models import Report
 
 
 class F3xLine6aOverrideTestCase(TestCase):
     fixtures = ["test_f3x_line6a_overrides"]
+
+    def setUp(self):
+        self.committee = CommitteeAccount.objects.create(committee_id="C00000000")
+        create_committee_view(self.committee.id)
+        self.contact_1 = create_test_individual_contact(
+            "last name", "First name", self.committee.id
+        )
 
     def test_f3x_line6a_override_crud(self):
         self.test_f3x_line6a_override = F3xLine6aOverride(
@@ -33,6 +47,31 @@ class F3xLine6aOverrideTestCase(TestCase):
         ).delete()
 
         with self.assertRaises(F3xLine6aOverride.DoesNotExist):
-            override_2024 = F3xLine6aOverride.objects.get(
+            F3xLine6aOverride.objects.get(
                 id="422095b3-e0a7-4499-9d2c-5efb3858fce4",
             )
+
+    def test_marks_future_reports_calculation_status_none(self):
+        f3x1 = create_form3x(
+            self.committee,
+            datetime.strptime("2015-01-30", "%Y-%m-%d").date(),
+            datetime.strptime("2015-02-28", "%Y-%m-%d").date(),
+            {},
+            "12C",
+        )
+        f3x2 = create_form3x(
+            self.committee,
+            datetime.strptime("2020-01-30", "%Y-%m-%d").date(),
+            datetime.strptime("2020-02-28", "%Y-%m-%d").date(),
+            {},
+            "12C",
+        )
+        Report.objects.update(calculation_status=CalculationState.SUCCEEDED)
+
+        F3xLine6aOverride.objects.create(
+            year="2016",
+            cash_on_hand=100,
+        )
+
+        self.assertIsNotNone(Report.objects.get(pk=f3x1.id).calculation_status)
+        self.assertIsNone(Report.objects.get(pk=f3x2.id).calculation_status)
