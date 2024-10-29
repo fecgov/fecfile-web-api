d50e3edf280e0918e579cb4880548a9c79e9e5cc
Elaine Krauss <elaine.krauss@tcg.com>
2024-10-29 18:37:51 -0400

    Restores the original summary views

--- django-backend/fecfiler/web_services/summary/views.py
+++ django-backend/fecfiler/web_services/summary/views.py
@@ -26,12 +26,9 @@ class SummaryViewSet(viewsets.ViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         report_id = serializer.validated_data["report_id"]
         report = Report.objects.get(id=report_id)
-        if (report.calculation_status != CalculationState.CALCULATING):
-            report.calculation_status = CalculationState.CALCULATING
-            report.save()
-            logger.debug(f"Starting Celery Task calculate_summary for report :{report_id}")
-            task = calculate_summary.apply_async((report_id,), retry=False)
-            logger.debug(f"Status from calculate_summary report {report_id}: {task.status}")
-            return Response({"status": "summary task created"})
-        else:
-            return Response({"status": "summary calculation in progress"})
+        report.calculation_status = CalculationState.CALCULATING
+        report.save()
+        logger.debug(f"Starting Celery Task calculate_summary for report :{report_id}")
+        task = calculate_summary.apply_async((report_id,), retry=False)
+        logger.debug(f"Status from calculate_summary report {report_id}: {task.status}")
+        return Response({"status": "summary task created"})
