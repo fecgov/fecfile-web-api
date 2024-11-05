4dccb29f278a32944cf1f23adfb4cb4d69b9b251
toddlees <todd.lees@tcg.com>
2024-11-05 16:45:37 -0500

    remove comment

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -4,4 +4,3 @@ cd django-backend
 ./manage.py migrate --no-input --traceback --verbosity 3 &&
 	python manage.py create_committee_views &&
 	exec gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9
-# Runs create_committee_views to update view definition in case it's changed
