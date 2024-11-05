5716e788962eba1fa693fcd7f34ea750f7293ebe
toddlees <todd.lees@tcg.com>
2024-11-05 16:14:18 -0500

    stage version of 1714 patch

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -2,4 +2,6 @@ cd django-backend
 
 # Run migrations and application
 ./manage.py migrate --no-input --traceback --verbosity 3 &&
+	python manage.py create_committee_views &&
 	exec gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9
+# Runs create_committee_views to update view definition in case it's changed
