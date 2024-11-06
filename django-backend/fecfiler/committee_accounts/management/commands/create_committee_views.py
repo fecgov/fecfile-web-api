1ee1efb2fde8412dcc1f8b6064bb025ea77f5df1
Elaine Krauss <elaine.krauss@tcg.com>
2024-11-06 12:45:15 -0500

    Converts logging to Structlog for consistency

--- django-backend/fecfiler/committee_accounts/management/commands/create_committee_views.py
+++ django-backend/fecfiler/committee_accounts/management/commands/create_committee_views.py
@@ -2,17 +2,18 @@ from django.core.management.base import BaseCommand
 from fecfiler.committee_accounts.models import CommitteeAccount
 from fecfiler.committee_accounts.utils import create_committee_view
 
+import structlog
+
+
+logger = structlog.get_logger(__name__)
+
 
 class Command(BaseCommand):
     help = "Create or update committee views"
 
     def handle(self, *args, **options):
         for committee in CommitteeAccount.objects.all():
-            self.stdout.write(
-                self.style.NOTICE(f"Running create_committee_view on {committee.id}")
-            )
+            logger.info(f"Running create_committee_view on {committee.id}")
             create_committee_view(committee.id)
 
-        self.stdout.write(
-            self.style.SUCCESS("Successfully created/updated committee views")
-        )
+        logger.info("Successfully created/updated committee views")
