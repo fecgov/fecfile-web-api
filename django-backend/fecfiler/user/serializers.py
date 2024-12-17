0f68e39b167ba73177636c8bc7f4437c9a7b9452
David Heitzer <david.heitzer@tcg.com>
2024-12-17 17:32:58 -0500

    1824 add session sec consent

--- django-backend/fecfiler/user/serializers.py
+++ django-backend/fecfiler/user/serializers.py
@@ -1,19 +1,20 @@
 from .models import User
 from rest_framework import serializers
+from rest_framework.serializers import BooleanField
+import datetime
 import logging
 
 logger = logging.getLogger(__name__)
 
 
 class CurrentUserSerializer(serializers.ModelSerializer):
+    consent_for_one_year = BooleanField(write_only=True, default=None)
+
     class Meta:
         model = User
         fields = [
             "first_name",
             "last_name",
             "email",
-            "security_consent_exp_date",
         ]
-        read_only_fields = [
-            "email",
-        ]
+        read_only_fields = ["email", "security_consented"]
--- django-backend/fecfiler/user/views.py
+++ django-backend/fecfiler/user/views.py
@@ -1,22 +1,38 @@
-from rest_framework import viewsets
-from rest_framework.decorators import action
-from .serializers import CurrentUserSerializer
-from django.http import HttpResponseBadRequest
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+import datetime
 import logging
 
 logger = logging.getLogger(__name__)
 
+session_security_consented_key = "session_security_consented"
 
-class UserViewSet(viewsets.GenericViewSet):
-    @action(detail=False, methods=["GET", "PUT"], serializer_class=CurrentUserSerializer)
-    def current(self, request):
-        if request.method == 'GET':
-            serializer = self.get_serializer(request.user)
-            return Response(serializer.data)
-        if request.method == 'PUT':
-            serializer = self.get_serializer(request.user, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return Response(serializer.data)
-            return HttpResponseBadRequest()
+
+class UserViewSet(ModelViewSet):
+
+    def retrieve(self, request, *args, **kwargs):
+        serializer = self.get_serializer(request.user)
+        augmented_response = {
+            "security_consented": (
+                datetime.datetime.now() <= request.user.security_consent_exp_date
+                or request.session[session_security_consented_key]
+            )
+        }
+        augmented_response.update(serializer.data)
+        return Response(augmented_response)
+
+    def update(self, request, *args, **kwargs):
+        add_session_consent = False
+        consent_for_one_year = request.data.get("consent_for_one_year", None)
+        if consent_for_one_year:
+            request.data["security_consent_exp_date"] = (
+                datetime.datetime.now() + datetime.timedelta(days=365)
+            )
+        else:
+            add_session_consent = True
+        serializer = self.get_serializer(request.user, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        if add_session_consent:
+            request.session[session_security_consented_key] = True
+        return Response(serializer.data)
