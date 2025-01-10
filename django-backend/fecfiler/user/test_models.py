9bee8884659fe9d41757b1289930e02f24689725
Sasha Dresden <sasha.dresden@proton.me>
2025-01-10 16:10:35 -0500

    Fix wierd test error with user membership caused by test spillover

--- django-backend/fecfiler/user/test_models.py
+++ django-backend/fecfiler/user/test_models.py
@@ -4,19 +4,17 @@ from fecfiler.committee_accounts.models import CommitteeAccount, Membership
 
 
 class UserModelTestCase(TestCase):
-    fixtures = ["test_committee_accounts"]
+
+    def setUp(self):
+        self.committee = CommitteeAccount.objects.create(committee_id="C87654321")
 
     def test_redeem_pending_membership(self):
-        committee_account = CommitteeAccount.objects.create(
-            committee_id="C87654321",
-        )
-
         new_membership = Membership(
-            committee_account=committee_account,
+            committee_account=self.committee,
             role=Membership.CommitteeRole.COMMITTEE_ADMINISTRATOR,
-            pending_email="test_1234@test.com"
+            pending_email="test_1234@test.com",
         )
-        new_membership.committee_account_id = committee_account.id
+        new_membership.committee_account_id = self.committee.id
         new_membership.save()
 
         self.assertEquals(
@@ -24,7 +22,7 @@ class UserModelTestCase(TestCase):
         )
 
         new_user = User.objects.create_user(
-            email="test_1234@test.com", user_id='5e3c145f-a813-46c7-af5a-5739304acc27'
+            email="test_1234@test.com", user_id="5e3c145f-a813-46c7-af5a-5739304acc27"
         )
 
         self.assertEquals(
