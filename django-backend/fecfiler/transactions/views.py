b8ba24ec77e7a3d9fa751c91c0a583f63ea741dc
David Heitzer <david.heitzer@tcg.com>
2025-01-23 18:42:06 -0500

    1662 added debts

--- django-backend/fecfiler/transactions/views.py
+++ django-backend/fecfiler/transactions/views.py
@@ -245,7 +245,6 @@ class TransactionViewSet(CommitteeOwnedViewMixin, ModelViewSet):
         return Response(response, status=status.HTTP_404_NOT_FOUND)
 
     def save_transaction(self, transaction_data, request):
-        prior_expenditure_amount = None
         committee_id = request.session["committee_uuid"]
         report_ids = transaction_data.pop("report_ids", [])
         children = transaction_data.pop("children", [])
@@ -270,10 +269,6 @@ class TransactionViewSet(CommitteeOwnedViewMixin, ModelViewSet):
             schedule_serializer = SCHEDULE_SERIALIZERS.get(schedule)(
                 transaction_instance.get_schedule(), data=transaction_data
             )
-            if transaction_instance.schedule_b is not None:
-                prior_expenditure_amount = (
-                    transaction_instance.schedule_b.expenditure_amount
-                )
         else:
             transaction_serializer = TransactionSerializer(
                 data=transaction_data, context={"request": request}
@@ -353,9 +348,8 @@ class TransactionViewSet(CommitteeOwnedViewMixin, ModelViewSet):
         children and grandchildren transactions with any changes to the committee name
         """
         update_dependent_children(transaction_instance)
-        delete_carried_forward_loans_if_needed(
-            transaction_instance, committee_id, prior_expenditure_amount
-        )
+        delete_carried_forward_loans_if_needed(transaction_instance, committee_id)
+        delete_carried_forward_debts_if_needed(transaction_instance, committee_id)
         return self.queryset.get(id=transaction_instance.id)
 
     @action(detail=False, methods=["put"], url_path=r"multisave/reattribution")
@@ -444,17 +438,8 @@ def stringify_queryset(qs):
     return s
 
 
-def delete_carried_forward_loans_if_needed(
-    transaction: Transaction, committee_id, prior_expenditure_amount=None
-):
-    if (
-        transaction.schedule_b is not None
-        and transaction.transaction_type_identifier == "LOAN_REPAYMENT_MADE"
-        and (
-            prior_expenditure_amount is None
-            or transaction.schedule_b.expenditure_amount != prior_expenditure_amount
-        )
-    ):
+def delete_carried_forward_loans_if_needed(transaction: Transaction, committee_id):
+    if transaction.is_loan_repayment() is True:
         current_loan_read_model_dict = (
             get_read_model(committee_id)
             .objects.values("loan_balance", "loan_id", "id")
@@ -470,6 +455,28 @@ def delete_carried_forward_loans_if_needed(
             transactions_to_delete = list(
                 Transaction.objects.filter(
                     loan_id=original_loan_id,
+                    reports__id__in=models.Subquery(future_reports.values("id")),
+                )
+            )
+            for transaction_to_delete in transactions_to_delete:
+                transaction_to_delete.delete()
+
+
+def delete_carried_forward_debts_if_needed(transaction: Transaction, committee_id):
+    if transaction.is_debt_repayment() is True:
+        current_debt_read_model_dict = (
+            get_read_model(committee_id)
+            .objects.values("balance_at_close", "debt_id", "id")
+            .get(pk=transaction.debt_id)
+        )
+        current_debt_balance = current_debt_read_model_dict["balance_at_close"]
+        original_debt_id = current_debt_read_model_dict["debt_id"]
+        if current_debt_balance == 0:
+            current_report = transaction.reports.filter(form_3x__isnull=False).first()
+            future_reports = current_report.get_future_in_progress_reports()
+            transactions_to_delete = list(
+                Transaction.objects.filter(
+                    debt_id=original_debt_id,
                     reports__id__in=models.Subquery(future_reports.values("id")),
                 )
             )
