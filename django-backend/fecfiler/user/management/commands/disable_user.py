c8205099a74392e1b326858dd29ff36c4c047f38
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-02-10 19:03:46 -0500

    FECFILE-1992: Linting.

--- django-backend/fecfiler/user/management/commands/disable_user.py
+++ django-backend/fecfiler/user/management/commands/disable_user.py
@@ -1,19 +1,20 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 
+
 class Command(BaseCommand):
     help = 'Disable (or re-enable) a user with a given email.'
 
     def add_arguments(self, parser):
-        parser.add_argument('email', type=str, 
+        parser.add_argument('email', type=str,
                 help='The email address of the user to be disabled/enabled.')
         # add an --enable flag that defaults to false if not found
-        parser.add_argument('-d', '--enable', action='store_true', 
+        parser.add_argument('-d', '--enable', action='store_true',
                 help='Flag to instead (re-)enable the user.')
 
     def handle(self, *args, **options):
         User = get_user_model()
-        
+
         try:
             user = User.objects.get(email=options['email'])
         except User.DoesNotExist:
@@ -24,4 +25,4 @@ class Command(BaseCommand):
 
         self.stdout.write(self.style.SUCCESS(
             f'The is_active flag for user [{user.email}] set to: {user.is_active}'
-            ))
+        ))
