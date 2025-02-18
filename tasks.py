f08c7d1a34c7ee78c72a164047b29279df705714
David Heitzer <david.heitzer@tcg.com>
2025-02-18 13:28:24 -0500

    1908 app csp

--- tasks.py
+++ tasks.py
@@ -48,7 +48,7 @@ def _detect_space(repo, branch=None):
 DEPLOY_RULES = (
     ("test", lambda _, branch: branch == "main"),
     ("stage", lambda _, branch: branch.startswith("release")),
-    ("dev", lambda _, branch: branch == "develop"),
+    ("dev", lambda _, branch: branch == "feature/1908-testing-in-dev"),
 )
 
 
