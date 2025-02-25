706b0a97e1f1385036e05f6f4e787f2665b79151
toddlees <todd.lees@tcg.com>
2025-02-25 14:27:13 -0500

    reorganize memo text tests

--- django-backend/fecfiler/web_services/dot_fec/tests/dot_fec_record_testing/test_memo_text.py
+++ django-backend/fecfiler/web_services/dot_fec/tests/dot_fec_record_testing/test_memo_text.py
@@ -34,62 +34,73 @@ class DotFECTextRecordsTestCase(TestCase):
             "last name", "First name", self.committee.id
         )
 
-    def create_rows(self, itemized: bool):
-        self.transaction = create_schedule_a(
+    def test_f3x_memo(self):
+        f3x_memo = create_report_memo(
+            self.committee,
+            self.f3x,
+            text4000="F3X_MEMO_TEXT",
+        )
+        f3x_memo_row = serialize_instance("Text", f3x_memo).split(FS_STR)
+        # row type is correct
+        self.assertEqual(f3x_memo_row[0], "TEXT")
+        # committee id is correct for report memo
+        self.assertEqual(f3x_memo_row[1], self.committee.committee_id)
+        # transaction id is correct for report memo
+        self.assertEqual(f3x_memo_row[2], "REPORT_MEMO_TEXT1")
+        # backreference to form type is correct
+        self.assertEqual(f3x_memo_row[4], self.f3x.form_type)
+        # memo content is correct
+        self.assertEqual(f3x_memo_row[5], "F3X_MEMO_TEXT")
+
+    def test_f99_memo(self):
+        f99_memo = create_report_memo(
+            self.committee,
+            self.f99,
+            text4000="F99_MEMO_TEXT",
+        )
+        f99_memo_row = serialize_instance("Text", f99_memo).split(FS_STR)
+        # row type is correct
+        self.assertEqual(f99_memo_row[0], "TEXT")
+        # committee id is correct for report memo
+        self.assertEqual(f99_memo_row[1], self.committee.committee_id)
+        # transaction id is correct for report memo
+        self.assertEqual(f99_memo_row[2], "REPORT_MEMO_TEXT1")
+        # backreference to form type is correct
+        self.assertEqual(f99_memo_row[4], self.f99.form_type)
+        # memo content is correct
+        self.assertEqual(f99_memo_row[5], "F99_MEMO_TEXT")
+
+    def test_transaction_memo(self):
+        """create a transaction and memo
+        set the form type of the transaction to SA11AII (unitemized)
+        but mark the transaction as itemized.  The queryset used should spit out
+        a form type of SA11AI (itemized) instead of SA11AII
+        This is important to test because if the correct queryset isn't used
+        the form type in the memo will say SA11AII instead of SA11AI
+        """
+        transaction = create_schedule_a(
             "INDIVIDUAL_RECEIPT",
             self.committee,
             self.contact_1,
             datetime.strptime("2024-01-03", "%Y-%m-%d"),
             "1.00",
             "GENERAL",
-            "SA11AI",
-            itemized=itemized,
+            "SA11AII",
+            itemized=True,
         )
-
-        self.f3x_memo = create_report_memo(
-            self.committee,
-            self.f3x,
-            text4000="F3X_MEMO_TEXT",
+        transaction_memo = create_transaction_memo(
+            self.committee, transaction, "TRANSACTION_MEMO_TEXT"
         )
-        self.f99_memo = create_report_memo(
-            self.committee,
-            self.f99,
-            text4000="F99_MEMO_TEXT",
-        )
-        self.transaction_memo = create_transaction_memo(
-            self.committee, self.transaction, "TRANSACTION_MEMO_TEXT"
-        )
-
-        self.f3x_row = serialize_instance("Text", self.f3x_memo).split(FS_STR)
-        self.f99_row = serialize_instance("Text", self.f99_memo).split(FS_STR)
-        self.transaction_row = serialize_instance("Text", self.transaction_memo).split(
-            FS_STR
-        )
-
-    def test_unitemized_model(self):
-        self.create_rows(False)
-        # Test Record names
-        self.assertEqual(self.f3x_row[0], "TEXT")
-        self.assertEqual(self.f99_row[0], "TEXT")
-        self.assertEqual(self.transaction_row[0], "TEXT")
+        memo_row = serialize_instance("Text", transaction_memo).split(FS_STR)
+        # row type is correct
+        self.assertEqual(memo_row[0], "TEXT")
         # Test committee ID
-        self.assertEqual(self.f3x_row[2], "REPORT_MEMO_TEXT1")
-        self.assertEqual(self.f3x_row[4], self.f3x.form_type)
-        self.assertEqual(self.f99_row[2], "REPORT_MEMO_TEXT1")
-        self.assertEqual(self.f99_row[4], self.f99.form_type)
-        # Test row contains report info
-        self.assertEqual(self.f3x_row[2], "REPORT_MEMO_TEXT1")
-        self.assertEqual(self.f3x_row[4], self.f3x.form_type)
-        self.assertEqual(self.f99_row[2], "REPORT_MEMO_TEXT1")
-        self.assertEqual(self.f99_row[4], self.f99.form_type)
-        # Test row contains transaction info
-        self.assertEqual(self.transaction_row[3], self.transaction.transaction_id)
-        self.assertEqual(self.transaction_row[4], "SA11AII")
-        # Test row contains text 400
-        self.assertEqual(self.f3x_row[5], "F3X_MEMO_TEXT")
-        self.assertEqual(self.f99_row[5], "F99_MEMO_TEXT")
-        self.assertEqual(self.transaction_row[5], "TRANSACTION_MEMO_TEXT")
-
-    def test_itemized_model(self):
-        self.create_rows(True)
-        self.assertEqual(self.transaction_row[4], "SA11AI")
+        self.assertEqual(memo_row[1], self.committee.committee_id)
+        # transaction id is correct for transaction memo
+        self.assertEqual(memo_row[2], transaction_memo.transaction_id_number)
+        # backreference to transaction is correct
+        self.assertEqual(memo_row[3], transaction.transaction_id)
+        # backreference to form type is correct
+        self.assertEqual(memo_row[4], "SA11AI")
+        # memo content is correct
+        self.assertEqual(memo_row[5], "TRANSACTION_MEMO_TEXT")
