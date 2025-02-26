4629a9cf33af9da608e712f59902c470eb83f92c
Elaine Krauss <elaine.krauss@tcg.com>
2025-02-26 10:34:10 -0500

    Updates locust app testing to fail on requests that don't return 200

--- performance-testing/locust_run_app.py
+++ performance-testing/locust_run_app.py
@@ -1,22 +1,25 @@
-import os
-import resource
-import logging
-import random
-import json
-import math
-
 from locust import task, TaskSet, user, between
+
+
+POLLING_INTERVAL = 5  # Seconds between requests made to /index.html
+POLLING_DEVIATION = 0.1  # Variation, measured in seconds in requests
+
+# A slight variation is applied between requests because real-world users
+# won't all be aligned to exact 1,000ms intervals.
 
 
 class Tasks(TaskSet):
 
     @task
     def get_index_html(self):
-        self.client.get(
-            "/index.html", allow_redirects=False
-        )
+        with self.client.get("/index.html", catch_response=True) as response:
+            if response.status_code != 200:
+                response.failure("Non-200 return")
 
 
 class Swarm(user.HttpUser):
     tasks = [Tasks]
-    wait_time = between(4.5, 5.5)
+    wait_time = between(
+        POLLING_INTERVAL-POLLING_DEVIATION,
+        POLLING_INTERVAL+POLLING_DEVIATION
+    )
