cd0042123aca2cc68484d54cfe0117b4ebee05c7
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-03-10 18:32:21 -0400

    FECFILE-2070: Updated test cases.

--- django-backend/fecfiler/user/test_disable_user_command.py
+++ django-backend/fecfiler/user/test_disable_user_command.py
@@ -9,7 +9,7 @@ class DisableUserCommandTest(TestCase):
     def setUp(self):
         self.test_user = User.objects.create(email="test@fec.gov", username="gov")
 
-    def test_disable_user(self):
+    def test_disable_user_email(self):
         user_model = get_user_model()
 
         # get the test user
@@ -18,10 +18,27 @@ class DisableUserCommandTest(TestCase):
 
         # disable the test user
         try:
-            call_command("disable_user", user.email)
+            call_command("disable_user", email=user.email)
         except Exception as e:
             print(f"Error running command: {e}")
 
         # re-get the test user
         user = user_model.objects.get(email=self.test_user.email)
+        self.assertFalse(user.is_active)
+
+    def test_disable_user_uuid(self):
+        user_model = get_user_model()
+
+        # get the test user
+        user = user_model.objects.get(username=self.test_user.username)
+        self.assertTrue(user.is_active)
+
+        # disable the test user
+        try:
+            call_command("disable_user", uuid=user.username)
+        except Exception as e:
+            print(f"Error running command: {e}")
+
+        # re-get the test user
+        user = user_model.objects.get(username=self.test_user.username)
         self.assertFalse(user.is_active)
