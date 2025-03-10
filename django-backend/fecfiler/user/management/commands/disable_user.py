fbd99e4e63ce44dd4aba3e0f6822f3477c0a491d
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-03-10 18:14:41 -0400

    FECFILE-2070: Added option to disable by UUID.

--- django-backend/fecfiler/user/management/commands/disable_user.py
+++ django-backend/fecfiler/user/management/commands/disable_user.py
@@ -6,7 +6,11 @@ class Command(BaseCommand):
     help = 'Disable (or re-enable) a user with a given email.'
 
     def add_arguments(self, parser):
-        parser.add_argument('email', type=str,
+        # require either a UUID or email
+        id_arg = parser.add_mutually_exclusive_group(required=True)
+        id_arg.add_argument('--uuid', type=str,
+                help='The UUID of the user to be disabled/enabled.')
+        id_arg.add_argument('--email', type=str,
                 help='The email address of the user to be disabled/enabled.')
         # add an --enable flag that defaults to false if not found
         parser.add_argument('-e', '--enable', action='store_true',
@@ -16,7 +20,11 @@ class Command(BaseCommand):
         user_model = get_user_model()
 
         try:
-            user = user_model.objects.get(email=options['email'])
+            # if they use both arguments, prefer UUID
+            if options['uuid'] is not None:
+                user = user_model.objects.get(username=options['uuid'])
+            else:
+                user = user_model.objects.get(email=options['email'])
         except user_model.DoesNotExist:
             raise CommandError('User does not exist')
 
@@ -24,5 +32,5 @@ class Command(BaseCommand):
         user.save()
 
         self.stdout.write(self.style.SUCCESS(
-            f'The is_active flag for user [{user.email}] set to: {user.is_active}'
+            f'The is_active flag for user [{user.username} | {user.email}] set to: {user.is_active}'
         ))
