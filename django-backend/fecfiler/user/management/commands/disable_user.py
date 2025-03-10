83270db81974f91ef3dce5e072f55c20a99c3a37
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-03-10 18:23:07 -0400

    FECFILE-2070: Linting redux.

--- django-backend/fecfiler/user/management/commands/disable_user.py
+++ django-backend/fecfiler/user/management/commands/disable_user.py
@@ -32,6 +32,6 @@ class Command(BaseCommand):
         user.save()
 
         self.stdout.write(self.style.SUCCESS(
-            f'The is_active flag for user [{user.username} | {user.email}] ' +
+            f'The is_active flag for user [{user.username} | {user.email}] '
             f'set to: {user.is_active}'
         ))
