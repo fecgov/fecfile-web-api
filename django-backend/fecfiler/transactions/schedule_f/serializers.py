fef6748fa343f3dfb855c6b9013f2c3fbd0c7904
David Heitzer <david.heitzer@tcg.com>
2025-03-20 17:49:48 -0400

    674 local testing

--- django-backend/fecfiler/transactions/schedule_f/serializers.py
+++ django-backend/fecfiler/transactions/schedule_f/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from fecfiler.transactions.schedule_f.models import ScheduleF
 from fecfiler.shared.utilities import get_model_data
 
@@ -16,8 +16,6 @@ CONTACT_1_FIELDS = [
     "payee_city",
     "payee_state",
     "payee_zip",
-    "payee_employer",
-    "payee_occupation",
 ]
 
 # candidate who the CE is on behalf of
@@ -28,13 +26,9 @@ CONTACT_2_FIELDS = [
     "payee_candidate_middle_name",
     "payee_candidate_prefix",
     "payee_candidate_suffix",
-    "payee_candidate_street_1",
-    "payee_candidate_street_2",
-    "payee_candidate_city",
+    "payee_candidate_office",
     "payee_candidate_state",
-    "payee_candidate_zip",
-    "payee_candidate_employer",
-    "payee_candidate_occupation",
+    "payee_candidate_district",
 ]
 
 # candidate committee that the CE is on behalf of
@@ -61,6 +55,41 @@ CONTACT_5_FIELDS = [
 
 
 class ScheduleFSerializer(ModelSerializer):
+    # Contact 1
+    payee_organization_name = CharField(required=False, allow_null=True)
+    payee_last_name = CharField(required=False, allow_null=True)
+    payee_first_name = CharField(required=False, allow_null=True)
+    payee_middle_name = CharField(required=False, allow_null=True)
+    payee_prefix = CharField(required=False, allow_null=True)
+    payee_suffix = CharField(required=False, allow_null=True)
+    payee_street_1 = CharField(required=False, allow_null=True)
+    payee_street_2 = CharField(required=False, allow_null=True)
+    payee_city = CharField(required=False, allow_null=True)
+    payee_state = CharField(required=False, allow_null=True)
+    payee_zip = CharField(required=False, allow_null=True)
+    # Contact 2
+    payee_candidate_id_number = CharField(required=False, allow_null=True)
+    payee_candidate_last_name = CharField(required=False, allow_null=True)
+    payee_candidate_first_name = CharField(required=False, allow_null=True)
+    payee_candidate_middle_name = CharField(required=False, allow_null=True)
+    payee_candidate_prefix = CharField(required=False, allow_null=True)
+    payee_candidate_suffix = CharField(required=False, allow_null=True)
+    payee_candidate_office = CharField(required=False, allow_null=True)
+    payee_candidate_state = CharField(required=False, allow_null=True)
+    payee_candidate_district = CharField(required=False, allow_null=True)
+    # Contact 3
+    payee_committee_id_number = CharField(required=False, allow_null=True)
+    # Contact 4
+    designating_committee_id_number = CharField(required=False, allow_null=True)
+    designating_committee_name = CharField(required=False, allow_null=True)
+    # Contact 5
+    subordinate_committee_id_number = CharField(required=False, allow_null=True)
+    subordinate_committee_name = CharField(required=False, allow_null=True)
+    subordinate_street_1 = CharField(required=False, allow_null=True)
+    subordinate_street_2 = CharField(required=False, allow_null=True)
+    subordinate_city = CharField(required=False, allow_null=True)
+    subordinate_state = CharField(required=False, allow_null=True)
+    subordinate_zip = CharField(required=False, allow_null=True)
 
     def create(self, validated_data):
         model_data = get_model_data(validated_data, ScheduleF)
