1066f212b4b3d7994408c7bfe6d30eea9325bf29
toddlees <todd.lees@tcg.com>
2025-04-08 09:25:55 -0400

    update save trigger migration

--- django-backend/fecfiler/transactions/migrations/0020_trigger_save_on_transactions.py
+++ django-backend/fecfiler/transactions/migrations/0020_trigger_save_on_transactions.py
@@ -1,4 +1,4 @@
-from django.db import migrations
+from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     def trigger_save_on_transactions(apps, schema_editor):
         transactions = apps.get_model("transactions", "transaction")
-        transactions.objects.update()
+        transactions.objects.update(id=models.F('id'))
 
     operations = [
         migrations.RunPython(trigger_save_on_transactions, migrations.RunPython.noop),
