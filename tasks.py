69274761187b0543b18a5d9a11a0c151fbfcb6bc
Laura Beaufort <lbeaufort@fec.gov>
2025-05-05 09:51:01 -0400

    Deploy to dev

--- tasks.py
+++ tasks.py
@@ -49,7 +49,7 @@ DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("test", lambda _, branch: branch == "release/test"),
     ("stage", lambda _, branch: branch.startswith("release/sprint")),
-    ("dev", lambda _, branch: branch == "feature/2078"),
+    ("dev", lambda _, branch: branch == "feature/2078-deploy"),
 )
 
 
