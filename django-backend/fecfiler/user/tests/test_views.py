06a6eb0ec9fba6b6196e628ac07affcb1cb29bba
David Heitzer <david.heitzer@tcg.com>
2025-06-04 22:24:05 -0400

    2104 user viewset

--- django-backend/fecfiler/user/tests/test_views.py
+++ django-backend/fecfiler/user/tests/test_views.py
@@ -1,22 +1,20 @@
-from django.test import RequestFactory, TestCase
-from rest_framework.test import APIClient
 from datetime import date, timedelta
+from fecfiler.test.viewset_test import FecfilerViewSetTest
+from fecfiler.user.views import UserViewSet
 
-from fecfiler.user.models import User
 
-
-class UserViewSetTest(TestCase):
+class UserViewSetTest(FecfilerViewSetTest):
     fixtures = ["C01234567_user_and_committee"]
 
     def setUp(self):
-        self.user = User.objects.get(id="12345678-aaaa-bbbb-cccc-111122223333")
-        self.factory = RequestFactory()
+        super().setUp()
 
     def test_get_current_user_unsigned_security_consent(self):
-        client = APIClient()
-        client.force_authenticate(user=self.user)
-
-        response = client.get("/api/v1/users/get_current/", secure=True)
+        response = self.send_viewset_get_request(
+            "/api/v1/users/get_current/",
+            UserViewSet,
+            "get_current",
+        )
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["first_name"], "First")
@@ -25,28 +23,26 @@ class UserViewSetTest(TestCase):
         self.assertEqual(response.data["security_consented"], False)
 
     def test_get_current_user_signed_ephemeral_security_consent(self):
-        client = APIClient()
-        client.force_authenticate(user=self.user)
         test_put_data = {
             "first_name": "test_first_name_updated",
             "last_name": "test_last_name_updated",
             "email": "test@fec.gov",
             "consent_for_one_year": False,
         }
-
-        client.put("/api/v1/users/update_current/", test_put_data, secure=True)
-        response = client.get("/api/v1/users/get_current/", secure=True)
-
+        response = self.send_viewset_put_request(
+            "/api/v1/users/update_current/",
+            test_put_data,
+            UserViewSet,
+            "update_current",
+        )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["first_name"], "test_first_name_updated")
         self.assertEqual(response.data["last_name"], "test_last_name_updated")
         self.assertEqual(response.data["email"], "test@fec.gov")
         self.assertEqual(response.data["security_consented"], True)
-        self.assertNotEqual(self.user.security_consent_exp_date, date.today())
+        self.assertNotEqual(self.default_user.security_consent_exp_date, date.today())
 
     def test_get_current_user_signed_annual_security_consent(self):
-        client = APIClient()
-        client.force_authenticate(user=self.user)
         test_put_data = {
             "first_name": "test_first_name_updated2",
             "last_name": "test_last_name_updated2",
@@ -54,14 +50,18 @@ class UserViewSetTest(TestCase):
             "consent_for_one_year": True,
         }
 
-        client.put("/api/v1/users/update_current/", test_put_data, secure=True)
-        response = client.get("/api/v1/users/get_current/", secure=True)
-
+        response = self.send_viewset_put_request(
+            "/api/v1/users/update_current/",
+            test_put_data,
+            UserViewSet,
+            "update_current",
+        )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data["first_name"], "test_first_name_updated2")
         self.assertEqual(response.data["last_name"], "test_last_name_updated2")
         self.assertEqual(response.data["email"], "test@fec.gov")
         self.assertEqual(response.data["security_consented"], True)
         self.assertEqual(
-            self.user.security_consent_exp_date, date.today() + timedelta(days=365)
+            self.default_user.security_consent_exp_date,
+            date.today() + timedelta(days=365),
         )
