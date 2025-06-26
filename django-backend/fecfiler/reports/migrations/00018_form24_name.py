3721a979ad3ef71e9fd520bb06e2a731e4f8875d
Sasha Dresden <sasha.dresden@proton.me>
2025-06-26 16:05:31 -0400

    Form 24 name field can't be 'unique' is sql sense because it is unique to committee. Added validation check to restrict it to committee.

--- django-backend/fecfiler/reports/migrations/00018_form24_name.py
+++ django-backend/fecfiler/reports/migrations/00018_form24_name.py
@@ -11,14 +11,6 @@ def update_form24_names(apps, schema_editor):
         form.save()
 
 
-def reverse_update_form24_names(apps, schema_editor):
-    form24 = apps.get_model("reports", "Form24")
-    form24_objects = form24.objects.all()
-    for form in form24_objects:
-        form.name = None  # Or set it to some placeholder value if you prefer
-        form.save()
-
-
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -30,7 +22,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name="form24",
             name="name",
-            field=models.TextField(null=False, blank=False, unique=True),
+            field=models.TextField(null=True, blank=False),
         ),
-        migrations.RunPython(update_form24_names, reverse_update_form24_names),
+        migrations.RunPython(update_form24_names),
+        migrations.AlterField(
+            model_name="form24",
+            name="name",
+            field=models.TextField(null=False, blank=False),
+        ),
     ]
