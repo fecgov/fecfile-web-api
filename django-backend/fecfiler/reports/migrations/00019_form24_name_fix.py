574fc0b3bfe2474f2c0c9b4afc4fe282fba7643f
toddlees <todd.lees@tcg.com>
2025-07-01 23:19:52 -0400

    simplify

--- django-backend/fecfiler/reports/migrations/00019_form24_name_fix.py
+++ django-backend/fecfiler/reports/migrations/00019_form24_name_fix.py
@@ -3,17 +3,11 @@ from django.db import migrations
 
 def update_form24_names(apps, schema_editor):
     form24 = apps.get_model("reports", "Form24")
-    committeeAccount = apps.get_model("committee_accounts", "CommitteeAccount")
-    for committee_account in list(committeeAccount.objects.all()):
-        form24_objects = list(
-            form24.objects.filter(report__committee_account=committee_account).order_by(
-                "report__created"
-            )
-        )
-        for form in form24_objects:
-            report_type = form.report_type_24_48
-            form.name = f"{report_type}-HOUR: Report of Independent Expenditure"
-            form.save()
+    form24_objects = list(form24.objects.all())
+    for form in form24_objects:
+        report_type = form.report_type_24_48
+        form.name = f"{report_type}-HOUR: Report of Independent Expenditure"
+        form.save()
 
 
 class Migration(migrations.Migration):
