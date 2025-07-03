58bda30aa030d0abc0f21e4d71e65f74c36e2dfa
Laura Beaufort <lbeaufort@fec.gov>
2025-07-03 12:26:35 -0400

    Manual deploy

--- tasks.py
+++ tasks.py
@@ -49,7 +49,7 @@ DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("test", lambda _, branch: branch == "release/test"),
     ("stage", lambda _, branch: branch.startswith("release/sprint")),
-    ("dev", lambda _, branch: branch == "feature/2154-intentional_migration_fail-lb"),
+    ("dev", lambda _, branch: branch == "feature/2336-fail-build-for-running-migrations-test"),
 )
 
 
