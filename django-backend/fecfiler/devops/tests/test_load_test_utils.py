960d61eac84355a37588c42834f8d279b9b7c76d
David Heitzer <david.heitzer@tcg.com>
2025-07-21 22:12:53 -0400

    2287 lints

--- django-backend/fecfiler/devops/tests/test_load_test_utils.py
+++ django-backend/fecfiler/devops/tests/test_load_test_utils.py
@@ -25,14 +25,14 @@ class LoadTestUtilsTestCase(TestCase):
         mock_create_reports,
         mock_create_and_activate_committee,
         mock_get_current_user_id,
-        mock_LocustDataGenerator,
+        mock_locust_data_generator,
     ):
         mock_get_current_user_id.return_value = "test_user_id"
         mock_committee = MagicMock()
         mock_create_and_activate_committee.return_value = mock_committee
         mock_create_reports.return_value = [{id: "test_report_id"}]
         mock_create_contacts.return_value = [{id: "test_contact_id"}]
-        mock_LocustDataGenerator.return_value = MagicMock()
+        mock_locust_data_generator.return_value = MagicMock()
 
         test_new_committee_id = "C33333333"
         test_number_of_reports = 10
@@ -56,7 +56,7 @@ class LoadTestUtilsTestCase(TestCase):
 
     @patch("fecfiler.devops.utils.load_test_utils.requests.get")
     @patch("fecfiler.devops.utils.load_test_utils.User")
-    def test_get_current_user_id(self, mock_User, mock_requests_get):
+    def test_get_current_user_id(self, mock_user, mock_requests_get):
         test_user_id = "test_user_id"
         test_user_email = "test@test.com"
         mock_response = MagicMock()
@@ -65,35 +65,35 @@ class LoadTestUtilsTestCase(TestCase):
         mock_requests_get.return_value = mock_response
         mock_user_obj = MagicMock()
         mock_user_obj.id = test_user_id
-        mock_User.objects.get.return_value = mock_user_obj
+        mock_user.objects.get.return_value = mock_user_obj
 
         user_id = self.utils.get_current_user_id()
         self.assertEqual(user_id, test_user_id)
         mock_requests_get.assert_called_once()
-        mock_User.objects.get.assert_called_once_with(email=test_user_email)
+        mock_user.objects.get.assert_called_once_with(email=test_user_email)
 
     @patch("fecfiler.devops.utils.load_test_utils.CommitteeAccount")
     @patch("fecfiler.devops.utils.load_test_utils.Membership")
     @patch("fecfiler.devops.utils.load_test_utils.requests.post")
     def test_create_and_activate_committee(
-        self, mock_requests_post, mock_Membership, mock_CommitteeAccount
+        self, mock_requests_post, mock_membership, mock_committee_account
     ):
         test_new_committee_id = "C33333333"
         test_committee_id = "test_committee_id"
         test_user_id = "test_user_id"
         mock_committee = MagicMock()
         mock_committee.id = test_committee_id
-        mock_CommitteeAccount.objects.create.return_value = mock_committee
+        mock_committee_account.objects.create.return_value = mock_committee
         mock_requests_post.return_value.raise_for_status.return_value = None
 
         committee = self.utils.create_and_activate_committee(
             test_new_committee_id, test_user_id
         )
         self.assertEqual(committee, mock_committee)
-        mock_CommitteeAccount.objects.create.assert_called_once_with(
+        mock_committee_account.objects.create.assert_called_once_with(
             committee_id=test_new_committee_id
         )
-        mock_Membership.objects.create.assert_called_once()
+        mock_membership.objects.create.assert_called_once()
         mock_requests_post.assert_called_once()
 
     @patch.object(LoadTestUtils, "locust_data_generator", create=True)
@@ -110,18 +110,18 @@ class LoadTestUtilsTestCase(TestCase):
 
     @patch("fecfiler.devops.utils.load_test_utils.Contact")
     @patch.object(LoadTestUtils, "locust_data_generator", create=True)
-    def test_create_contacts(self, mock_locust_data_generator, mock_Contact):
+    def test_create_contacts(self, mock_locust_data_generator, mock_contact):
         test_number_of_contacts = 100
         mock_contact_objs = [MagicMock(), MagicMock()]
         mock_locust_data_generator.generate_contacts.return_value = mock_contact_objs
-        mock_Contact.objects.bulk_create.return_value = mock_contact_objs
+        mock_contact.objects.bulk_create.return_value = mock_contact_objs
         self.utils.locust_data_generator = mock_locust_data_generator
         result = self.utils.create_contacts(test_number_of_contacts)
         self.assertEqual(result, mock_contact_objs)
         mock_locust_data_generator.generate_contacts.assert_called_once_with(
             test_number_of_contacts
         )
-        mock_Contact.objects.bulk_create.assert_called_once_with(mock_contact_objs)
+        mock_contact.objects.bulk_create.assert_called_once_with(mock_contact_objs)
 
     @patch.object(LoadTestUtils, "locust_data_generator", create=True)
     def test_create_single_transactions(self, mock_locust_data_generator):
