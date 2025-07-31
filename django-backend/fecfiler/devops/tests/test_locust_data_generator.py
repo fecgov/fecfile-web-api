a6e85c91fe76857f79c6c71ca1527c466a844d66
David Heitzer <david.heitzer@tcg.com>
2025-07-31 17:24:30 -0400

    2373 locust_data_generator tests

--- django-backend/fecfiler/devops/tests/test_locust_data_generator.py
+++ django-backend/fecfiler/devops/tests/test_locust_data_generator.py
@@ -1,66 +1,147 @@
 from django.test import TestCase
-from unittest.mock import MagicMock, patch
-from fecfiler.devops.utils.locust_data_generator import LocustDataGenerator, save_json
+from unittest.mock import patch, MagicMock
+from fecfiler.devops.utils.locust_data_generator import LocustDataGenerator
+from fecfiler.reports.models import Report
+from fecfiler.contacts.models import Contact
+from fecfiler.transactions.schedule_a.models import ScheduleA
+from fecfiler.committee_accounts.models import CommitteeAccount
+import uuid
 
 
 class LocustDataGeneratorTestCase(TestCase):
     def setUp(self):
-        self.committee = MagicMock()
-        self.committee.id = "test_committee_id"
-        self.generator = LocustDataGenerator(self.committee)
+        test_committee_account = CommitteeAccount()
+        test_committee_account.id = uuid.uuid4()
+        self.locust_data_generator = LocustDataGenerator(test_committee_account)
 
-    @patch("fecfiler.devops.utils.locust_data_generator.create_form3x")
-    def test_generate_form_3x(self, mock_create_form3x):
-        test_f3x_count = 3
-        mock_create_form3x.return_value = MagicMock()
-        result = self.generator.generate_form_3x(test_f3x_count)
-        self.assertEqual(len(result), test_f3x_count)
-        self.assertTrue(all(isinstance(x, MagicMock) for x in result))
+    def add_schedule_a_to_transaction(transaction_list):
+        for transaction in transaction_list:
+            transaction.schedule_a = ScheduleA(
+                **{
+                    "contribution_purpose_descrip": uuid.uuid4(),
+                }
+            )
+        return transaction_list
 
-    def test_generate_contacts(self):
-        test_contacts_count = 3
-        contacts = self.generator.generate_contacts(test_contacts_count)
-        self.assertEqual(len(contacts), test_contacts_count)
-        for contact in contacts:
-            self.assertEqual(contact.committee_account_id, self.committee.id)
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator.Form3X.objects.bulk_create",
+        side_effect=lambda x: x,
+    )
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator.Report.objects.bulk_create",
+        side_effect=lambda x: x,
+    )
+    def test_generate_form_3x(self, mock_report_bulk_create, mock_form3x_bulk_create):
+        result = self.locust_data_generator.generate_form_3x(3)
+        self.assertEqual(len(result), 3)
+        mock_report_bulk_create.assert_called_with(result)
+        for report in result:
+            self.assertIsNotNone(report.id)
+            self.assertEqual(report.form_type, "F3XN")
+            self.assertEqual(
+                report.committee_account_id, self.locust_data_generator.committee.id
+            )
+
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator.Contact.objects.bulk_create",
+        side_effect=lambda x: x,
+    )
+    def test_generate_contacts(self, mock_contact_bulk_create):
+        result = self.locust_data_generator.generate_contacts(3)
+        self.assertEqual(len(result), 3)
+        mock_contact_bulk_create.assert_called_with(result)
+        for contact in result:
+            self.assertIsNotNone(contact.id)
+            self.assertEqual(
+                contact.committee_account_id, self.locust_data_generator.committee.id
+            )
             self.assertEqual(contact.type, "IND")
-            self.assertEqual(contact.city, "Testville")
-            self.assertEqual(contact.state, "AK")
-            self.assertEqual(contact.zip, "12345")
-            self.assertEqual(contact.country, "USA")
-            self.assertEqual(contact.employer, "Business Inc.")
-            self.assertEqual(contact.occupation, "Job")
 
-    @patch("fecfiler.devops.utils.locust_data_generator.create_schedule_a")
-    def test_generate_single_transactions(self, mock_create_schedule_a):
-        test_transaction_count = 4
-        test_reports = [MagicMock(coverage_from_date="2020-01-01")]
-        test_contacts = [MagicMock()]
-        mock_create_schedule_a.return_value = MagicMock()
-        result = self.generator.generate_single_transactions(
-            test_transaction_count, test_reports, test_contacts
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator.ScheduleA.objects.bulk_create",
+        side_effect=lambda x: x,
+    )
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator.Transaction.objects.bulk_create",
+        side_effect=add_schedule_a_to_transaction,
+    )
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator"
+        ".ReportTransaction.objects.bulk_create"
+    )
+    def test_generate_single_schedule_a_transactions(
+        self,
+        mock_report_transaction_bulk_create,
+        mock_transaction_bulk_create,
+        mock_schedule_a_bulk_create,
+    ):
+        test_report = Report(
+            **{
+                "id": uuid.uuid4(),
+                "form_type": "F3XN",
+                "committee_account_id": self.locust_data_generator.committee.id,
+                "coverage_from_date": "2025-01-01",
+                "coverage_through_date": "2025-03-31",
+                "report_code": "Q1",
+            }
         )
-        self.assertEqual(len(result), test_transaction_count)
-        self.assertTrue(all(isinstance(x, MagicMock) for x in result))
+        test_reports = [test_report]
 
-    @patch("fecfiler.devops.utils.locust_data_generator.create_schedule_a")
-    def test_generate_triple_transactions(self, mock_create_schedule_a):
-        # Each triple creates 3 transactions, but only the first is returned
-        mock_a = MagicMock()
-        mock_b = MagicMock()
-        mock_c = MagicMock()
-        mock_a.id = 1
-        mock_b.id = 2
-        test_transaction_count = 2
-        mock_create_schedule_a.side_effect = [
-            mock_a,
-            mock_b,
-            mock_c,
-        ] * test_transaction_count
-        test_reports = [MagicMock(coverage_from_date="2020-01-01")]
-        test_contacts = [MagicMock()]
-        result = self.generator.generate_triple_transactions(
-            test_transaction_count, test_reports, test_contacts
+        test_contact = Contact(
+            **{
+                "id": uuid.uuid4(),
+                "committee_account_id": self.locust_data_generator.committee.id,
+                "type": "IND",
+            }
         )
-        self.assertEqual(len(result), test_transaction_count)
-        self.assertTrue(all(isinstance(x, MagicMock) for x in result))
+        test_contacts = [test_contact]
+
+        result = self.locust_data_generator.generate_single_schedule_a_transactions(
+            3, test_reports, test_contacts
+        )
+        self.assertEqual(len(result), 3)
+        mock_transaction_bulk_create.assert_called_with(result)
+        for transaction in result:
+            self.assertIsNotNone(transaction.id)
+            self.assertEqual(
+                transaction.transaction_type_identifier, "INDIVIDUAL_RECEIPT"
+            )
+            self.assertEqual(
+                transaction.committee_account_id, self.locust_data_generator.committee.id
+            )
+            self.assertEqual(transaction.contact_1_id, test_contact.id)
+            self.assertEqual(transaction.aggregation_group, "GENERAL")
+            self.assertEqual(transaction.form_type, "SA11AI")
+
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator.Transaction.objects.bulk_update",
+    )
+    @patch(
+        "fecfiler.devops.utils.locust_data_generator"
+        ".LocustDataGenerator.generate_single_schedule_a_transactions"
+    )
+    def test_generate_triple_schedule_a_transactions(
+        self,
+        mock_generate_single_schedule_a_transactions,
+        mock_transaction_bulk_update,
+    ):
+        mock_tier1_transaction = MagicMock(id=1, parent_transaction_id=None)
+        mock_tier2_transaction = MagicMock(id=2, parent_transaction_id=None)
+        mock_tier3_transaction = MagicMock(id=3, parent_transaction_id=None)
+
+        mock_generate_single_schedule_a_transactions.side_effect = [
+            [mock_tier1_transaction],
+            [mock_tier2_transaction],
+            [mock_tier3_transaction],
+        ]
+
+        result = self.locust_data_generator.generate_triple_schedule_a_transactions(
+            1, [], []
+        )
+        self.assertEqual(len(result), 1)
+        self.assertEqual(mock_tier3_transaction.id, 3)
+        self.assertEqual(mock_tier3_transaction.parent_transaction_id, 2)
+        self.assertEqual(mock_tier2_transaction.id, 2)
+        self.assertEqual(mock_tier2_transaction.parent_transaction_id, 1)
+        self.assertEqual(mock_tier1_transaction.id, 1)
+        self.assertEqual(mock_tier1_transaction.parent_transaction_id, None)
