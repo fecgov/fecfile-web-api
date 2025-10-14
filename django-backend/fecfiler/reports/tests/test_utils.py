762665295b0e40ce678e2ad0d45284c708d729b8
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-10-14 16:21:08 -0400

    FECFILE-2444: Renamed tests to match command, added comments, added tests of removing dotfec.

--- django-backend/fecfiler/reports/tests/test_utils.py
+++ django-backend/fecfiler/reports/tests/test_utils.py
@@ -22,22 +22,29 @@ class ReportUtilTestCase(TestCase):
         self.committee = CommitteeAccount.objects.create(committee_id="C00000000")
         self.f3x = create_form3x(self.committee, "2024-01-01", "2024-02-01", {})
 
-    def test_reset_calculation_status(self, use="method"):
+    def test_reset_submitting_report(self, use="method"):
         # create mock calculation bits
         self.f3x.calculation_token = uuid4()  # mock token
         self.f3x.calculation_status = CalculationState.CALCULATING
 
-        # create mock dot fec record and associated upload/webprint submissions
-        test_dot_fec_record = DotFEC.objects.create()
+        # create mock dot fec record
+        test_dot_fec_record = DotFEC.objects.create(
+            report=self.f3x, file_name="testfile.txt"
+        )
+
+        # create mock upload submission
         test_upload_submission = UploadSubmission.objects.create(
             dot_fec=test_dot_fec_record
         )
         self.f3x.upload_submission = test_upload_submission
+
+        # create mock webprint submission
         test_webprint_submission = WebPrintSubmission.objects.create(
             dot_fec=test_dot_fec_record,
         )
         self.f3x.webprint_submission = test_webprint_submission
 
+        # save the report with all the mock data
         self.f3x.save()
 
         # reset the submitting report, either with the method or command
@@ -50,17 +57,21 @@ class ReportUtilTestCase(TestCase):
         else:
             reset_submitting_report(str(self.f3x.id))
 
-        # refresh from db and validate reset
+        # refresh from db and validate reset report submission
         self.f3x.refresh_from_db()
         self.assertEqual(self.f3x.calculation_token, None)
         self.assertEqual(self.f3x.calculation_status, None)
         self.assertEqual(self.f3x.upload_submission_id, None)
         self.assertEqual(self.f3x.webprint_submission_id, None)
 
-    def test_reset_calculation_status_command(self):
-        self.test_reset_calculation_status(use="command")
+        # validate dot fec record deleted
+        with self.assertRaises(DotFEC.DoesNotExist):
+            DotFEC.objects.get(report_id=self.f3x.id)
 
-    def test_reset_calculation_status_command_missing_arg(self):
+    def test_reset_submitting_report_command(self):
+        self.test_reset_submitting_report(use="command")
+
+    def test_reset_submitting_report_command_missing_arg(self):
         with self.assertRaises(CommandError) as cm:
             call_command("reset_submitting_report")
 
