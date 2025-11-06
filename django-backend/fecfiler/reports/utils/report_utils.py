eb4b8b85777b8063c62fa25aec3ea0c0b9dd96cd
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-11-06 13:15:31 -0500

    FECFILE-2506: Finish removing mysterious plurality.

--- django-backend/fecfiler/reports/utils/report_utils.py
+++ django-backend/fecfiler/reports/utils/report_utils.py
@@ -60,7 +60,6 @@ def delete_committee_reports(committee_id, delete_contacts=False):
     if committee_id is None or len(committee_id) == 0:
         logger.error("No committee ID provided")
         return
-    committee_id = committee_ids[0]
 
     cid_regex = re.compile("^C[0-9]{8}$")
     if not cid_regex.match(str(committee_id)):
