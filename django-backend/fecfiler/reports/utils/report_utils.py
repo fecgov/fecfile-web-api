450a05a1f4f521792c971cfe7b62bd446c5715b6
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-11-06 13:04:53 -0500

    FECFILE-2506: Initial refactor of delete_committee_reports

--- django-backend/fecfiler/reports/utils/report_utils.py
+++ django-backend/fecfiler/reports/utils/report_utils.py
@@ -1,12 +1,17 @@
-from ..models import Report
 from uuid import UUID
+from ..models import Report
 from fecfiler.web_services.models import (
     DotFEC,
     UploadSubmission,
     WebPrintSubmission,
 )
+from fecfiler.contacts.models import Contact
+from fecfiler.committee_accounts.models import CommitteeAccount
+from fecfiler.transactions.models import Transaction
+from fecfiler.memo_text.models import MemoText
 from fecfiler.s3 import S3_SESSION
 from fecfiler.settings import AWS_STORAGE_BUCKET_NAME
+import re
 import structlog
 
 logger = structlog.get_logger(__name__)
@@ -49,3 +54,64 @@ def reset_submitting_report(id):
         upload_submission_id=None,
         webprint_submission_id=None,
     )
+
+
+def delete_committee_reports(committee_id, delete_contacts=False):
+    if committee_id is None or len(committee_id) == 0:
+        logger.error("No committee ID provided")
+        return
+    committee_id = committee_ids[0]
+
+    cid_regex = re.compile("^C[0-9]{8}$")
+    if not cid_regex.match(str(committee_id)):
+        logger.error(f'Invalid committee ID "{committee_id}"')
+        return
+
+    committee = CommitteeAccount.objects.filter(committee_id=committee_id).first()
+    if committee is None:
+        logger.error("No matching committee")
+        return
+
+    delete_all_reports(committee_id, log_method=logger.warn)
+
+    # If delete-contacts flag is set, delete contacts as well
+    if delete_contacts:
+        logger.info("Deleting contacts associated with the committee...")
+        delete_committee_contacts(committee_id)
+
+
+def delete_all_reports(committee_id="C99999999", log_method=logger.warn):
+    reports = Report.objects.filter(committee_account__committee_id=committee_id)
+    transactions = Transaction.objects.filter(
+        committee_account__committee_id=committee_id
+    )
+
+    report_count = reports.count()
+    transaction_count = transactions.count()
+    memo_count = MemoText.objects.filter(
+        report__committee_account__committee_id=committee_id
+    ).count()
+    dot_fec_count = DotFEC.objects.filter(
+        report__committee_account__committee_id=committee_id
+    ).count()
+    upload_submission_count = UploadSubmission.objects.filter(
+        dot_fec__report__committee_account__committee_id=committee_id
+    ).count()
+    web_print_submission_count = WebPrintSubmission.objects.filter(
+        dot_fec__report__committee_account__committee_id=committee_id
+    ).count()
+
+    log_method(f"Deleting Reports and Transactions for {committee_id}")
+    log_method(f"Deleting Reports: {report_count}")
+    log_method(f"Deleting Transactions: {transaction_count}")
+    log_method(f"Memos: {memo_count}")
+    log_method(f"Dot Fecs: {dot_fec_count}")
+    log_method(f"Upload Submissions: {upload_submission_count}")
+    log_method(f"WebPrint Submissions: {web_print_submission_count}")
+
+    reports.delete()
+    transactions.hard_delete()
+
+
+def delete_committee_contacts(committee_id):
+    Contact.objects.filter(committee_account__committee_id=committee_id).delete()
