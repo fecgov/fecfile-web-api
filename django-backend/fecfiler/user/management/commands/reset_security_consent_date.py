6e77d2861b34fd3566658e0aa9f1a8ff92e20060
Dan Fowlkes <dfowlkes@precision-solutions.com>
2025-11-06 18:19:05 -0500

    FECFILE-2506: Fixed command syntax.

--- django-backend/fecfiler/user/management/commands/reset_security_consent_date.py
+++ django-backend/fecfiler/user/management/commands/reset_security_consent_date.py
@@ -5,7 +5,7 @@ from fecfiler.user.utils import reset_security_consent_date
 class Command(FECCommand):
     help = """Resets the security consent expiration date
     for the user corresponding to the given email"""
-    command = "reset_security_consent_date"
+    command_name = "reset_security_consent_date"
 
     def add_arguments(self, parser):
         parser.add_argument(
