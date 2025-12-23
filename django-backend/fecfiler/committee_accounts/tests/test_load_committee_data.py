da3eb3cc9cb6a4e3e213c69c65d770c19eea9330
sVmsepi0l <joshua.coombs@tcg.com>
2025-12-23 11:56:48 -0500

    linting fix

--- django-backend/fecfiler/committee_accounts/tests/test_load_committee_data.py
+++ django-backend/fecfiler/committee_accounts/tests/test_load_committee_data.py
@@ -31,7 +31,7 @@ class DumpTestDataCommandTest(TestCase):
         delete_all_reports(committee_id=TEST_COMMITTEE_ID)
         self.committee = CommitteeAccount.objects.create(committee_id=TEST_COMMITTEE_ID)
         delete_all_contacts(committee_uuid=self.committee.id)
-        
+
         self.user = User.objects.create(
             email="tester@fake.gov", first_name="Tester", last_name="Testerson"
         )
