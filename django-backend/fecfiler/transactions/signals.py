1bb9b9831b37ac5d5dc90e4b441c4413dee6baaa
Dan Fowlkes <dfowlkes@precision-solutions.com>
2026-01-07 19:30:41 -0500

    FECFILE-2726: Revert transactions/signals.py to the develop branch version.

--- django-backend/fecfiler/transactions/signals.py
+++ django-backend/fecfiler/transactions/signals.py
@@ -5,12 +5,8 @@ to handle bulk delete cases
 https://docs.djangoproject.com/en/dev/topics/db/models/#overriding-predefined-model-methods
 
 We use signals to log saves to be consistent with delete logging
-
-Note: Aggregate recalculation is now handled explicitly in the
-aggregate_service module, not via signals.
 """
 
-from django.db import transaction
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from .models import Transaction
@@ -19,24 +15,24 @@ import structlog
 logger = structlog.get_logger(__name__)
 
 
-def _log_transaction_action(instance, action: str) -> None:
-    """Log the transaction action."""
-    schedule = instance.get_schedule_name()
-    txn_id = instance.id
-    txn_tid = instance.transaction_id
-    transaction.on_commit(
-        lambda s=schedule, i=txn_id, t=txn_tid, a=action: logger.info(
-            f"{s} Transaction: {i} ({t}) was {a}"
-        )
-    )
-
-
 @receiver(post_save, sender=Transaction)
 def log_post_save(sender, instance, created, **kwargs):
-    action = "created" if created else "updated"
-    _log_transaction_action(instance, action)
+    action = "updated"
+    if created:
+        action = "created"
+    elif instance.deleted:
+        action = "deleted"
+    schedule = instance.get_schedule_name()
+    logger.info(
+        f"{schedule} Transaction: {instance.id} "
+        f"({instance.transaction_id}) was {action}"
+    )
 
 
 @receiver(post_delete, sender=Transaction)
 def log_post_delete(sender, instance, **kwargs):
-    _log_transaction_action(instance, "deleted")
+    schedule = instance.get_schedule_name()
+    logger.info(
+        f"{schedule} Transaction: {instance.id} "
+        f"({instance.transaction_id}) was deleted"
+    )
