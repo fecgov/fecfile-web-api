8bdea91f867c8ef31233dfb91b726734d045b46e
David Heitzer <david.heitzer@tcg.com>
2026-04-08 11:36:06 -0400

    2427 50 thread

--- bin/docker-celery-worker.sh
+++ bin/docker-celery-worker.sh
@@ -3,4 +3,4 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-exec celery -A fecfiler worker --loglevel=info --pool=threads --concurrency=100
+exec celery -A fecfiler worker --loglevel=info --pool=threads --concurrency=50
