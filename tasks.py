cc03ac70daec596aa461c3b470989ac495440fdc
David Heitzer <david.heitzer@tcg.com>
2026-04-21 11:02:47 -0400

    2899 dev test

--- tasks.py
+++ tasks.py
@@ -51,7 +51,10 @@ DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("test", lambda _, branch: branch == "release/test"),
     ("stage", lambda _, branch: branch.startswith("release/sprint")),
-    ("dev", lambda _, branch: branch == "develop"),
+    (
+        "dev",
+        lambda _, branch: branch == "feature/2899-celery-concurrency-env-var-test-in-dev",
+    ),
 )
 
 
