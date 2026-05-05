7d0db95e62ea735919197ab76e59c8928e514674
Elaine Krauss <elaine.krauss@tcg.com>
2026-05-05 10:35:12 -0400

    Only sets CORS headers in the API for local development

--- django-backend/fecfiler/settings/local.py
+++ django-backend/fecfiler/settings/local.py
@@ -1,9 +1,22 @@
 from .base import *  # NOSONAR # noqa: F401, F403
+from corsheaders.defaults import default_headers
 
 # These settings are for local development only.
 
-CORS_ALLOWED_ORIGIN_REGEXES.append("http://localhost:4200")  # NOSONAR # noqa: F405
-CSRF_TRUSTED_ORIGINS.append("http://localhost:4200")  # NOSONAR # noqa: F405
+INSTALLED_APPS.append("corsheaders")
+MIDDLEWARE.append("corsheaders.middleware.CorsMiddleware")
+
+CORS_ALLOWED_ORIGIN_REGEXES = ["http://localhost:4200"]  # NOSONAR # noqa: F405
+CSRF_TRUSTED_ORIGINS = ["http://localhost:4200"]  # NOSONAR # noqa: F405
+
+CORS_ALLOW_HEADERS = (
+    *default_headers,
+    "enctype",
+    "token",
+    "cache-control",
+)
+
+CORS_ALLOW_CREDENTIALS = True
 
 try:
     from .local import *  # NOSONAR # noqa: F401, F403
