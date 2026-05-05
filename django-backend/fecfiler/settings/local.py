f2e2901bc5c1fe78616a279e3ee7f4c10fb6634b
Elaine Krauss <104506225+Elaine-Krauss-TCG@users.noreply.github.com>
2026-05-05 10:41:33 -0400

    Linting fixes

--- django-backend/fecfiler/settings/local.py
+++ django-backend/fecfiler/settings/local.py
@@ -3,11 +3,11 @@ from corsheaders.defaults import default_headers
 
 # These settings are for local development only.
 
-INSTALLED_APPS.append("corsheaders")
-MIDDLEWARE.append("corsheaders.middleware.CorsMiddleware")
+INSTALLED_APPS.append("corsheaders")  # NOSONAR # noqa: F405
+MIDDLEWARE.append("corsheaders.middleware.CorsMiddleware")  # NOSONAR # noqa: F405
 
-CORS_ALLOWED_ORIGIN_REGEXES = ["http://localhost:4200"]  # NOSONAR # noqa: F405
-CSRF_TRUSTED_ORIGINS = ["http://localhost:4200"]  # NOSONAR # noqa: F405
+CORS_ALLOWED_ORIGIN_REGEXES = ["http://localhost:4200"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:4200"]
 
 CORS_ALLOW_HEADERS = (
     *default_headers,
