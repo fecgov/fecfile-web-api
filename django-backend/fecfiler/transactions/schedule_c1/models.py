e9dc9a300f40372b18a6bf41c01d8f1de9b31378
Elaine Krauss <elaine.krauss@tcg.com>
2026-05-19 17:20:58 -0400

    Adds interest rate and date format fields to schedule c1

--- django-backend/fecfiler/transactions/schedule_c1/models.py
+++ django-backend/fecfiler/transactions/schedule_c1/models.py
@@ -14,7 +14,11 @@ class ScheduleC1(models.Model):
         null=True, blank=True, max_digits=11, decimal_places=2
     )
     loan_interest_rate = models.TextField(null=True, blank=True)
+    loan_interest_rate_is_percent = models.BooleanField(
+        null=True, blank=True, default=False
+    )
     loan_incurred_date = models.DateField(null=True, blank=True)
+    loan_due_date_is_date = models.BooleanField(null=True, blank=True, default=False)
     loan_due_date = models.TextField(null=True, blank=True)
     loan_restructured = models.BooleanField(null=True, blank=True, default=False)
     loan_originally_incurred_date = models.DateField(null=True, blank=True)
