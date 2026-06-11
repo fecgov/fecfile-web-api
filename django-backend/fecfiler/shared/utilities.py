4ca8e06274572302aba6fbb06b1945c7b33f9d08
Max Zaremba <max.zaremba@tcg.com>
2026-06-11 18:25:24 -0400

    Added logging before and after request in query_fec_api

--- django-backend/fecfiler/shared/utilities.py
+++ django-backend/fecfiler/shared/utilities.py
@@ -2,6 +2,7 @@ import uuid
 import requests
 from rest_framework.status import HTTP_404_NOT_FOUND
 from fecfiler import settings
+import logging
 
 
 def generate_fec_uid():
@@ -49,7 +50,9 @@ def query_fec_api(endpoint, params, raise_for_404=True):
         "Content-Type": "application/json",
         "User-Agent": f"FECfile+ {settings.SPACE}",
     }
+    logging.info(f"Querying endpoint {endpoint}")
     response = requests.get(endpoint, headers=headers, params=params)
+    logging.info(f"Response received for endpoint {endpoint}: {response.status_code}")
     if response.status_code != HTTP_404_NOT_FOUND or raise_for_404:
         response.raise_for_status()
     response_data = response.json()
