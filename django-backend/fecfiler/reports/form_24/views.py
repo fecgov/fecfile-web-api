3087e358cabd7f65e8af263c522d7b556cb2c4ca
Sasha Dresden <sasha.dresden@tcg.com>
2026-06-22 16:29:40 -0400

    Add endpoint to check for F24 name clash

--- django-backend/fecfiler/reports/form_24/views.py
+++ django-backend/fecfiler/reports/form_24/views.py
@@ -3,6 +3,9 @@ from fecfiler.reports.models import Report
 from fecfiler.reports.managers import ReportType
 from fecfiler.reports.views import ReportViewSet
 from .serializers import Form24Serializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
 import structlog
 
 logger = structlog.get_logger(__name__)
@@ -28,3 +31,16 @@ class Form24ViewSet(ReportViewSet):
 
     def partial_update(self, request, pk=None):
         return super(ModelViewSet, self).partial_update(request, pk)
+
+    @action(detail=False, methods=["get"], url_path="check")
+    def check_name(self, request):
+        name = request.query_params.get("name", None)
+
+        if not name:
+            return Response(
+                {"error": "Name parameter is required."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        exists = self.get_queryset().filter(form_24__name__iexact=name).exists()
+        return Response({"available": not exists}, status=status.HTTP_200_OK)
