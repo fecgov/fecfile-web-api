e65ba1fda98ccb9c789b528064defd72f585ef26
Dan Fowlkes <dfowlkes@precision-solutions.com>
2026-07-23 16:56:34 -0400

    FECFILE-3275: Ignore exception returned as it's handled elsewhere.

--- django-backend/fecfiler/committee_accounts/views.py
+++ django-backend/fecfiler/committee_accounts/views.py
@@ -254,9 +254,12 @@ class CommitteeMembershipViewSet(CommitteeOwnedViewMixin, viewsets.ModelViewSet)
                     "You can access the committee account by signing in to FECfile+:\n"
                     f"https://{envbit}fecfile.fec.gov/"
                 )
-                send_email_notification(
-                    to_email=email, subject=subject, body_text=body_text
-                )
+                try:
+                    send_email_notification(
+                        to_email=email, subject=subject, body_text=body_text
+                    )
+                except Exception as e:
+                    pass
 
                 logger.info(
                     f"User {request.user.first_name} added {email} to committee "
