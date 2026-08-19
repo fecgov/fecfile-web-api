68a219eb27119a78490de940c08ebaf5b7a55f9b
Elaine Krauss <elaine.krauss@tcg.com>
2026-08-19 11:31:45 -0400

    Adds minimum viable data to update request

--- django-backend/fecfiler/reports/form_3x/tests/test_views.py
+++ django-backend/fecfiler/reports/form_3x/tests/test_views.py
@@ -120,9 +120,13 @@ class Form3XViewSetTest(FecfilerViewSetTest):
         )
 
         response = self.send_viewset_put_request(
-            f"/api/v1/reports/form3x/{str(report.form_3x.id)}/",
+            f"/api/v1/reports/form3x/{str(report.id)}/",
             {
-                "report_code":"Q2"
+                "report_code":"Q2",
+                'form_type': "F3XN",
+                'treasurer_last_name': "Treasurer",
+                'treasurer_first_name': "Mr",
+                'date_signed': "2004-01-01",
             },
             Form3XViewSet,
             "update",
