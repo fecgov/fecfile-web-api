974cea5f4bbbd056cb6db09afe59ac4114602b52
Sasha Dresden <sasha.dresden@tcg.com>
2026-08-19 15:29:24 -0400

    Update transaction viewset 'update' function to use get_object() in order to catch requests for invalid transaction ids and correctly return 404

--- django-backend/fecfiler/transactions/views.py
+++ django-backend/fecfiler/transactions/views.py
@@ -176,7 +176,11 @@ class TransactionViewSet(CommitteeOwnedViewMixin, ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         with db_transaction.atomic():
-            saved_transaction = self.save_transaction(request.data, request)
+            saved_transaction = self.save_transaction(
+                request.data,
+                request,
+                instance=self.get_object()
+            )
             update_dependent_parent_purpose_description_if_needed(saved_transaction)
         return Response(saved_transaction.id)
 
@@ -472,7 +476,7 @@ class TransactionViewSet(CommitteeOwnedViewMixin, ModelViewSet):
             ):
                 target.schedule_f.aggregate_general_elec_expended -= amount
 
-    def save_transaction(self, transaction_data, request):
+    def save_transaction(self, transaction_data, request, instance=None):
         committee_id = request.session["committee_uuid"]
         children = transaction_data.pop("children", [])
         schedule = transaction_data.get("schedule_id")
@@ -492,7 +496,7 @@ class TransactionViewSet(CommitteeOwnedViewMixin, ModelViewSet):
         old_snapshot = None  # Initialize early
 
         if is_existing:
-            original_instance = Transaction.objects.get(
+            original_instance = instance or Transaction.objects.get(
                 pk=transaction_data["id"], committee_account=committee_id
             )
             if original_instance is not None:
