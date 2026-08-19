2074088f42053c17a4950180525e49401a50168b
David Heitzer <david.heitzer@tcg.com>
2026-08-19 12:07:06 -0400

    crud tests

--- django-backend/fecfiler/transactions/tests/test_views.py
+++ django-backend/fecfiler/transactions/tests/test_views.py
@@ -1083,6 +1083,170 @@ class TransactionViewsTestCase(FecfilerViewSetTest):
         for transaction in c2_response.data:
             self.assertIn(transaction["id"], [str(c2_sa.id)])
 
+    def test_crud_committee_transactions(self):
+        c1 = CommitteeAccount.objects.create(committee_id="C00000001")
+        c1_q1_report = create_form3x(c1, "2024-01-01", "2024-02-01", {})
+        c1_org_contact = create_test_organization_contact(
+            "test-org-name1",
+            c1.id,
+            {
+                "street_1": "test_sa1",
+                "street_2": "test_sa2",
+                "city": "test_c1",
+                "state": "AL",
+                "zip": "12345",
+                "telephone": "555-555-5555",
+                "country": "USA",
+            },
+        )
+        c1_ind_contact = create_test_individual_contact(
+            "test_ln1",
+            "test_fn1",
+            c1.id,
+        )
+
+        c1_sa = create_schedule_a(
+            "INDIVIDUAL_RECEIPT",
+            c1,
+            c1_ind_contact,
+            "2023-01-01",
+            "500.00",
+            group="OTHER",
+            report=c1_q1_report,
+            memo_code=False,
+        )
+
+        c1_sb = create_schedule_b(
+            "OPERATING_EXPENDITURE_CREDIT_CARD_PAYMENT",
+            c1,
+            c1_org_contact,
+            "2023-01-02",
+            Decimal("250.00"),
+            report=c1_q1_report,
+        )
+
+        c2 = CommitteeAccount.objects.create(committee_id="C00000002")
+        c2_q1_report = create_form3x(c2, "2024-01-01", "2024-02-01", {})
+        c2_ind_contact = create_test_individual_contact(
+            "test_ln2",
+            "test_fn2",
+            c2.id,
+        )
+
+        c2_sa = create_schedule_a(
+            "INDIVIDUAL_RECEIPT",
+            c2,
+            c2_ind_contact,
+            "2023-01-01",
+            "500.00",
+            group="OTHER",
+            report=c2_q1_report,
+            memo_code=False,
+        )
+
+        ind_receipt_payload = {
+            "schedule_id": "A",
+            "form_type": "SA11AI",
+            "contribution_date": "2023-01-01",
+            "schema_name": "INDIVIDUAL_RECEIPT",
+            "transaction_type_identifier": "INDIVIDUAL_RECEIPT",
+            "aggregation_group": "GENERAL",
+            "contribution_amount": "100",
+            "contact_1_id": str(c1_ind_contact.id),
+            "report_ids": [str(c1_q1_report.id)],
+            "fields_to_validate": [
+                "schedule_id",
+                "form_type",
+                "schema_name",
+                "transaction_type_identifier",
+                "contribution_amount",
+                "parent_id",
+                "contact_1_id",
+            ],
+        }
+
+        # CREATE
+
+        c2_post_response = self.send_viewset_post_request(
+            "api/v1/transactions/",
+            ind_receipt_payload,
+            TransactionViewSet,
+            "create",
+            committee=c2,
+        )
+        # self.assertEqual(c2_post_response.status_code, 500)
+
+        c1_post_response = self.send_viewset_post_request(
+            "api/v1/transactions/",
+            ind_receipt_payload,
+            TransactionViewSet,
+            "create",
+            committee=c1,
+        )
+        self.assertEqual(c1_post_response.status_code, 200)
+
+        # READ
+
+        c2_get_response = self.send_viewset_get_request(
+            f"api/v1/transactions/{c1_post_response.data}/",
+            TransactionViewSet,
+            "retrieve",
+            pk=c1_post_response.data,
+            committee=c2,
+        )
+        # self.assertEqual(c2_get_response.status_code, 500)
+
+        c1_get_response = self.send_viewset_get_request(
+            f"api/v1/transactions/{c1_post_response.data}/",
+            TransactionViewSet,
+            "retrieve",
+            pk=c1_post_response.data,
+            committee=c1,
+        )
+        self.assertEqual(c1_get_response.status_code, 200)
+
+        # UPDATE
+
+        c2_put_response = self.send_viewset_put_request(
+            f"api/v1/transactions/{c1_post_response.data}/",
+            ind_receipt_payload,
+            TransactionViewSet,
+            "update",
+            pk=c1_post_response.data,
+            committee=c2,
+        )
+        # self.assertEqual(c2_put_response.status_code, 500)
+
+        c1_put_response = self.send_viewset_put_request(
+            f"api/v1/transactions/{c1_post_response.data}/",
+            ind_receipt_payload,
+            TransactionViewSet,
+            "update",
+            pk=c1_post_response.data,
+            committee=c1,
+        )
+        self.assertEqual(c1_put_response.status_code, 200)
+
+        # DELETE
+
+        c2_delete_response = self.send_viewset_delete_request(
+            f"api/v1/transactions/{c1_post_response.data}/",
+            TransactionViewSet,
+            "destroy",
+            pk=c1_post_response.data,
+            committee=c2,
+        )
+        # self.assertEqual(c2_delete_response.status_code, 500)
+
+        c1_delete_response = self.send_viewset_delete_request(
+            f"api/v1/transactions/{c1_post_response.data}/",
+            TransactionViewSet,
+            "destroy",
+            pk=c1_post_response.data,
+            committee=c1,
+        )
+        self.assertEqual(c1_delete_response.status_code, 204)
+
     def xtest_destroy(self):
         response = self.send_viewset_delete_request(
             f"api/v1/transactions/{self.transaction.id}/",
