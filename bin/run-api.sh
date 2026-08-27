ddf9ee9b9cca8787037503ed24d3811f48eaf97b
Laura Beaufort <laurabeaufort@gmail.com>
2026-08-27 10:00:09 -0400

    Revert "feature/3070 - Use an environment variable to define gunicorn worker and thread counts"

--- bin/run-api.sh
+++ bin/run-api.sh
@@ -3,5 +3,4 @@ cd django-backend
 echo "------ Starting APP ------"
 
 # Run application
-./manage.py collectstatic --noinput --traceback --verbosity 3 && exec gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w $GUNICORN_WORKERS --threads=$GUNICORN_THREADS
-
+./manage.py collectstatic --noinput --traceback --verbosity 3 && exec gunicorn --bind 0.0.0.0:8080 fecfiler.wsgi -w 9 --threads=8
