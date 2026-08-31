2279a04a834f9eb468db344ee5c5e1c7515cb994
David Heitzer <david.heitzer@tcg.com>
2026-08-31 16:47:28 -0400

    fosec-130 committee mixin pagination

--- django-backend/fecfiler/committee_accounts/views.py
+++ django-backend/fecfiler/committee_accounts/views.py
@@ -139,17 +139,6 @@ class CommitteeOwnedViewMixin(viewsets.GenericViewSet):
             raise PermissionDenied("You must activate a committee account")
         return committee_uuid
 
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-        if "page" in request.query_params:
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
-
 
 class IsCommitteeAdministrator(BasePermission):
     """
