b9b998d6eefed98920d3eb8a6fb965678068a487
Dan Fowlkes <dfowlkes@precision-solutions.com>
2026-09-09 10:19:52 -0400

    MIRROR TEST: update branch to deploy

--- tasks.py
+++ tasks.py
@@ -53,7 +53,7 @@ DEPLOY_RULES = (
     ("prod", lambda _, branch: branch == "main"),
     ("test", lambda _, branch: branch == "release/test"),
     ("stage", lambda _, branch: branch.startswith("release/sprint")),
-    ("dev", lambda _, branch: branch == "feature/2427-deploy-to-mirror"),
+    ("dev", lambda _, branch: branch == "feature/2421-delete_load_test_data-test"),
 )
 
 
